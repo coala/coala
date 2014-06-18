70e0057d6fdb4457fc8fed64b3ec106d6d5402f6
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-06-18 11:24:41 +0200

    Settings: Make backup of .codecfile on autosave
    See https://github.com/sils1297/codec/issues/8

--- codeclib/fillib/util/settings.py
+++ codeclib/fillib/util/settings.py
@@ -14,6 +14,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from collections import OrderedDict
+import shutil
 from codeclib.fillib.util.setting import Setting
 
 import os
@@ -112,6 +113,8 @@ class Settings(OrderedDict):
             self.__import_setting('comment', Setting('', '', import_history, comments))
 
     def save_to_file(self, path):
+        # create backup file
+        shutil.copy2(path, path+'~')
         with open(path, 'w') as config_file:
             for setting in self.values():
                 imp = self.__setting_is_implicit(setting)
