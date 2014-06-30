b221ea01d3e0d5b96d5563eb02de6a639323b200
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-06-30 18:44:48 +0200

    Add setting (again)

---
+++ codeclib/fillib/settings/Setting.py
@@ -0,0 +1,46 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from collections import OrderedDict
+
+
+def path(obj):
+    assert (type(obj) == Setting)
+    obj.__path__()
+
+
+class Setting:
+    def __init__(self, value, origin):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __bool__(self):
+        true_strings = ['1', 'y', 'yes', 'yeah', 'always', 'sure', 'definitely', 'yup', 'true']
+        false_strings = ['0', 'n', 'no', 'nope', 'never', 'nah', 'false']
+        if self.value in true_strings:
+            return True
+        if self.value in false_strings:
+            return False
+        raise AttributeError
+
+    def __int__(self):
+        return int(self.value)
+
+    def __len__(self):
+        return len(self.value)
+
+    def __path__(self):
+        raise NotImplementedError
