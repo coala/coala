5e3743e0a10141093f953c52482ee013f01d6a69
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-03 21:03:02 +0200

    i18n: Add pot update script

---
+++ generate_pot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p tmp
+echo '' > tmp/messages.po
+echo Generating new pot file...
+xgettext -j --language=Python --output=tmp/messages.po --keyword=_ --from-code=UTF-8 `find . -name "*.py"` codec
+echo Merging with existent pot file...
+msgmerge -N i18n/Codec.pot tmp/messages.po > i18n/Codec.pot 2> /dev/null
+echo Removing temporary files...
+rm -rf tmp
+echo Done.
+echo
+echo Please update the po files accordingly.
