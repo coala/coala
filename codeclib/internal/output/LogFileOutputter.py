307794d2996dbdf3dd88fc7faf7d557474f23534
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-03 14:03:45 +0200

    LogFileOutputter: Finish implementation

--- codeclib/internal/output/LogFileOutputter.py
+++ codeclib/internal/output/LogFileOutputter.py
@@ -12,15 +12,32 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+import os
+import shutil
 from codeclib.internal.output.Outputter import Outputter
 
 
-class ConsoleOutputter(Outputter):
-    def __init__(self):
+class LogFileOutputter(Outputter):
+    def __init__(self, filename):
         Outputter.__init__(self)
 
+        # backup old logfile, don't backup old backup
+        if os.path.exists(filename):
+            shutil.copy2(filename, filename+"~")
+
+        self.file = open(filename, "w")
+
+    def __del__(self):
+        self.file.close()
+
     def print(self, *args, delimiter=' ', end='\n', log_date=True):
-        raise NotImplementedError
+        output = ""
+        for arg in args:
+            if output != "":
+                output += delimiter
+            output += arg
+
+        self.file.write(output + end)
 
     def color_print(self, color, *args, delimiter=' ', end='\n', log_date=True):
         self.print(args, delimiter=delimiter, end=end, log_date=log_date)
