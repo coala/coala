c8847af0a49bb8b1fcbb0ac1511672059a77c6ca
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-04 09:23:42 +0200

    Add noinpection tags for pycharm

--- codeclib/internal/process_managing/FilterProcess.py
+++ codeclib/internal/process_managing/FilterProcess.py
@@ -13,6 +13,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+# noinspection PyUnresolvedReferences
 from codeclib.fillib.misc import i18n
 import queue
 import sys
