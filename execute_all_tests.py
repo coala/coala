6d4a99b4f26776975f3c1234b09c6d0d6ec118c7
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-07 18:41:47 +0200

    LineParserTest: Make tests w/o installation possible

--- execute_all_tests.py
+++ execute_all_tests.py
@@ -12,9 +12,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+import os
 
 import tests
 from tests.TestHelper import TestHelper
+import sys
 
 
 if __name__ == '__main__':
