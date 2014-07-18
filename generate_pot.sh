5865e6a81ab1714c48951ef0558b0f69e872b6d3
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-18 14:23:30 +0200

    Add POT generation script
    This allows the translation template to be generated easily.

---
+++ generate_pot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p tmp
+echo '' > tmp/messages.po
+echo Generating new pot file...
+xgettext -j -c --language=Python --output=tmp/messages.po --keyword=_ --from-code=UTF-8 `find . -name "*.py" | grep -v build`
+echo Merging with existent pot file...
+msgmerge -N locale/coala.pot tmp/messages.po > locale/coala.pot 2> /dev/null
+echo Removing temporary files...
+#rm -rf tmp
+echo Done.
+echo
+echo Please update the po files accordingly.
