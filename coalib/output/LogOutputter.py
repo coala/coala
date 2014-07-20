daba6018414eb3ba81a92640d218bd3f91f84e8a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-20 17:30:48 +0200

    output: Add LogOutputter

--- coalib/output/LogOutputter.py
+++ coalib/output/LogOutputter.py
@@ -10,24 +10,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-import sys
-sys.path.append(".")
-from coalib.output.LogOutputter import LogOutputter
 from coalib.output.Outputter import Outputter
-import unittest
 
 
-class OutputterTestCase(unittest.TestCase):
-    def test_run_available(self):
-        self.uut = Outputter()
-        self.assertRaises(NotImplementedError, self.uut.print, "test")
-
-        self.uut = LogOutputter()
-        self.assertRaises(NotImplementedError, self.uut.print, "test")
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+class LogOutputter(Outputter):
+    def print(self, *args, delimiter=' ', end='\n', log_date=True):
+        """
+        Some outputters may choose to ignore certain parameters (color, log_date)
+        """
+        raise NotImplementedError
