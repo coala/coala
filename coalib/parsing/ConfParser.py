17341b63ad6817749c66a88333647f8d9cc0ee88
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-06 20:23:12 +0200

    ConfParser: Open file, add __parse_lines stub

--- coalib/parsing/ConfParser.py
+++ coalib/parsing/ConfParser.py
@@ -12,10 +12,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+from collections import OrderedDict
 from coalib.parsing.Parser import Parser
+from coalib.settings.Settings import Settings
+from coalib.misc.i18n import _
 
 
 class ConfParser(Parser):
+    def __init__(self):
+        # Declare it
+        self.sections = None
+        self.__init_sections()
+
     def parse(self, input_data, overwrite=False):
         """
         Parses the input and adds the new data to the existing
@@ -24,7 +32,17 @@ class ConfParser(Parser):
         :param overwrite: behaves like reparse if this is True
         :return a non empty string containing an error message on failure
         """
-        raise NotImplementedError
+        try:
+            f = open(input_data, "r", encoding='utf-8')
+            lines = f.readlines()
+
+            if overwrite:
+                self.__init_sections()
+
+            self.__parse_lines(lines)
+        except FileNotFoundError:
+            return _("Failed reading file. Please make sure to provide a file that is existent and "
+                     "you have the permission to read it.")
 
     def reparse(self, input_data):
         """
@@ -34,3 +52,10 @@ class ConfParser(Parser):
         :return a non empty string containing an error message on failure
         """
         return self.parse(input_data, overwrite=True)
+
+    def __parse_lines(self, lines):
+        raise NotImplementedError
+
+    def __init_sections(self):
+        self.sections = OrderedDict()
+        self.sections["default"] = Settings("Default")
