8f24ed3f57e2368629e08b85b5ba0ebdebc45dd9
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-08 10:04:45 +0200

    ColorPrinter: Handle None case in baseclass

--- coalib/output/ColorPrinter.py
+++ coalib/output/ColorPrinter.py
@@ -22,13 +22,16 @@ class ColorPrinter(Printer):
     p.print("some", "output", delimiter=" ", end="", color="green");
     """
     def _print(self, output, **kwargs):
+        if kwargs.get("color") is None:
+            return self._print_uncolored(output, **kwargs)
+
         try:
             return self._print_colored(output, **kwargs)
         except:
             return self._print_uncolored(output, **kwargs)
 
-    def _print_colored(self, output, color=None):
+    def _print_colored(self, output, color=None, **kwargs):
         raise NotImplementedError
 
-    def _print_uncolored(self, output):
+    def _print_uncolored(self, output, **kwargs):
         raise NotImplementedError
