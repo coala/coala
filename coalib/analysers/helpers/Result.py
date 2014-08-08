dfb523ac445ab43e596b76c67ec7bd0344e42bad
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-08 20:23:27 +0200

    Result: Add comparision methods

--- coalib/analysers/helpers/Result.py
+++ coalib/analysers/helpers/Result.py
@@ -13,6 +13,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 from coalib.analysers.helpers.ANNOTATION_SEVERITY import ANNOTATION_SEVERITY
+from coalib.misc.i18n import _
 
 
 class Result:
@@ -27,3 +28,53 @@ class Result:
         self.line = line
         self.relevant_lines_before = relevant_lines_before
         self.severity = severity
+
+    def __eq__(self, other):
+        self.__check_other_for_comparision(other)
+
+        return ((self.line_nr == other.line_nr) and
+                (self.line == other.line) and
+                (self.severity == other.severity) and
+                (self.relevant_lines_before == other.relevant_lines_before))
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        self.__check_other_for_comparision(other)
+
+        return ((self.line_nr < other.line_nr) or
+                (self.line_nr == other.line_nr and
+                 self.line == other.line and
+                 self.severity < other.severity))
+
+    def __gt__(self, other):
+        self.__check_other_for_comparision(other)
+
+        return ((self.line_nr > other.line_nr) or
+                (self.line_nr == other.line_nr and
+                 self.line == other.line and
+                 self.severity > other.severity))
+
+    def __le__(self, other):
+        self.__check_other_for_comparision(other)
+
+        return ((self.line_nr <= other.line_nr) or
+                (self.line_nr == other.line_nr and
+                 self.line == other.line and
+                 self.severity <= other.severity))
+
+    def __ge__(self, other):
+        self.__check_other_for_comparision(other)
+
+        return ((self.line_nr >= other.line_nr) or
+                (self.line_nr == other.line_nr and
+                 self.line == other.line and
+                 self.severity >= other.severity))
+
+    def __check_other_for_comparision(self, other):
+        if not isinstance(other, Result):
+            raise TypeError(_("Comparision of a Result with a non-Result is impossible."))
+
+        if self.file_name != other.file_name:
+            raise AttributeError(_("Comparision of results affecting different files is impossible."))
