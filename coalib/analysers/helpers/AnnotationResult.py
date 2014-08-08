15f762d0ef9061384eaed798853e9a7da3f2e3a9
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-08 13:39:35 +0200

    AnnotationResult: Add members

--- coalib/analysers/helpers/AnnotationResult.py
+++ coalib/analysers/helpers/AnnotationResult.py
@@ -12,9 +12,22 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+from coalib.analysers.helpers.ANNOTATION_SEVERITY import ANNOTATION_SEVERITY
 from coalib.analysers.helpers.Result import Result
 
 
 class AnnotationResult(Result):
-    def __init__(self):
+    def __init__(self,
+                 message,
+                 file_name,
+                 line_nr,
+                 line,
+                 relevant_lines_before=1,
+                 severity=ANNOTATION_SEVERITY.NORMAL):
         Result.__init__(self)
+        self.message = message
+        self.file_name = file_name
+        self.line_nr = line_nr
+        self.line = line
+        self.relevant_lines_before = relevant_lines_before
+        self.severity = severity
