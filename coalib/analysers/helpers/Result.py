8f0fab392c1789db9675448d89d1d1b07ace8f37
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-10 20:35:11 +0200

    helpers: Add Result

---
+++ coalib/analysers/helpers/Result.py
@@ -0,0 +1,23 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+from coalib.analysers.helpers.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class Result:
+    def __init__(self, origin, message, file=None, severity=RESULT_SEVERITY.NORMAL):
+        self.origin = origin
+        self.message = message
+        self.file = file
+        self.severity = severity
