b88977a0ffc06e0e8efaba3bebeabae5f0c5c9a5
Fabian Neuschmidt <fabian@neuschmidt.de>
2014-08-18 19:58:43 +0200

    Collectors: Add base class
    A Collector is the base class of anything that collects a set of
    arbitrary items. It provides mainly iterators.

--- coalib/collecting/Collector.py
+++ coalib/collecting/Collector.py
@@ -12,3 +12,35 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+
+
+class Collector:
+    def __init__(self):
+        self._items = None
+
+    def collect(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        self._check_item_availability()
+
+        return iter(self._items)
+
+    def __len__(self):
+        self._check_item_availability()
+
+        return len(self._items)
+
+    def __getitem__(self, item):
+        self._check_item_availability()
+
+        return self._items[item]
+
+    def __reversed__(self):
+        self._check_item_availability()
+
+        return reversed(self._items)
+
+    def _check_item_availability(self):
+        if self._items is None:
+            raise ValueError("Collector must collect items before they can be accessed")
