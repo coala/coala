f716fed399a97a8922eb661c38c8fa771a0897bc
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-09-19 13:18:45 +0200

    ConsolePrinter: Pass file for colorized output too

--- coalib/output/ConsolePrinter.py
+++ coalib/output/ConsolePrinter.py
@@ -43,4 +43,4 @@ class ConsolePrinter(LogPrinter, ColorPrinter):
         if color_code is None:
             raise ValueError("Invalid color value")
 
-        print('\033[' + color_code + 'm' + output + '\033[0m', end="")
+        print('\033[' + color_code + 'm' + output + '\033[0m', file=self.output, end="")
