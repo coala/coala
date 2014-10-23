836b771bcf62aa9327df7c5ab2c1443ed9ec8a47
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-23 15:54:36 +0200

    results: Add ResultTest

---
+++ coalib/tests/analysers/results/ResultTest.py
@@ -0,0 +1,36 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+sys.path.insert(0, ".")
+from coalib.analysers.results.Result import Result, RESULT_SEVERITY
+import unittest
+
+
+class ResultTestCase(unittest.TestCase):
+    def setUp(self):
+        self.uut = Result("origin", "message")
+
+    def test_equality(self):
+        cmp = Result("origin", "message")
+        self.assertEqual(cmp, self.uut)
+        cmp = Result("another origin", "message")
+        self.assertNotEqual(cmp, self.uut)
+        cmp = Result("origin", "another message")
+        self.assertNotEqual(cmp, self.uut)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
