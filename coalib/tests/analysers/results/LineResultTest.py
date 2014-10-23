eac99d3ae7a2a2473dcec72b14326e900ef9a0ef
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-23 16:01:10 +0200

    results: Add LineResultTest

---
+++ coalib/tests/analysers/results/LineResultTest.py
@@ -0,0 +1,36 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+sys.path.insert(0, ".")
+from coalib.analysers.results.LineResult import Result, LineResult, RESULT_SEVERITY
+import unittest
+
+
+class ResultTestCase(unittest.TestCase):
+    def setUp(self):
+        self.uut = LineResult("origin", 1, "line", "message", "file")
+
+    def test_equality(self):
+        cmp = LineResult("origin", 1, "line", "message", "file")
+        self.assertEqual(cmp, self.uut)
+        cmp = Result("origin", "message")
+        self.assertNotEqual(cmp, self.uut)
+        cmp = LineResult("origin", 1, "lineswrong", "message", "file")
+        self.assertNotEqual(cmp, self.uut)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
