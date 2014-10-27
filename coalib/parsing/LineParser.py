749e3c55f4725cb77a83d4a220860f22c65c9262
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-27 07:57:13 +0100

    LineParser: Add __seperate_by_first_occurrence()
    This is part of a small refactoring to reduce code duplication in the
    LineParser.

--- coalib/parsing/LineParser.py
+++ coalib/parsing/LineParser.py
@@ -43,6 +43,33 @@ class LineParser:
 
         return '', keys, value, comment
 
+    @staticmethod
+    def __seperate_by_first_occurrence(string, delimiters, strip_delim=False, return_second_part_nonempty=False):
+        """
+        Seperates a string by the first of all given delimiters. Any whitespace characters will be stripped away from
+        the parts.
+
+        :param string: The string to seperate.
+        :param delimiters: The delimiters.
+        :param strip_delim: Strips the delimiter from the result if true.
+        :param return_second_part_nonempty: If no delimiter is found and this is true the contents of the string will be
+        returned in the second part of the touple instead of the first one.
+        :return: (first_part, second_part)
+        """
+        delim_pos = len(string)
+        used_delim = ""
+        for delim in delimiters:
+            pos = string.find(delim)
+            if 0 <= pos < delim_pos:
+                delim_pos = pos
+                used_delim = delim
+
+        if return_second_part_nonempty and delim_pos == len(string):
+            return "", string.strip(" \n")
+
+        return string[:delim_pos].strip(" \n"),\
+               string[delim_pos + (len(used_delim) if strip_delim else 0):].strip(" \n")
+
     def __get_section_name(self, line):
         for begin, end in self.section_name_surroundings.items():
             if line[0:len(begin)] == begin and \
