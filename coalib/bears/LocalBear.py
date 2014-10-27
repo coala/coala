086dd322e70ea6466d1ce18d373bf7a6789193de
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-27 19:03:03 +0100

    LocalBear: Documentation changes only

--- coalib/bears/LocalBear.py
+++ coalib/bears/LocalBear.py
@@ -17,6 +17,17 @@ from coalib.bears.BEAR_KIND import BEAR_KIND
 
 
 class LocalBear(Bear):
+    """
+    A LocalBear is a Bear that analyzes only one file at once. It therefore can not analyze semantical facts over
+    multiple files or so.
+
+    This has the advantage that it can be highly parrallelized. In addition the results from multiple bears for one
+    file can be shown together for that file which is better to grasp for the user. coala takes care of all that.
+
+    Examples for LocalBear's could be:
+    - A SpaceConsistencyBear that checks every line for trailing whitespaces, tabs, ...
+    - A VariableNameBear that checks variable names and constant names for certain conditions
+    """
     def __init__(self,
                  settings,
                  message_queue,
