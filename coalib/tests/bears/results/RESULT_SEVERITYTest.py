866ba4a7b0a03afc6b64c643c93d973d3b458bb5
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-28 17:36:34 +0100

    RESULT_SEVERITY: Add tests

---
+++ coalib/tests/bears/results/RESULT_SEVERITYTest.py
@@ -0,0 +1,31 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+
+sys.path.insert(0, ".")
+from coalib.bears.results.RESULT_SEVERITY import RESULT_SEVERITY
+import unittest
+
+
+class RESULT_SEVERITYTestCase(unittest.TestCase):
+    def test_str_conversion(self):
+        self.assertEqual("INFO", RESULT_SEVERITY.__str__(RESULT_SEVERITY.INFO))
+        self.assertEqual("NORMAL", RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL))
+        self.assertEqual("MAJOR", RESULT_SEVERITY.__str__(RESULT_SEVERITY.MAJOR))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
