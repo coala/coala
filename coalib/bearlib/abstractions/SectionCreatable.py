3eaeffb7f3924dbaf37e8492a0b81cfadc8b1737
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-31 19:49:00 +0100

    coalib: Add bearlib.abstractions.SettingsCreatable

---
+++ coalib/bearlib/abstractions/SectionCreatable.py
@@ -0,0 +1,70 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+class SectionCreatable:
+    """
+    A SectionCreatable is an object that is creatable out of a section object. Thus this is the class for many helper
+    objects provided by the bearlib.
+
+    If you want to use an object that inherits from this class the following approach is recommended:
+    Instantiate it via the from_section method. You can provide default arguments via the lower case keyword
+    arguments. Example:
+        SpacingHelper.from_section(section, tabwidth=8)
+    creates a SpacingHelper and if the "tabwidth" setting is needed and not contained in section, 8 will be taken.
+
+    In addition you might want to implement the get_needed_settings method of your bear in the following manner
+    (exemplary):
+        def get_needed_settings():
+            needed_settings = {}  # Get your needed settings
+
+            # Retrieve minimal needed settings from SpacingHelper and add them to the dict
+            needed_settings.extend(SpacingHelper.get_minimal_needed_settings())
+            return needed_settings
+
+    If you don't want to rely on any defaults you can get a dict of all settings needed by the object via:
+        SpacingHelper.get_needed_settings()
+
+    Please consider that it is usually good to guess settings for the user if it is possible to do a reasonable guess
+    that suits most needs. Thus the use of get_minimal_needed_settings is preferred over get_needed_settings.
+    """
+    @classmethod
+    def from_section(cls, section, **kwargs):
+        """
+        Creates the object from a section object.
+
+        :param section: A section object containing at least the settings specified by get_minimal_needed_settings()
+        :param kwargs: defaults for settings with the given key. If no setting with the specified key is needed it will
+                       be ignored.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def get_minimal_needed_settings():
+        """
+        Retrieves the minimal set of settings that need to be defined in order to use this object.
+
+        :return: a dictionary of needed settings as keys and help texts as values
+        """
+        return {}
+
+    @staticmethod
+    def get_needed_settings():
+        """
+        Retrieves the settings needed to use this object without using defaults.
+
+        :return: a dictionary of needed settings as keys and help texts as values
+        """
+        raise NotImplementedError
---
+++ coalib/tests/bearlib/abstractions/SectionCreatableTest.py
@@ -0,0 +1,32 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+
+sys.path.insert(0, ".")
+import unittest
+from coalib.bearlib.abstractions.SectionCreatable import SectionCreatable
+
+
+class SectionCreatableTestCase(unittest.TestCase):
+    def test_api(self):
+        uut = SectionCreatable()
+        self.assertRaises(NotImplementedError, uut.from_section, 5)
+        self.assertRaises(NotImplementedError, uut.get_needed_settings)
+        self.assertEqual(uut.get_minimal_needed_settings(), {})
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
