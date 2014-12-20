de35ced6f4ab6284571701ef5983eaad1b59158b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-12-20 17:21:55 +0100

    LocalTestBear: Reduce sleep time
    0.05 seconds should be enough for the other thread to go ahead. This
    reduces time it takes to run all tests by about one second which is
    good.

--- coalib/tests/processes/section_executor_test_files/LocalTestBear.py
+++ coalib/tests/processes/section_executor_test_files/LocalTestBear.py
@@ -21,5 +21,5 @@ class LocalTestBear(LocalBear):  # pragma: no cover
     def run_bear(self, filename, file):
         # we need to test that the SectionExecutor holds back the global results until processing of all local ones is
         # finished
-        time.sleep(1)
+        time.sleep(0.05)
         return [Result("LocalTestBear", "test message")]
