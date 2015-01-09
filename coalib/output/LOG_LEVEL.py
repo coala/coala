d75b13b18eee1997b1ed9ba8b8906eea906f1a19
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-09 08:22:49 +0100

    LOG_LEVEL: Add from_str()
    It isn't needed to have a dedicated test for this method solely. It will
    be covered in one of the following commits I promise.

--- coalib/output/LOG_LEVEL.py
+++ coalib/output/LOG_LEVEL.py
@@ -18,3 +18,11 @@ class LOG_LEVEL:
     DEBUG = 0
     WARNING = 1
     ERROR = 2
+
+    @staticmethod
+    def from_str(string):
+        return {
+            "debug": LOG_LEVEL.DEBUG,
+            "warning": LOG_LEVEL.WARNING,
+            "error": LOG_LEVEL.ERROR
+        }.get(string.lower(), LOG_LEVEL.WARNING)
