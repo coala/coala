9a44f116b48cfbe335d0f5e86e826f6c0fe40a63
Fabian Neuschmidt <fabian@neuschmidt.de>
2015-01-18 16:47:25 +0100

    ConsoleOutputter: Override print_results()
    This adds console output according to our specification. Since no line
    replacements are allowed yet the mod_nr remains the same as the real_nr
    for now.
    
    Further credit goes to Lasse Schuirmann who refined the original work
    from Fabian Neuschmidt and added tests.

--- coalib/output/ConsoleOutputter.py
+++ coalib/output/ConsoleOutputter.py
@@ -21,9 +21,21 @@ from coalib.misc.i18n import _
 
 
 class ConsoleOutputter(Outputter, ConsolePrinter):
-    def __init__(self, output=sys.stdout, log_level=LOG_LEVEL.WARNING, timestamp_format="%X"):
+    STR_LINE_DOESNT_EXIST = _("A the line belonging to the following result cannot be printed because it refers to a "
+                              "line that doesn't seem to exist in the given file.")
+    STR_PROJECT_WIDE = _("Project wide:")
+
+    def __init__(self,
+                 output=sys.stdout,
+                 log_level=LOG_LEVEL.WARNING,
+                 timestamp_format="%X",
+                 pre_padding=3,
+                 log_printer=None):
         Outputter.__init__(self)
         ConsolePrinter.__init__(self, output=output, log_level=log_level, timestamp_format=timestamp_format)
+
+        self.pre_padding = pre_padding
+        self.log_printer = self if log_printer is None else log_printer
 
     def acquire_settings(self, settings_names_dict):
         if not isinstance(settings_names_dict, dict):
@@ -54,7 +66,7 @@ class ConsoleOutputter(Outputter, ConsolePrinter):
                                                                                                  needed))
 
     def _format_line(self, line, real_nr="", sign="|", mod_nr="", symbol="", ):
-        return "|{:>4}{}{:>4}|{:1}{}".format(real_nr, sign, mod_nr, symbol, line)
+        return "|{:>4}{}{:>4}|{:1}{}".format(real_nr, sign, mod_nr, symbol, line.rstrip("\n"))
 
     def _print_result(self, result):
         if not isinstance(result, Result):
@@ -65,3 +77,58 @@ class ConsoleOutputter(Outputter, ConsolePrinter):
                                                               msg=result.message).split("\n")
 
         return self.print("\n".join([self._format_line(line) for line in message_string_list]))
+
+    def _print_segregation(self, n=3):
+        self.print("\n".join(self._format_line(line="", sign=".") for i in range(n)))
+
+    def _print_lines(self, file_dict, current_line, result_line, result_file):
+        """
+        Prints the lines between the current and the result line. If needed they will be shortened.
+        """
+        line_delta = result_line - current_line
+
+        if line_delta > self.pre_padding:
+            self._print_segregation()
+
+            for i in range(max(result_line - self.pre_padding, 1), result_line + 1):
+                self.print(self._format_line(line=file_dict[result_file][i - 1],
+                                             real_nr=i,
+                                             mod_nr=i))
+        else:
+            for i in range(1, line_delta + 1):
+                self.print(self._format_line(line=file_dict[result_file][current_line + i - 1],
+                                             real_nr=current_line + i,
+                                             mod_nr=current_line + i))
+
+    def print_results(self, result_list, file_dict):
+        if not isinstance(result_list, list):
+            raise TypeError("result_list should be of type list")
+        if not isinstance(file_dict, dict):
+            raise TypeError("file_dict should be of type dict")
+
+        current_file = False  # We can't use None since we need line 109 be executed if file of first result is None
+        current_line = 0
+
+        for result in sorted(result_list):
+            if result.file != current_file:
+                if result.file in file_dict or result.file is None:
+                    current_file = result.file
+                    current_line = 0
+                    self.print("\n\n{}".format(current_file if current_file is not None else self.STR_PROJECT_WIDE))
+                else:
+                    self.log_printer.warn(_("A result ({}) cannot be printed because it refers to a file that doesn't"
+                                            " seem to exist.").format(str(result)))
+                    continue
+
+            if hasattr(result, "line_nr"):
+                if current_file is None:
+                    raise AssertionError("A result with a line_nr should also have a file.")
+                if result.line_nr < current_line:  # pragma: no cover
+                    raise AssertionError("The sorting of the results doesn't work correctly.")
+                if len(file_dict[result.file]) < result.line_nr - 1:
+                    self.print(self._format_line(line=self.STR_LINE_DOESNT_EXIST))
+                else:
+                    self._print_lines(file_dict, current_line, result.line_nr, result.file)
+                    current_line = result.line_nr
+
+            self._print_result(result)
--- coalib/tests/output/ConsoleOutputterTest.py
+++ coalib/tests/output/ConsoleOutputterTest.py
@@ -12,14 +12,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+import queue
 import unittest
 import sys
-
 sys.path.insert(0, ".")
 import builtins
-
-from coalib.bears.results.Result import Result
+from coalib.bears.results.LineResult import LineResult, Result
 from coalib.bears.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.output.NullPrinter import NullPrinter
 from coalib.misc.i18n import _
 
 _input = builtins.__dict__["input"]
@@ -64,6 +64,93 @@ class ConsoleOutputterTestCase(unittest.TestCase):
         self.assertEqual("|    |    | [{normal}] {bear}:".format(normal=RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL),
                                                                  bear="origin") + "\n|    |    | message",
                          self.uut.print_result(Result("origin", "message")))
+
+    def test_print_results(self):
+        self.assertRaises(TypeError, self.uut.print_results, 5, {})
+        self.assertRaises(TypeError, self.uut.print_results, [], 5)
+        q = queue.Queue()
+        self.uut._print = lambda string: q.put(string)
+
+        self.uut.print_results([], {})
+        self.assertRaises(queue.Empty, q.get, timeout=0)
+
+        self.uut.print_results([Result("origin", "message")], {})
+        self.assertEqual("\n\n{}\n|    |    | [{}] origin:\n|    |    | message"
+                         "\n".format(self.uut.STR_PROJECT_WIDE, RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL)),
+                         self.get_str_from_queue(q))
+
+        self.uut.print_results([LineResult("SpaceConsistencyBear", 2, "", "Trailing whitespace found", "proj/white")],
+                               {"proj/white": ["test line\n",
+                                               "line 2\n",
+                                               "line 3\n"]})
+        self.assertEqual("""\n\nproj/white
+|   1|   1| test line\n|   2|   2| line 2
+|    |    | [{}] SpaceConsistencyBear:
+|    |    | Trailing whitespace found
+""".format(RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL)), self.get_str_from_queue(q))
+
+        self.uut.print_results([LineResult("SpaceConsistencyBear", 5, "", "Trailing whitespace found", "proj/white")],
+                               {"proj/white": ["test line\n",
+                                               "line 2\n",
+                                               "line 3\n",
+                                               "line 4\n",
+                                               "line 5\n"]})
+        self.assertEqual("""\n\nproj/white
+|    .    | \n|    .    | \n|    .    | \n|   2|   2| line 2
+|   3|   3| line 3
+|   4|   4| line 4
+|   5|   5| line 5
+|    |    | [{}] SpaceConsistencyBear:
+|    |    | Trailing whitespace found
+""".format(RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL)), self.get_str_from_queue(q))
+
+        # Check sorting and multi result output
+        self.uut.print_results([LineResult("SpaceConsistencyBear", 5, "", "Trailing whitespace found", "proj/white"),
+                                LineResult("SpaceConsistencyBear", 2, "", "Trailing whitespace found", "proj/white")],
+                               {"proj/white": ["test line\n",
+                                               "line 2\n",
+                                               "line 3\n",
+                                               "line 4\n",
+                                               "line 5\n"]})
+
+        self.assertEqual("""\n\nproj/white
+|   1|   1| test line
+|   2|   2| line 2
+|    |    | [{}] SpaceConsistencyBear:
+|    |    | Trailing whitespace found
+|   3|   3| line 3
+|   4|   4| line 4
+|   5|   5| line 5
+|    |    | [{}] SpaceConsistencyBear:
+|    |    | Trailing whitespace found
+""".format(RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL),
+           RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL)), self.get_str_from_queue(q))
+
+        # File isn't in dict, shouldn't print but also shouldn't throw. This can occur if filter writers are doing
+        # nonsense. If this happens twice the same should happen (whitebox testing: this is a potential bug.)
+        self.uut.log_printer = NullPrinter()
+        self.uut.print_results([LineResult("t", 5, "", "msg", "file"), LineResult("t", 5, "", "msg", "file")], {})
+        self.assertEqual("", self.get_str_from_queue(q))
+
+        # Line isn't in dict[file], shouldn't print but also shouldn't throw. This can occur if filter writers are doing
+        # nonsense.
+        self.uut.print_results([LineResult("t", 5, "", "msg", "file")], {"file": []})
+        self.assertEqual("""\n\nfile\n|    |    | {}\n|    |    | [{}] t:
+|    |    | msg\n""".format(self.uut.STR_LINE_DOESNT_EXIST, RESULT_SEVERITY.__str__(RESULT_SEVERITY.NORMAL)),
+                         self.get_str_from_queue(q))
+
+        self.assertRaises(AssertionError, self.uut.print_results, [LineResult("t", 5, "", "msg", None)], {})
+
+    @staticmethod
+    def get_str_from_queue(q):
+        result = ""
+        try:
+            while True:
+                result += q.get(timeout=0)
+        except queue.Empty:
+            pass
+
+        return result
 
 
 if __name__ == '__main__':
