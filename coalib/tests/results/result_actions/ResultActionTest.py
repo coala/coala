cc977137227cd5cfa9ca84a6829b84dffd6548a6
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-24 18:24:15 +0100

    result_actions: Add ResultAction

---
+++ coalib/tests/results/result_actions/ResultActionTest.py
@@ -0,0 +1,27 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.results.result_actions.ResultAction import ResultAction
+from coalib.settings.Section import Section
+
+
+class ResultActionTestCase(unittest.TestCase):
+    def test_api(self):
+        uut = ResultAction()
+
+        self.assertRaises(NotImplementedError, uut.apply, 5, {}, {})
+        self.assertRaises(NotImplementedError, uut.apply_from_section, "", {}, {}, Section("name"))
+
+        self.assertRaises(TypeError, uut.apply_from_section, "", {}, {}, 5)
+        self.assertRaises(TypeError, uut.apply_from_section, "", 5, {}, Section("name"))
+        self.assertRaises(TypeError, uut.apply_from_section, "", {}, 5, Section("name"))
+
+        self.assertFalse(uut.is_applicable(5))
+
+        self.assertEqual(len(uut.get_metadata().non_optional_params), 0)
+        self.assertEqual(len(uut.get_metadata().optional_params), 0)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
