c2d0bae467201d0fda0283aa27fa52d1423b9d0e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-24 07:51:40 +0100

    results: Add Diff
    This object will be used to create a patchresult which represents a
    change to several files.

---
+++ coalib/results/Diff.py
@@ -0,0 +1,85 @@
+"""
+A Diff result represents a difference for one file.
+"""
+from coalib.results.LineDiff import LineDiff
+
+
+class ConflictError(Exception):
+    pass
+
+
+class Diff:
+    def __init__(self):
+        self._changes = {}
+
+    def _get_change(self, line_nr, min_line=1):
+        if not isinstance(line_nr, int):
+            raise TypeError("line_nr needs to be an integer.")
+        if line_nr < min_line:
+            raise ValueError("The given line number is not allowed.")
+
+        return self._changes.get(line_nr, LineDiff())
+
+    def apply(self, file):
+        """
+        Applies this diff to the given file.
+
+        :param file: A list of all lines in the file. (readlines) Will not be modified.
+        :return: The modified file.
+        """
+        result = []
+        current_line = 0
+
+        # Note that line_nr counts from _1_ although 0 is possible when inserting lines before everything
+        for line_nr in sorted(self._changes):
+            result.extend(file[current_line:max(line_nr-1, 0)])
+            linediff = self._changes[line_nr]
+            if not linediff.delete and not linediff.change and line_nr > 0:
+                result.append(file[line_nr-1])
+            elif linediff.change:
+                result.append(linediff.change[1])
+
+            if linediff.add_after:
+                result.extend(linediff.add_after)
+
+            current_line = line_nr
+
+        result.extend(file[current_line:])
+
+        return result
+
+    def delete_line(self, line_nr):
+        """
+        Mark the given line nr as deleted. The first line is line number 1.
+        """
+        linediff = self._get_change(line_nr)
+        linediff.delete = True
+        self._changes[line_nr] = linediff
+
+    def add_lines(self, line_nr_before, lines):
+        """
+        Adds lines after the given line number.
+
+        :param line_nr_before: Line number of the line before the additions. Use 0 for insert lines before everything.
+        :param lines: A list of lines to add.
+        """
+        if lines == []:
+            return  # No action
+
+        linediff = self._get_change(line_nr_before, min_line=0)
+        if linediff.add_after is not False:
+            raise ConflictError("Cannot add lines after the given line since there are already lines.")
+
+        linediff.add_after = lines
+        self._changes[line_nr_before] = linediff
+
+    def change_line(self, line_nr, original_line, replacement):
+        """
+        Changes the given line with the given line number. The replacement will be there instead.
+        """
+        linediff = self._get_change(line_nr)
+        if linediff.change is not False:
+            raise ConflictError("An already changed line cannot be changed.")
+
+        linediff.change = (original_line, replacement)
+        self._changes[line_nr] = linediff
---
+++ coalib/tests/results/DiffTest.py
@@ -0,0 +1,64 @@
+import sys
+
+sys.path.insert(0, ".")
+from coalib.results.Diff import Diff, ConflictError
+import unittest
+
+
+class DiffTestCase(unittest.TestCase):
+    def setUp(self):
+        self.uut = Diff()
+
+    def test_add_lines(self):
+        self.uut.add_lines(0, [])
+        self.uut.add_lines(0, ["t"])
+        self.uut.add_lines(0, [])
+        self.assertRaises(ConflictError, self.uut.add_lines, 0, ["t"])  # No double addition allowed
+        self.assertRaises(ValueError, self.uut.add_lines, -1, ["t"])
+        self.assertRaises(TypeError, self.uut.add_lines, "str", ["t"])
+
+    def test_delete_line(self):
+        self.uut.delete_line(1)
+        self.uut.delete_line(1)  # Double deletion possible without conflict
+        self.assertRaises(ValueError, self.uut.delete_line, 0)
+
+    def test_change_line(self):
+        self.uut.change_line(2, "1", "2")
+        self.assertRaises(ConflictError, self.uut.change_line, 2, "1", "3")
+        self.assertRaises(ValueError, self.uut.change_line, 0, "1", "2")
+
+        self.uut.delete_line(1)
+        self.assertRaises(AssertionError, self.uut.change_line, 1, "1", "2")  # Line was deleted, unchangeable
+
+    def test_apply(self):
+        file = ["1",
+                "2",
+                "3",
+                "4"]
+
+        result_file = ["0.1",
+                       "0.2",
+                       "1",
+                       "1.1",
+                       "3.changed",
+                       "4"]
+
+        self.uut.delete_line(2)
+        self.uut.add_lines(0, ["0.1", "0.2"])
+        self.uut.add_lines(1, ["1.1"])
+        self.uut.change_line(3, "3", "3.changed")
+        self.assertEqual(self.uut.apply(file), result_file)
+
+        self.uut.delete_line(len(file))
+        del result_file[len(result_file) - 1]
+        self.assertEqual(self.uut.apply(file), result_file)
+
+        self.uut.delete_line(1)
+        del result_file[2]
+        self.assertEqual(self.uut.apply(file), result_file)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
