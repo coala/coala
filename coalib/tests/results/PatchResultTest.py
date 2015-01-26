36d66a519dd476f2fdbcda72d7f8bc7e4940e7aa
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-26 22:23:54 +0100

    PatchResult: Add __add__

--- coalib/tests/results/PatchResultTest.py
+++ coalib/tests/results/PatchResultTest.py
@@ -10,6 +10,7 @@ class PatchResultTestCase(unittest.TestCase):
     def test_raises(self):
         self.assertRaises(TypeError, PatchResult, origin="test", message="test", diffs=5)
         self.assertRaises(TypeError, PatchResult("t", "t", {}).apply, 5)
+        self.assertRaises(TypeError, PatchResult("t", "t", {}).__add__, 5)
 
     def test_apply(self):
         file_dict = {
@@ -28,6 +29,36 @@ class PatchResultTestCase(unittest.TestCase):
         uut.apply(file_dict)
 
         self.assertEqual(file_dict, expected_file_dict)
+
+    def test_add(self):
+        file_dict = {
+            "f_a": ["1", "2", "3"],
+            "f_b": ["1", "2", "3"],
+            "f_c": ["1", "2", "3"]
+        }
+        expected_file_dict = {
+            "f_a": ["1", "3_changed"],
+            "f_b": ["1", "2", "3_changed"],
+            "f_c": ["1", "2", "3"]
+        }
+
+        diff = Diff()
+        diff.delete_line(2)
+        uut1 = PatchResult("origin", "msg", {"f_a": diff})
+
+        diff = Diff()
+        diff.change_line(3, "3", "3_changed")
+        uut2 = PatchResult("origin", "msg", {"f_a": diff})
+
+        diff = Diff()
+        diff.change_line(3, "3", "3_changed")
+        uut3 = PatchResult("origin", "msg", {"f_b": diff})
+
+        uut1 += uut2 + uut3
+        uut1.apply(file_dict)
+
+        self.assertEqual(file_dict, expected_file_dict)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
