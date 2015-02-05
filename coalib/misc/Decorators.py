d360739c648cdcb1477a26cbc276230e9f31ac93
Fabian Neuschmidt <coding@neuschmidt.de>
2015-02-05 16:56:32 +0100

    misc: Add arguments_to_lists decorator

--- coalib/misc/Decorators.py
+++ coalib/misc/Decorators.py
@@ -13,3 +13,39 @@ def cached_iterator(iterator):
                 yielded.append(item)
                 yield item
     return c_iterator
+
+
+def _to_list(var):
+    """
+    make variable to list
+    :param var: variable of any type
+    :return: list
+    """
+    if isinstance(var, list):
+        return var
+    elif var is None:
+        return []
+    elif isinstance(var, str) or isinstance(var, dict):
+        return [var]  # we can make a list out of this but be don't want to
+    else:
+        try:
+            return list(var)
+        except TypeError:
+            return [var]
+
+
+def arguments_to_lists(function):
+    """
+    Decorator for a function that converts all arguments to lists
+    :param function: target function
+    :return: target function with only lists as parameters
+    """
+    def l_function(*args, **kwargs):
+        l_args = [_to_list(arg) for arg in args]
+        l_kwargs = {}
+
+        for key, value in kwargs.items():
+            l_kwargs[key] = _to_list(value)
+        return function(*l_args, **l_kwargs)
+
+    return l_function
--- coalib/tests/misc/DecoratorsTest.py
+++ coalib/tests/misc/DecoratorsTest.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 
 sys.path.insert(0, ".")
-from coalib.misc.Decorators import cached_iterator
+from coalib.misc.Decorators import cached_iterator, arguments_to_lists
 
 
 class CachedIteratorTestCase(unittest.TestCase):
@@ -15,6 +15,18 @@ class CachedIteratorTestCase(unittest.TestCase):
         self.assertEqual(list(iterate_over_list([1, 1, 2, 2, 3, 3, 1, 4])),
                          [1, 2, 3, 4])
 
+
+class ArgumentsToListsTestCase(unittest.TestCase):
+    def test_arguments_to_lists(self):
+        @arguments_to_lists
+        def return_args(*args, **kwargs):
+            return args, kwargs
+
+        self.assertEqual(
+            return_args(None, True, 1, "", "AB", [1, 2], t=(3, 4), d={"a": 1}),
+            (([], [True], [1], [""], ["AB"], [1, 2]), {"t": [3, 4],
+                                                       "d": [{"a": 1}]
+                                                       }))
 
 if __name__ == '__main__':
     unittest.main()
