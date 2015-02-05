b992d9ee272e73c9025feb8d2e93c95a6541f8bb
Fabian Neuschmidt <coding@neuschmidt.de>
2015-02-05 19:22:15 +0100

    misc: Add arguments_to_lists decorator

--- coalib/misc/Decorators.py
+++ coalib/misc/Decorators.py
@@ -15,3 +15,42 @@ def unique_iterating(iterator):
                 yield item
 
     return unique_iterator
+
+
+def _to_list(var):
+    """
+    Make variable to list.
+
+    :param var: variable of any type
+    :return: list
+    """
+    if isinstance(var, list):
+        return var
+    elif var is None:
+        return []
+    elif isinstance(var, str) or isinstance(var, dict):
+        # We dont want to make a list out of those via the default constructor
+        return [var]
+    else:
+        try:
+            return list(var)
+        except TypeError:
+            return [var]
+
+
+def arguments_to_lists(function):
+    """
+    Decorator for a function that converts all arguments to lists.
+
+    :param function: target function
+    :return: target function with only lists as parameters
+    """
+    def l_function(*args, **kwargs):
+        l_args = [_to_list(arg) for arg in args]
+        l_kwargs = {}
+
+        for key, value in kwargs.items():
+            l_kwargs[key] = _to_list(value)
+        return function(*l_args, **l_kwargs)
+
+    return l_function
