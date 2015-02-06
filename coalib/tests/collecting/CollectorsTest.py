4175ebe6b4dce143788e0005c951b48ebb206cea
Fabian Neuschmidt <coding@neuschmidt.de>
2015-02-06 18:06:36 +0100

    collecting: Add collect_dirs

--- coalib/tests/collecting/CollectorsTest.py
+++ coalib/tests/collecting/CollectorsTest.py
@@ -5,24 +5,55 @@ import sys
 import unittest
 
 sys.path.insert(0, ".")
-from coalib.collecting.Collectors import collect_files
+from coalib.collecting.Collectors import collect_files, collect_dirs
 
 
-class ImportObjectsTestCase(unittest.TestCase):
+class CollectFilesTestCase(unittest.TestCase):
 
     def setUp(self):
         current_dir = os.path.split(inspect.getfile(inspect.currentframe()))[0]
-        self.collectors_test_dir = os.path.join(current_dir, "collectors_test_dir")
+        self.collectors_test_dir = os.path.join(current_dir,
+                                                "collectors_test_dir")
 
-    def test_empty(self):
+    def test_file_empty(self):
         self.assertRaises(TypeError, collect_files)
 
-    def test_invalid(self):
+    def test_file_invalid(self):
         self.assertEqual(collect_files(["invalid_path"]), [])
 
-    def test_collection(self):
-        self.assertEqual(collect_files([os.path.join(self.collectors_test_dir, "others", "*", "*2.py")]), [os.path.join(self.collectors_test_dir, "others", "py_files", "file2.py")])
+    def test_file_collection(self):
+        self.assertEqual(collect_files([os.path.join(self.collectors_test_dir,
+                                                     "others",
+                                                     "*",
+                                                     "*2.py")]),
+                         [os.path.join(self.collectors_test_dir,
+                                       "others",
+                                       "py_files",
+                                       "file2.py")])
 
+
+class CollectDirsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        current_dir = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+        self.collectors_test_dir = os.path.join(current_dir,
+                                                "collectors_test_dir")
+
+    def test_dir_empty(self):
+        self.assertRaises(TypeError, collect_dirs)
+
+    def test_dir_invalid(self):
+        self.assertEqual(collect_dirs(["invalid_path"]), [])
+
+    def test_dir_collection(self):
+        self.assertEqual(
+            sorted(collect_dirs([os.path.join(self.collectors_test_dir,
+                                              "**")])),
+            sorted([os.path.join(self.collectors_test_dir, "bears"),
+                os.path.join(self.collectors_test_dir, "others"),
+                os.path.join(self.collectors_test_dir, "others", "c_files"),
+                os.path.join(self.collectors_test_dir, "others", "py_files"),
+                self.collectors_test_dir]))
 
 
 if __name__ == '__main__':
