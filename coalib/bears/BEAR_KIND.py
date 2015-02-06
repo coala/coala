d2c2ca6c0ccd12e88b05cb03c3fd2a3550166a62
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-06 16:39:25 +0100

    BEAR_KIND: Convert to enum

--- coalib/bears/BEAR_KIND.py
+++ coalib/bears/BEAR_KIND.py
@@ -1,6 +1,3 @@
-class BEAR_KIND:
-    """
-    For more information on the kinds of the Bears look at the classes of them themselves.
-    """
-    LOCAL = 1
-    GLOBAL = 2
+from coalib.misc.Enum import enum
+
+BEAR_KIND = enum("LOCAL", "GLOBAL")
