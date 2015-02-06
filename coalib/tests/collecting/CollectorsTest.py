8aa54a01ad71e28a3d557b376e22eaf994f907d4
Fabian Neuschmidt <coding@neuschmidt.de>
2015-02-06 19:15:56 +0100

    collecting: Add collect_bears

--- coalib/tests/collecting/CollectorsTest.py
+++ coalib/tests/collecting/CollectorsTest.py
@@ -5,7 +5,9 @@ import sys
 import unittest
 
 sys.path.insert(0, ".")
-from coalib.collecting.Collectors import collect_files, collect_dirs
+from coalib.collecting.Collectors import collect_files, \
+                                         collect_dirs, \
+                                         collect_bears
 
 
 class CollectFilesTestCase(unittest.TestCase):
@@ -50,10 +52,51 @@ class CollectDirsTestCase(unittest.TestCase):
             sorted(collect_dirs([os.path.join(self.collectors_test_dir,
                                               "**")])),
             sorted([os.path.join(self.collectors_test_dir, "bears"),
+                os.path.join(self.collectors_test_dir, "bears", "__pycache__"),
                 os.path.join(self.collectors_test_dir, "others"),
                 os.path.join(self.collectors_test_dir, "others", "c_files"),
                 os.path.join(self.collectors_test_dir, "others", "py_files"),
                 self.collectors_test_dir]))
+
+
+class CollectBearsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        current_dir = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+        self.collectors_test_dir = os.path.join(current_dir,
+                                                "collectors_test_dir")
+
+    def test_bear_empty(self):
+        self.assertRaises(TypeError, collect_bears)
+
+    def test_bear_invalid(self):
+        self.assertEqual(collect_bears(["invalid_paths"],
+                                       ["invalid_name"],
+                                       ["invalid kind"]), [])
+
+    def test_simple_single(self):
+        self.assertEqual(len(collect_bears([os.path.join(
+            self.collectors_test_dir, "bears")],
+            ["bear1"],
+            ["kind"])), 1)
+
+    def test_reference_single(self):
+        self.assertEqual(len(collect_bears([os.path.join(
+            self.collectors_test_dir, "bears")],
+            ["metabear"],
+            ["kind"])), 1)
+
+    def test_no_duplications(self):
+        self.assertEqual(len(collect_bears([os.path.join(
+            self.collectors_test_dir, "bears", "**")],
+            ["*"],
+            ["kind"])), 2)
+
+    def test_wrong_kind(self):
+        self.assertEqual(len(collect_bears([os.path.join(
+            self.collectors_test_dir, "bears", "**")],
+            ["*"],
+            ["other_kind"])), 0)
 
 
 if __name__ == '__main__':
