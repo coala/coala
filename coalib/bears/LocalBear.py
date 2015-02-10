e8986c4ec149e2f1655d945b4d511487e3cba77e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-10 21:42:53 +0100

    Bear: Add get_dependencies

--- coalib/bears/LocalBear.py
+++ coalib/bears/LocalBear.py
@@ -38,4 +38,6 @@ class LocalBear(Bear):
 
     @classmethod
     def get_metadata(cls):
-        return FunctionMetadata.from_function(cls.run_bear, omit=["self", "filename", "file"])
+        return FunctionMetadata.from_function(
+            cls.run_bear,
+            omit=["self", "filename", "file", "dependency_results"])
