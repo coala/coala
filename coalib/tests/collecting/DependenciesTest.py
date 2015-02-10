543747b29f71ed6291c3d52c80c50e585e2f3689
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-10 21:18:24 +0100

    collecting: Add Dependencies
    We noch can resolve dependencies.

---
+++ coalib/tests/collecting/DependenciesTest.py
@@ -0,0 +1,65 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.bears.Bear import Bear
+from coalib.collecting import Dependencies
+
+
+class BearWithoutDeps(Bear):
+    @staticmethod
+    def get_dependencies():
+        return []
+
+
+class ResolvableBear1(Bear):
+    @staticmethod
+    def get_dependencies():
+        return [BearWithoutDeps]
+
+
+class ResolvableBear2(Bear):
+    @staticmethod
+    def get_dependencies():
+        return [ResolvableBear1, BearWithoutDeps]
+
+
+class UnresolvableBear1(Bear):
+    @staticmethod
+    def get_dependencies():
+        return [ResolvableBear1, BearWithoutDeps, UnresolvableBear3]
+
+
+class UnresolvableBear2(Bear):
+    @staticmethod
+    def get_dependencies():
+        return [ResolvableBear1, BearWithoutDeps, UnresolvableBear1]
+
+
+class UnresolvableBear3(Bear):
+    @staticmethod
+    def get_dependencies():
+        return [ResolvableBear1, BearWithoutDeps, UnresolvableBear2]
+
+
+class DependenciesTestCase(unittest.TestCase):
+    def test_no_deps(self):
+        self.assertEqual(
+            len(Dependencies.resolve([BearWithoutDeps,
+                                                   BearWithoutDeps])),
+            1)
+
+    def test_resolvable_deps(self):
+        self.assertEqual(Dependencies.resolve([ResolvableBear1,
+                                                            ResolvableBear2]),
+                         [BearWithoutDeps, ResolvableBear1, ResolvableBear2])
+
+    def test_unresolvable_deps(self):
+        self.assertRaises(
+            Dependencies.CircularDependencyError,
+            Dependencies.resolve,
+            [UnresolvableBear1])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
