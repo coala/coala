85ed7a93ae067c16d08aa4a93d14a19e51a1ad78
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-12 06:59:57 +0100

    LogPrinter: Strip lines to 79 chars

--- coalib/output/printers/LogPrinter.py
+++ coalib/output/printers/LogPrinter.py
@@ -9,11 +9,15 @@ from coalib.processes.communication.LogMessage import LogMessage
 class LogPrinter(Printer):
     def __init__(self, log_level=LOG_LEVEL.WARNING, timestamp_format="%X"):
         """
-        Creates a new log printer. The log printer adds logging capabilities to any normal printer, just derive from
-        this class and you should have this logging capabilities for free. (Note: LogPrinter itself is abstract.)
+        Creates a new log printer. The log printer adds logging capabilities to
+        any normal printer, just derive from this class and you should have
+        this logging capabilities for free. (Note: LogPrinter itself is
+        abstract.)
 
-        :param log_level: The minimum log level, everything below will not be logged.
-        :param timestamp_format: The format string for the datetime.today().strftime(format) method.
+        :param log_level: The minimum log level, everything below will not be
+        logged.
+        :param timestamp_format: The format string for the
+        datetime.today().strftime(format) method.
         """
         Printer.__init__(self)
 
@@ -28,28 +32,46 @@ class LogPrinter(Printer):
 
         return '[{}]{} '.format({LOG_LEVEL.DEBUG: _("DEBUG"),
                                  LOG_LEVEL.WARNING: _("WARNING"),
-                                 LOG_LEVEL.ERROR: _("ERROR")}.get(log_level, _("ERROR")),
+                                 LOG_LEVEL.ERROR: _("ERROR")}.get(log_level,
+                                                                  _("ERROR")),
                                 datetime_string)
 
     def debug(self, message, timestamp=None, **kwargs):
-        return self.log_message(LogMessage(LOG_LEVEL.DEBUG, message), timestamp=timestamp, **kwargs)
-
-    def warn(self, message, timestamp=None, **kwargs):
-        return self.log_message(LogMessage(LOG_LEVEL.WARNING, message), timestamp=timestamp, **kwargs)
-
-    def err(self, message, timestamp=None, **kwargs):
-        return self.log_message(LogMessage(LOG_LEVEL.ERROR, message), timestamp=timestamp, **kwargs)
-
-    def log(self, log_level, message, timestamp=None, **kwargs):
-        return self.log_message(LogMessage(log_level, message), timestamp=timestamp, **kwargs)
-
-    def log_exception(self, message, exception, log_level=LOG_LEVEL.ERROR, timestamp=None, **kwargs):
-        if not isinstance(exception, BaseException):
-            raise TypeError("log_exception can only log derivatives of BaseException.")
-
-        return self.log_message(LogMessage(log_level, message + "\n\n" + _("Exception was:") + "\n" + str(exception)),
+        return self.log_message(LogMessage(LOG_LEVEL.DEBUG, message),
                                 timestamp=timestamp,
                                 **kwargs)
+
+    def warn(self, message, timestamp=None, **kwargs):
+        return self.log_message(LogMessage(LOG_LEVEL.WARNING, message),
+                                timestamp=timestamp,
+                                **kwargs)
+
+    def err(self, message, timestamp=None, **kwargs):
+        return self.log_message(LogMessage(LOG_LEVEL.ERROR, message),
+                                timestamp=timestamp,
+                                **kwargs)
+
+    def log(self, log_level, message, timestamp=None, **kwargs):
+        return self.log_message(LogMessage(log_level, message),
+                                timestamp=timestamp,
+                                **kwargs)
+
+    def log_exception(self,
+                      message,
+                      exception,
+                      log_level=LOG_LEVEL.ERROR,
+                      timestamp=None,
+                      **kwargs):
+        if not isinstance(exception, BaseException):
+            raise TypeError("log_exception can only log derivatives of "
+                            "BaseException.")
+
+        return self.log_message(
+            LogMessage(log_level,
+                       message + "\n\n" + _("Exception was:") + "\n" +
+                       str(exception)),
+            timestamp=timestamp,
+            **kwargs)
 
     def log_message(self, log_message, timestamp=None, **kwargs):
         if not isinstance(log_message, LogMessage):
