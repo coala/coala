c6b6853ec7f88c6025e80d49cec9c20c77b42f0c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-16 20:01:37 +0100

    FunctionMetadata: Strip to 79 chars

--- coalib/settings/FunctionMetadata.py
+++ coalib/settings/FunctionMetadata.py
@@ -8,16 +8,23 @@ class FunctionMetadata:
     str_nodesc = _("No description given.")
     str_optional = _("Optional, defaults to '{}'.")
 
-    def __init__(self, name, desc="", retval_desc="", non_optional_params={}, optional_params={}):
+    def __init__(self,
+                 name,
+                 desc="",
+                 retval_desc="",
+                 non_optional_params={},
+                 optional_params={}):
         """
         Creates the FunctionMetadata object.
 
         :param name: The name of the function.
         :param desc: The description of the function.
         :param retval_desc: The retval description of the function.
-        :param non_optional_params: A dict containing the name of non optional parameters as the key and
-        a tuple of a description and the python annotation.
-        :param optional_params: A dict containing the name of optional parameters as the key and a tuple
+        :param non_optional_params: A dict containing the name of non optional
+        parameters as the key and a tuple of a description and the python
+        annotation.
+        :param optional_params: A dict containing the name of optional
+        parameters as the key and a tuple
         of a description, the python annotation and the default value.
         """
         if not isinstance(name, str):
@@ -39,17 +46,18 @@ class FunctionMetadata:
 
     def create_params_from_section(self, section):
         """
-        Create a params dictionary for this function that holds all values the function needs plus optional ones that
-        are available.
+        Create a params dictionary for this function that holds all values the
+        function needs plus optional ones that are available.
 
         :param section: The section to retrieve the values from.
         :return: A dictionary. Unfold it with ** to pass it to the function.
         """
-        # Import Section only as needed to avoid circular dependency with the Section class
+        # Import Section only as needed to avoid circular dependency
         from coalib.settings.Section import Section
 
         if not isinstance(section, Section):
-            raise TypeError("The 'section' parameter should be a coalib.settings.Section instance.")
+            raise TypeError("The 'section' parameter should be a "
+                            "coalib.settings.Section instance.")
 
         params = {}
 
@@ -71,14 +79,16 @@ class FunctionMetadata:
     @classmethod
     def from_function(cls, func, omit=[]):
         """
-        Creates a FunctionMetadata object from a function. Please note that any variable argument lists are not
-        supported. If you do not want the first (usual named 'self') argument to appear please pass the method of an
-        actual INSTANCE of a class; passing the method of the class isn't enough. Alternatively you can add "self" to
-        the omit list.
+        Creates a FunctionMetadata object from a function. Please note that any
+        variable argument lists are not supported. If you do not want the
+        first (usual named 'self') argument to appear please pass the method of
+        an actual INSTANCE of a class; passing the method of the class isn't
+        enough. Alternatively you can add "self" to the omit list.
 
         :param func: The function.
         :param omit: A list of parameter names that are to be ignored.
-        :return: The FunctionMetadata object corresponding to the given function.
+        :return: The FunctionMetadata object corresponding to the given
+        function.
         """
         if not isfunction(func) and not ismethod(func):
             raise TypeError("function has to be a function")
@@ -98,17 +108,21 @@ class FunctionMetadata:
         defaults = argspec.defaults if argspec.defaults is not None else ()
         num_non_defaults = len(args) - len(defaults)
         for i, arg in enumerate(args):
-            if (i < 1 and ismethod(func)) or arg in omit:  # Implicit self argument or omitted explicitly
+            # Implicit self argument or omitted explicitly
+            if (i < 1 and ismethod(func)) or arg in omit:
                 continue
 
             if i < num_non_defaults:
-                non_optional_params[arg] = (doc_comment.param_dict.get(arg, cls.str_nodesc),
-                                            argspec.annotations.get(arg, None))
+                non_optional_params[arg] = (
+                    doc_comment.param_dict.get(arg, cls.str_nodesc),
+                    argspec.annotations.get(arg, None))
             else:
-                optional_params[arg] = (doc_comment.param_dict.get(arg, cls.str_nodesc) + \
-                                        " (" + cls.str_optional.format(str(defaults[i-num_non_defaults])) + ")",
-                                        argspec.annotations.get(arg, None),
-                                        defaults[i-num_non_defaults])
+                optional_params[arg] = (
+                    doc_comment.param_dict.get(arg, cls.str_nodesc) + " (" +
+                    cls.str_optional.format(str(defaults[i-num_non_defaults]))
+                    + ")",
+                    argspec.annotations.get(arg, None),
+                    defaults[i-num_non_defaults])
 
         return cls(name=func.__name__,
                    desc=doc_comment.desc,
