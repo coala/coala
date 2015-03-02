00f8808cfdbb44dd99a819a7f3f24b32e454cc7c
Makman2 <mak95@arcor.de>
2015-03-02 02:37:47 +0100

    parsing: Add split() function
    Add the split() function that splits a string with a provided
    pattern while ignoring escapes. For that purpose add the
    StringProcessing module.
    
    Includes a full test set for this function.
    
    Partially fixes #160.

--- coalib/parsing/StringProcessing.py
+++ coalib/parsing/StringProcessing.py
@@ -52,3 +52,43 @@ def search_for(pattern, string, max_matches = 0, flags = 0):
         raise ValueError("Provided value for parameter 'max_matches' below "
                          "zero. Negative numbers are not allowed.")
 
+
+def split(pattern,
+          string,
+          max_split = 0,
+          remove_empty_matches = False):
+    """
+    Splits the given string by the specified pattern. The return character (\n)
+    is not a natural split pattern (if you don't specify it yourself).
+    This function ignores escape sequences.
+
+    :param pattern:              A regex pattern that defines where to split.
+    :param string:               The string to split by the defined pattern.
+    :param max_split:            Defines the maximum number of splits. If 0 is
+                                 provided, unlimited splits are made.
+    :param remove_empty_matches: Defines whether empty entries should
+                                 be removed from the resulting list.
+    :raises ValueError:          Raised when a negative number is provided for
+                                 max_split.
+    :return:                     An iterator returning the split up strings.
+    """
+    # re.split() is not usable for this function. It has a bug when using too
+    # many capturing groups "()".
+    matches = search_for(r"(.*?)(?:" + pattern + r")",
+                         string,
+                         max_split,
+                         re.DOTALL)
+
+    for item in matches:
+        if not remove_empty_matches or len(item.group(1)) != 0:
+            # Return the first matching group. The pattern from parameter can't
+            # change the group order.
+            yield item.group(1)
+
+    last_pos = item.end()
+
+    # Append the rest of the string, since it's not in the result list (only
+    # matches are captured that have a leading separator).
+    if not remove_empty_matches or len(string[last_pos : ]) != 0:
+        yield string[last_pos : ]
+
