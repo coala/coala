1cee0b92e180ea9033ff0bb2bc317108b369f60d
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-03-18 23:07:02 +0100

    FilePrinter: Strip to 79 chars

--- coalib/output/printers/FilePrinter.py
+++ coalib/output/printers/FilePrinter.py
@@ -4,12 +4,16 @@ from coalib.output.printers.LOG_LEVEL import LOG_LEVEL
 
 class FilePrinter(LogPrinter):
     """
-    This is a simple printer/logprinter that prints everything to a file. Note that everything will be appended.
+    This is a simple printer/logprinter that prints everything to a file. Note
+    that everything will be appended.
     """
-    def __init__(self, filename, log_level=LOG_LEVEL.WARNING, timestamp_format="%X"):
+    def __init__(self,
+                 filename,
+                 log_level=LOG_LEVEL.WARNING,
+                 timestamp_format="%X"):
         """
-        Creates a new FilePrinter. If the directory of the given file doesn't exist or if there's any access problems,
-        an exception will be thrown.
+        Creates a new FilePrinter. If the directory of the given file doesn't
+        exist or if there's any access problems, an exception will be thrown.
 
         :param filename: the name of the file to put the data into (string).
         """
@@ -17,7 +21,9 @@ class FilePrinter(LogPrinter):
         if not isinstance(filename, str):
             raise TypeError("filename must be a string.")
 
-        LogPrinter.__init__(self, timestamp_format=timestamp_format, log_level=log_level)
+        LogPrinter.__init__(self,
+                            timestamp_format=timestamp_format,
+                            log_level=log_level)
 
         self.file = open(filename, 'a+')
 
