123e891404706b368de3b07dee4e75c6a4923c74
Makman2 <mak95@arcor.de>
2015-03-21 19:20:29 +0100

    StringProcessing: Add unescaped_search_for()
    Possibility to match only occurrences that are unescaped. Already implements
    the use_regex parameter.
    Includes a complete test set.
    
    Fixes https://github.com/coala-analyzer/coala/issues/394.

--- coalib/parsing/StringProcessing.py
+++ coalib/parsing/StringProcessing.py
@@ -21,6 +21,39 @@ def search_for(pattern, string, flags=0, max_match=0, use_regex=False):
         yield elem
 
 
+def unescaped_search_for(pattern,
+                         string,
+                         flags=0,
+                         max_match=0,
+                         use_regex=False):
+    """
+    Searches for a given pattern in a string that is not escaped.
+
+    :param pattern:   A pattern that defines what to match unescaped.
+    :param string:    The string to search in.
+    :param flags:     Additional flags to pass to the regex processor.
+    :param max_match: Defines the maximum number of matches to perform. If 0 or
+                      less is provided, the number of splits is not limited.
+    :param use_regex: Specifies whether to treat the pattern as a regex or
+                      simple string.
+    :return:          An iterator returning MatchObject's. Every match object
+                      contains in its first group the backslashes that were
+                      consumed by the internal regex and so don't escape your
+                      pattern.
+    """
+    if not use_regex:
+        pattern = re.escape(pattern)
+
+    # Regex explanation:
+    # 1. (?<!\\)((?:\\\\)*) Unescaping sequence. Only matches backslashes if
+    #                       their count is even.
+    # 2. (?:pattern)        A non-capturing group that matches the pattern.
+    regex = r"(?<!\\)((?:\\\\)*)(?:" + pattern + r")"
+
+    for elem in search_for(regex, string, flags, max_match, True):
+        yield elem
+
+
 def limit(iterator, count):
     """
     A filter that removes all elements behind the set limit.
