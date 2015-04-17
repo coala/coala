89d4c1f05b097f4d8a264a250e0a979bf4bc04c5
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-17 12:50:14 +0200

    bears: Add codeclone_detection package

---
+++ bears/codeclone_detection/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains a bear and utilities to allow easy to use code clone
+detection.
+
+It is based on count matrix based code clone detection.
+
+See http://goo.gl/8UuAW5
+"""
