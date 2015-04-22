5b3d79f45e1d9d9edb83e7446e94fd69cc9ca906
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-22 12:57:16 +0200

    run_tests: Ignore coalib when testing bears only
    We don't need to ignore bears when testing coalib only because the
    coalib doesn't use the bears (while they obviously use the coalib).

--- run_tests.py
+++ run_tests.py
@@ -20,13 +20,6 @@ if __name__ == '__main__':
 
     testhelper = TestHelper(parser)
 
-    if not testhelper.args.ignore_main_tests:
-        testhelper.add_test_files(os.path.abspath(os.path.join("coalib",
-                                                               "tests")))
-    if not testhelper.args.ignore_bear_tests:
-        testhelper.add_test_files(os.path.abspath(os.path.join("bears",
-                                                               "tests")))
-
     ignore_list = [
         os.path.join(tempfile.gettempdir(), "**"),
         os.path.join(os.path.dirname(get_python_lib()), "**"),
@@ -34,5 +27,15 @@ if __name__ == '__main__':
         os.path.join("coalib", "bearlib", "parsing", "clang", "**"),
         os.path.join("bears", "tests", "**")
     ]
+
+    if not testhelper.args.ignore_main_tests:
+        testhelper.add_test_files(os.path.abspath(os.path.join("coalib",
+                                                               "tests")))
+    else:
+        ignore_list.append(os.path.join("coalib", "**"))
+
+    if not testhelper.args.ignore_bear_tests:
+        testhelper.add_test_files(os.path.abspath(os.path.join("bears",
+                                                               "tests")))
 
     exit(testhelper.run_tests(ignore_list))
