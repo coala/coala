96dcfea92f09fba21e831c1f7cc53ee2536ad94f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-29 09:37:47 +0200

    ConsolePrinter: Adapt to codestyle

--- coalib/output/printers/ConsolePrinter.py
+++ coalib/output/printers/ConsolePrinter.py
@@ -20,16 +20,23 @@ class ConsolePrinter(LogPrinter, ColorPrinter):
 
     def _print_colored(self, output, color=None, **kwargs):
         color_code_dict = {
-            'black': '0;30', 'bright gray': '0;37',
-            'blue': '0;34', 'white': '1;37',
-            'green': '0;32', 'bright blue': '1;34',
-            'cyan': '0;36', 'bright green': '1;32',
-            'red': '0;31', 'bright cyan': '1;36',
-            'purple': '0;35', 'bright red': '1;31',
-            'yellow': '0;33', 'bright purple': '1;35',
-            'dark gray': '1;30', 'bright yellow': '1;33',
-            'normal': '0',
-        }
+            'black': '0;30',
+            'bright gray': '0;37',
+            'blue': '0;34',
+            'white': '1;37',
+            'green': '0;32',
+            'bright blue': '1;34',
+            'cyan': '0;36',
+            'bright green': '1;32',
+            'red': '0;31',
+            'bright cyan': '1;36',
+            'purple': '0;35',
+            'bright red': '1;31',
+            'yellow': '0;33',
+            'bright purple': '1;35',
+            'dark gray': '1;30',
+            'bright yellow': '1;33',
+            'normal': '0'}
         color_code = color_code_dict.get(color, None)
         if color_code is None:
             raise ValueError("Invalid color value")
