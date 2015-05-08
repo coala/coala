b3603aed6dce8c7e44ea97d29eea73ee807144b2
Udayan Tandon <udayan12167@iiitd.ac.in>
2015-05-08 17:11:48 +0530

    Gui: Add tab switcher in header

--- gui/src/workspace/coalaWindow.py
+++ gui/src/workspace/coalaWindow.py
@@ -14,4 +14,7 @@ class coalaWindow(Gtk.ApplicationWindow):
         self.header_bar = self._ui.get_object("header-bar")
         self.set_titlebar(self.header_bar)
 
+        self.window_stack = self._ui.get_object("coalaWindowStack")
+        self.add(self.window_stack)
+
         self.show_all()
