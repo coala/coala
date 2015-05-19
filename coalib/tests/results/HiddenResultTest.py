f6e893f20548aec8604b8cd1478064cb45b117bd
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-19 12:32:37 +0200

    results: Add HiddenResult

---
+++ coalib/tests/results/HiddenResultTest.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.results.HiddenResult import HiddenResult
+
+
+class HiddenResultTest(unittest.TestCase):
+    def test_hidden_result(self):
+        uut = HiddenResult("any", "anything")
+        self.assertEqual(uut.contents, "anything")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
