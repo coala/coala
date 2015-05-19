32d6ce5777e7483c4e37b75c7ead993a37614122
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-19 12:16:49 +0200

    Clang clone detection: Modularize bear
    This makes it easy to get all the differences which will help me
    analyzing several scenarios.

--- bears/tests/codeclone_detection/ClangCloneDetectionBearTest.py
+++ bears/tests/codeclone_detection/ClangCloneDetectionBearTest.py
@@ -6,6 +6,7 @@ from queue import Queue
 
 sys.path.insert(0, ".")
 
+from bears.codeclone_detection.ClangSimilarityBear import ClangSimilarityBear
 from bears.codeclone_detection.ClangCloneDetectionBear import \
     ClangCloneDetectionBear
 from coalib.bearlib.parsing.clang.cindex import Index, LibclangError
@@ -39,10 +40,14 @@ class ClangCloneDetectionBearTest(unittest.TestCase):
         self.clone_files = [os.listdir(os.path.join(self.base_test_path,
                                                     "clones"))]
 
+    def test_dependencies(self):
+        self.assertIn(ClangSimilarityBear,
+                      ClangCloneDetectionBear.get_dependencies())
+
     def test_invalid_conditions(self):
         self.section.append(Setting("condition_list", "bullshit"))
 
-        self.uut = ClangCloneDetectionBear({}, self.section, Queue())
+        self.uut = ClangSimilarityBear({}, self.section, Queue())
         self.assertEqual(self.uut.run_bear_from_section([], {}), None)
 
     def test_non_clones(self):
@@ -51,11 +56,9 @@ class ClangCloneDetectionBearTest(unittest.TestCase):
             for elem in os.listdir(os.path.join(self.base_test_path,
                                                 "non_clones"))]
 
-        for file in self.non_clone_files:
-            self.uut = ClangCloneDetectionBear({file: ""},
-                                               self.section,
-                                               Queue())
-            self.assertEqual(self.uut.run_bear_from_section([], {}), [])
+        self.check_clone_detection_bear(self.non_clone_files,
+                                        lambda results:
+                                        self.assertEqual(results, []))
 
     def test_clones(self):
         self.clone_files = [
@@ -63,11 +66,32 @@ class ClangCloneDetectionBearTest(unittest.TestCase):
             for elem in os.listdir(os.path.join(self.base_test_path,
                                                 "clones"))]
 
-        for file in self.clone_files:
-            self.uut = ClangCloneDetectionBear({file: ""},
-                                               self.section,
-                                               Queue())
-            self.assertNotEqual(self.uut.run_bear_from_section([], {}), [])
+        self.check_clone_detection_bear(self.clone_files,
+                                        lambda results:
+                                        self.assertNotEqual(results, []))
+
+    def check_clone_detection_bear(self, files, result_check_function):
+        """
+        Checks the results of the CloneDetectionBear with the given function.
+
+        :param files:                 The files to check. Each will be checked
+                                      on its own.
+        :param result_check_function: A function yielding an exception if the
+                                      results are invalid.
+        """
+        for file in files:
+            similarity_results = ClangSimilarityBear(
+                {file: ""},
+                self.section,
+                Queue()).run_bear_from_section([], {})
+            uut = ClangCloneDetectionBear(
+                {file: ""},
+                self.section,
+                Queue())
+            arg_dict = {"dependency_results":
+                        {"ClangSimilarityBear": similarity_results}}
+
+            result_check_function(uut.run_bear_from_section([], arg_dict))
 
 
 def skip_test():
