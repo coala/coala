dfeb6324f14b93e58cde5d65542479abb9b27a92
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-21 12:36:03 +0200

    codeclone_detection: Add ClangCloneDetectionBear

---
+++ bears/codeclone_detection/ClangCloneDetectionBear.py
@@ -0,0 +1,42 @@
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.results.Result import Result
+from coalib.bears.GlobalBear import GlobalBear
+from coalib.misc.i18n import _
+from bears.codeclone_detection.ClangSimilarityBear import ClangSimilarityBear
+
+
+class ClangCloneDetectionBear(GlobalBear):
+    def run(self,
+            dependency_results: dict,
+            max_clone_difference: float=0.2):
+        '''
+        Checks the given code for similar functions that are probably
+        redundant.
+
+        :param max_clone_difference: The maximum difference a clone should
+                                     have.
+        '''
+        differences = dependency_results["ClangSimilarityBear"][0].contents
+
+        self.debug("Creating results...")
+        results = []
+        for function_1, function_2, difference in differences:
+            if difference < max_clone_difference:
+                results.append(Result(
+                    self.__class__.__name__,
+                    _("Code clone found. The other occurrence is at file "
+                      "{file}, line {line}, function {function}. The "
+                      "similarity is {similarity}.").format(
+                        file=function_2[0],
+                        line=function_2[1],
+                        function=function_2[2],
+                        similarity=1-difference),
+                    file=function_1[0],
+                    severity=RESULT_SEVERITY.MAJOR,
+                    line_nr=function_1[1]))
+
+        return results
+
+    @staticmethod
+    def get_dependencies():
+        return [ClangSimilarityBear]
---
+++ bears/tests/codeclone_detection/ClangCloneDetectionBearTest.py
@@ -0,0 +1,105 @@
+import sys
+import unittest
+import os
+import inspect
+from queue import Queue
+
+sys.path.insert(0, ".")
+
+from bears.codeclone_detection.ClangSimilarityBear import ClangSimilarityBear
+from bears.codeclone_detection.ClangCloneDetectionBear import \
+    ClangCloneDetectionBear
+from coalib.bearlib.parsing.clang.cindex import Index, LibclangError
+from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
+
+
+class ClangCloneDetectionBearTest(unittest.TestCase):
+    def setUp(self):
+        self.base_test_path = os.path.abspath(os.path.join(
+            os.path.dirname(inspect.getfile(ClangCloneDetectionBearTest)),
+            "clone_detection_samples"))
+        self.section = Section("default")
+        self.section.append(Setting("condition_list",
+                                    "returned, "
+                                    "is_condition, "
+                                    "in_condition, "
+                                    "in_second_level_condition, "
+                                    "in_third_level_condition, "
+                                    "is_assignee, "
+                                    "is_assigner, "
+                                    "loop_content, "
+                                    "second_level_loop_content, "
+                                    "third_level_loop_content, "
+                                    "is_param, "
+                                    "in_sum, "
+                                    "in_product, "
+                                    "in_binary_operation,"
+                                    "member_accessed"))
+        self.clone_files = [os.listdir(os.path.join(self.base_test_path,
+                                                    "clones"))]
+
+    def test_dependencies(self):
+        self.assertIn(ClangSimilarityBear,
+                      ClangCloneDetectionBear.get_dependencies())
+
+    def test_invalid_conditions(self):
+        self.section.append(Setting("condition_list", "bullshit"))
+
+        self.uut = ClangSimilarityBear({}, self.section, Queue())
+        self.assertEqual(self.uut.run_bear_from_section([], {}), None)
+
+    def test_non_clones(self):
+        self.non_clone_files = [
+            os.path.join(self.base_test_path, "non_clones", elem)
+            for elem in os.listdir(os.path.join(self.base_test_path,
+                                                "non_clones"))]
+
+        self.check_clone_detection_bear(self.non_clone_files,
+                                        lambda results:
+                                        self.assertEqual(results, []))
+
+    def test_clones(self):
+        self.clone_files = [
+            os.path.join(self.base_test_path, "clones", elem)
+            for elem in os.listdir(os.path.join(self.base_test_path,
+                                                "clones"))]
+
+        self.check_clone_detection_bear(self.clone_files,
+                                        lambda results:
+                                        self.assertNotEqual(results, []))
+
+    def check_clone_detection_bear(self, files, result_check_function):
+        """
+        Checks the results of the CloneDetectionBear with the given function.
+
+        :param files:                 The files to check. Each will be checked
+                                      on its own.
+        :param result_check_function: A function yielding an exception if the
+                                      results are invalid.
+        """
+        for file in files:
+            similarity_results = ClangSimilarityBear(
+                {file: ""},
+                self.section,
+                Queue()).run_bear_from_section([], {})
+            uut = ClangCloneDetectionBear(
+                {file: ""},
+                self.section,
+                Queue())
+            arg_dict = {"dependency_results":
+                        {"ClangSimilarityBear": similarity_results}}
+
+            result_check_function(uut.run_bear_from_section([], arg_dict))
+
+
+def skip_test():
+    try:
+        Index.create()
+        return False
+    except LibclangError as error:
+        return str(error)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
