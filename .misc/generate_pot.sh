1184283121fe123b5b860e6071f5f09ea2045df0
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-24 17:10:46 +0200

    generate_pot: Add metainformation

--- .misc/generate_pot.sh
+++ .misc/generate_pot.sh
@@ -7,11 +7,11 @@ cat `find . -name "*.py" \
  | grep -v build` coala \
   | python3 .misc/.extract_doc_translations > doc_strings
 xgettext -j -c --language=Python --output=locale/coala.pot --keyword=_ \
- --keyword=N_ --from-code=UTF-8 \
+ --keyword=N_ --from-code=UTF-8 --package-name=coala --package-version=0.2 \
+ --msgid-bugs-address=lasse.schuirmann@gmail.com \
+ --copyright-holder="The coala authors" \
  `find . -name "*.py" | grep -v build` coala doc_strings
 
 echo Removing temporary files...
 rm doc_strings
 echo Done.
-echo
-echo Please update the po files accordingly.
