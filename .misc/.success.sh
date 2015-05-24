5d335c961e1a736188a33d71227145773ff3627a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-24 15:41:59 +0200

    travis: Send pot to zanata on master
    This will automatically update our web translation UI on zanata on every
    push to the master branch.
    
    Fixes https://github.com/coala-analyzer/coala/issues/214

--- .misc/.success.sh
+++ .misc/.success.sh
@@ -1,4 +1,11 @@
 if python --version | grep 3\.4 ; then
   coveralls
   codecov
+
+  if [ "$TRAVIS_BRANCH" = "master" ] ; then
+    cd .misc
+    wget http://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/3.6.0/zanata-cli-3.6.0-dist.zip -O dist.zip
+    unzip dist.zip
+    zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_API --username sils --url https://translate.zanata.org/zanata/
+  fi
 fi
