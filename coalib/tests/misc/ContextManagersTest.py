392caa3f065ba6c7948ce8459c93b02b1ebe9c5b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-25 22:16:19 +0200

    ContextManagers: Add simulate_console_inputs
    This will be used to simplify the ConsoleInteractorTest in the following
    commit.

--- coalib/tests/misc/ContextManagersTest.py
+++ coalib/tests/misc/ContextManagersTest.py
@@ -2,10 +2,12 @@ import unittest
 import sys
 
 sys.path.insert(0, ".")
-from coalib.misc.ContextManagers import suppress_stdout, retrieve_stdout
+from coalib.misc.ContextManagers import (suppress_stdout,
+                                         retrieve_stdout,
+                                         simulate_console_inputs)
 
 
-class SuppressStdoutTest(unittest.TestCase):
+class ContextManagersTest(unittest.TestCase):
     def test_suppress_stdout(self):
         def print_func():
             print("func")
@@ -24,12 +26,20 @@ class SuppressStdoutTest(unittest.TestCase):
 
         sys.stdout = old_stdout
 
-
-class RetrieveStdoutTest(unittest.TestCase):
     def test_retrieve_stdout(self):
         with retrieve_stdout() as sio:
             print("test")
             self.assertEqual(sio.getvalue(), "test\n")
+
+    def test_simulate_console_inputs(self):
+        with simulate_console_inputs(0, 1, 2) as generator:
+            self.assertEqual(input(), 0)
+            self.assertEqual(generator.last_input, 0)
+            generator.inputs.append(3)
+            self.assertEqual(input(), 1)
+            self.assertEqual(input(), 2)
+            self.assertEqual(input(), 3)
+            self.assertEqual(generator.last_input, 3)
 
 
 if __name__ == '__main__':
