0253cf990cbf927a53f7314273a2fe7b873cc652
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-29 20:36:12 +0200

    SectionFiller: Make it a function
    The file will be renamed soon.

--- coalib/tests/settings/SectionFillerTest.py
+++ coalib/tests/settings/SectionFillerTest.py
@@ -5,7 +5,8 @@ sys.path.insert(0, ".")
 from coalib.misc.ContextManagers import simulate_console_inputs
 from coalib.bears.GlobalBear import GlobalBear
 from coalib.bears.LocalBear import LocalBear
-from coalib.settings.SectionFiller import SectionFiller, Section, Setting
+from coalib.settings.SectionFiller import fill_section, Setting
+from coalib.settings.Section import Section
 from coalib.output.ConsoleInteractor import ConsoleInteractor
 from coalib.output.printers.ConsolePrinter import ConsolePrinter
 
@@ -34,31 +35,19 @@ class SectionFillerTest(unittest.TestCase):
     def setUp(self):
         self.log_printer = ConsolePrinter()
         self.interactor = ConsoleInteractor(self.log_printer)
-        section = Section("test")
-        section.append(Setting("key", "val"))
-        self.uut = SectionFiller(section,
-                                 self.interactor,
-                                 self.log_printer)
-
-    def test_raises(self):
-        # Construction
-        self.assertRaises(TypeError,
-                          SectionFiller,
-                          0,
-                          self.interactor,
-                          self.log_printer)
-
-        # Fill section
-        self.assertRaises(TypeError, self.uut.fill_section, 0)
+        self.section = Section("test")
+        self.section.append(Setting("key", "val"))
 
     def test_fill_section(self):
         # Use the same value for both because order isn't predictable (uses
         # dict)
         with simulate_console_inputs(0, 0):
-            new_section = self.uut.fill_section([LocalTestBear,
-                                                 GlobalTestBear,
-                                                 "an inappropriate string "
-                                                 "object here"])
+            new_section = fill_section(self.section,
+                                       self.interactor,
+                                       self.log_printer,
+                                       [LocalTestBear,
+                                        GlobalTestBear,
+                                        "an inappropriate string object here"])
 
         self.assertEqual(int(new_section["local name"]), 0)
         self.assertEqual(int(new_section["global name"]), 0)
@@ -66,7 +55,10 @@ class SectionFillerTest(unittest.TestCase):
         self.assertEqual(len(new_section.contents), 3)
 
         # Shouldnt change anything the second time
-        new_section = self.uut.fill_section([LocalTestBear, GlobalTestBear])
+        new_section = fill_section(self.section,
+                                   self.interactor,
+                                   self.log_printer,
+                                   [LocalTestBear, GlobalTestBear])
 
         self.assertTrue("local name" in new_section)
         self.assertTrue("global name" in new_section)
