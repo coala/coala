70884b01af76b8f2714bfdf7285748f2d8fdee10
Makman2 <mak95@arcor.de>
2015-05-30 22:32:35 +0200

    StringProcessingTest: Move out tested function
    For function position_is_escaped().
    
    This was the last tested function in the base class
    StringProcessingTest. So no tests get shadow-executed in each separate
    test for a StringProcessing function.

--- coalib/tests/parsing/StringProcessingTest.py
+++ coalib/tests/parsing/StringProcessingTest.py
@@ -2,8 +2,6 @@ import sys
 sys.path.insert(0, ".")
 import unittest
 
-from coalib.parsing.StringProcessing import position_is_escaped
-
 
 class StringProcessingTest(unittest.TestCase):
     def setUp(self):
@@ -114,63 +112,6 @@ class StringProcessingTest(unittest.TestCase):
 
         self.assertRaises(StopIteration, next, iterator2)
 
-    # Test the position_is_escaped() function with a basic test string.
-    def test_position_is_escaped(self):
-        test_string = r"\\\\\abcabccba###\\13q4ujsabbc\+'**'ac###.#.####-ba"
-        result_dict = {
-            0: False,
-            1: True,
-            2: False,
-            3: True,
-            4: False,
-            5: True,
-            6: False,
-            7: False,
-            17: False,
-            18: True,
-            19: False,
-            30: False,
-            31: True,
-            50: False,
-            51: False,
-            6666666: False,
-            -1: False,
-            -20: True,
-            -21: False}
-        for position, value in result_dict.items():
-            self.assertEqual(position_is_escaped(test_string, position), value)
-
-    # Test the position_is_escaped() with more test strings from this class.
-    def test_position_is_escaped_advanced(self):
-        expected_results = [
-            30 * [False] + [True] + 7 * [False],
-            30 * [False] + [True] + 7 * [False],
-            30 * [False] + [True] + 7 * [False],
-            28 * [False] + [True, False, True] + 7 * [False],
-            31 * [False] + [True] + 6 * [False],
-            31 * [False] + [True] + 6 * [False],
-            38 * [False],
-            6 * [False] + [True] + 31 * [False],
-            6 * [False] + [True, False, True] + 29 * [False],
-            6 * [False] + [True] + 31 * [False],
-            6 * [False] + [True, False, True] + 29 * [False],
-            14 * [False] + [True] + 23 * [False],
-            12 * [False] + [True, False, True] + 23 * [False],
-            38 * [False],
-            [],
-            14 * [False],
-            [False],
-            [False, True]]
-
-        results = [[position_is_escaped(test_string, i)
-                    for i in range(len(test_string))]
-                   for test_string in self.test_strings]
-
-        zipped = [zip(x, y) for x, y in zip(expected_results, results)]
-        flattened = [item for sublist in zipped for item in sublist]
-
-        for elem in flattened:
-            self.assertEqual(elem[0], elem[1])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
