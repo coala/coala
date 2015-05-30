f610666e750e12ade401f1fe84c226367aa0b1e2
Makman2 <mak95@arcor.de>
2015-05-30 22:19:43 +0200

    StringProcessingTest: Move out test for function
    For function unescaped_search_for().
    
    Since the new test file uses StringProcessingTest as a base, all base
    tests are also shadow-executed. Later commits will move out other
    tests from StringProcessingTest, until no tests are executed and this
    class behaves as a pure test functionality base class.

--- coalib/tests/parsing/StringProcessingTest.py
+++ coalib/tests/parsing/StringProcessingTest.py
@@ -2,7 +2,6 @@ import sys
 sys.path.insert(0, ".")
 import unittest
 
-from coalib.parsing.StringProcessing import unescaped_search_for
 from coalib.parsing.StringProcessing import split
 from coalib.parsing.StringProcessing import unescaped_split
 from coalib.parsing.StringProcessing import search_in_between
@@ -67,111 +66,11 @@ class StringProcessingTest(unittest.TestCase):
             r"abc;a;;;;;asc"]
 
         # Set up test dependent variables.
-        self.set_up_unescaped_search_for()
         self.set_up_split()
         self.set_up_unescaped_split()
         self.set_up_search_in_between()
         self.set_up_unescaped_search_in_between()
         self.set_up_unescape()
-
-    def set_up_unescaped_search_for(self):
-        # Match either "out1" or "out2".
-        self.test_unescaped_search_for_pattern = "out1|out2"
-        # These are the expected results for the zero-group of the
-        # returned MatchObject's.
-        self.test_unescaped_search_for_expected_results = [
-            [r"out1", r"out2"],
-            [r"out1", r"out2"],
-            [r"out1", r"out2"],
-            [r"out1", r"out2"],
-            [r"out1", r"out2"],
-            [r"out1", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2", r"out2"],
-            [r"out1", r"out2"],
-            [],
-            [r"out1", r"out2"],
-            [],
-            []]
-
-        self.test_unescaped_search_for_simple_pattern_pattern = "'"
-        # Don't forget to prepend consumed escape characters.
-        self.test_unescaped_search_for_simple_pattern_expected_results = [
-            2 * [r"'"],
-            2 * [r"'"],
-            2 * [r"'"],
-            2 * [r"'"],
-            2 * [r"'"],
-            [r"'", r"\\'"],
-            4 * [r"'"],
-            4 * [r"'"],
-            4 * [r"'"],
-            4 * [r"'"],
-            4 * [r"'"],
-            [r"\\'"] + 3 * [r"'"],
-            [r"\\\\'"] + 3 * [r"'"],
-            6 * [r"'"],
-            [],
-            [],
-            [],
-            []]
-
-        # Since an empty pattern can also be escaped, the result contains
-        # special cases. Especially we check the completely matched string (and
-        # not only the matched pattern itself) we need to place also the
-        # matched escape characters inside the result list consumed from the
-        # internal regex of unescaped_search_for().
-        self.test_unescaped_search_for_empty_pattern_expected_results = [
-            29 * [r""] + [2 * self.bs] + 7 * [r""],
-            38 * [r""],
-            38 * [r""],
-            27 * [r""] + [4 * self.bs] + 7 * [r""],
-            38 * [r""],
-            30 * [r""] + [2 * self.bs] + 6 * [r""],
-            39 * [r""],
-            38 * [r""],
-            5 * [r""] + [2 * self.bs] + 30 * [r""],
-            5 * [r""] + [2 * self.bs] + 31 * [r""],
-            5 * [r""] + [4 * self.bs] + 29 * [r""],
-            13 * [r""] + [2 * self.bs] + 23 * [r""],
-            11 * [r""] + [4 * self.bs] + 23 * [r""],
-            39 * [r""],
-            [r""],
-            15 * [r""],
-            [r""],
-            [2 * self.bs]]
-
-        self.test_unescaped_search_for_max_match_pattern = (
-            self.test_unescaped_search_for_pattern)
-        self.test_unescaped_search_for_max_match_expected_master_result = (
-            self.test_unescaped_search_for_expected_results)
-
-        self.test_unescaped_search_for_disabled_regex_pattern = r"\'"
-        self.test_unescaped_search_for_disabled_regex_pattern_expected = [
-            [],
-            [self.test_unescaped_search_for_disabled_regex_pattern],
-            [],
-            [],
-            [self.test_unescaped_search_for_disabled_regex_pattern],
-            [],
-            [],
-            [self.test_unescaped_search_for_disabled_regex_pattern],
-            [2 * self.bs +
-                self.test_unescaped_search_for_disabled_regex_pattern],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            []]
 
     def set_up_split(self):
         self.test_split_pattern = "'"
@@ -435,45 +334,6 @@ class StringProcessingTest(unittest.TestCase):
             r"",
             self.bs]
 
-    def assertUnescapedSearchForResultEqual(self,
-                                            pattern,
-                                            test_strings,
-                                            expected_strings,
-                                            flags,
-                                            max_match,
-                                            use_regex):
-        """
-        Checks whether the given test_strings do equal the expected_strings
-        after feeding unescaped_search_for() with them.
-
-        :param pattern:          The pattern to pass to unescaped_search_for().
-        :param test_strings:     The test string to pass to
-                                 unescaped_search_for().
-        :param expected_strings: The expected results to make the asserts for.
-        :param flags:            Passed to the parameter flags in
-                                 unescaped_search_for().
-        :param max_match:        The number of matches to perform. 0 or
-                                 less would not limit the number of matches.
-        :param use_regex:        Whether to treat pattern as a regex or not.
-        """
-        self.assertEqual(len(expected_strings), len(test_strings))
-        for i in range(0, len(expected_strings)):
-            matches = unescaped_search_for(pattern,
-                                           test_strings[i],
-                                           flags,
-                                           max_match,
-                                           use_regex)
-
-            # Check each MatchObject. Need to iterate over the return_value
-            # since the return value is an iterator object pointing to the
-            # MatchObject's.
-            n = 0
-            for elem in matches:
-                self.assertEqual(expected_strings[i][n], elem.group(0))
-                n += 1
-
-            self.assertEqual(n, len(expected_strings[i]))
-
     def assertSplitEquals(self,
                           test_strings,
                           expected_results,
@@ -659,75 +519,6 @@ class StringProcessingTest(unittest.TestCase):
             self.assertEqual(x, next(iterator2))
 
         self.assertRaises(StopIteration, next, iterator2)
-
-    # Test unescaped_search_for() function.
-    def test_unescaped_search_for(self):
-        search_pattern = self.test_unescaped_search_for_pattern
-        expected_results = self.test_unescaped_search_for_expected_results
-
-        self.assertUnescapedSearchForResultEqual(search_pattern,
-                                                 self.test_strings,
-                                                 expected_results,
-                                                 0,
-                                                 0,
-                                                 True)
-
-    # Test unescaped_search_for() with a simple pattern.
-    def test_unescaped_search_for_simple_pattern(self):
-        search_pattern = self.test_unescaped_search_for_simple_pattern_pattern
-        expected_results = (
-            self.test_unescaped_search_for_simple_pattern_expected_results)
-
-        for use_regex in [True, False]:
-            self.assertUnescapedSearchForResultEqual(search_pattern,
-                                                     self.test_strings,
-                                                     expected_results,
-                                                     0,
-                                                     0,
-                                                     use_regex)
-
-    # Test unescaped_search_for() with an empty pattern.
-    def test_unescaped_search_for_empty_pattern(self):
-        expected_results = (
-            self.test_unescaped_search_for_empty_pattern_expected_results)
-
-        for use_regex in [True, False]:
-            self.assertUnescapedSearchForResultEqual("",
-                                                     self.test_strings,
-                                                     expected_results,
-                                                     0,
-                                                     0,
-                                                     use_regex)
-
-    # Test unescaped_search_for() for its max_match parameter.
-    def test_unescaped_search_for_max_match(self):
-        search_pattern = self.test_unescaped_search_for_max_match_pattern
-        expected_master_results = (
-            self.test_unescaped_search_for_max_match_expected_master_result)
-
-        for i in [1, 2, 3, 4, 5, 6, 987, 1122334455]:
-            expected_results = [
-                expected_master_results[j][0 : i]
-                for j in range(len(expected_master_results))]
-            self.assertUnescapedSearchForResultEqual(search_pattern,
-                                                     self.test_strings,
-                                                     expected_results,
-                                                     0,
-                                                     i,
-                                                     True)
-
-    # Test unescaped_search_for() with regexes disabled.
-    def test_unescaped_search_for_disabled_regex(self):
-        search_pattern = self.test_unescaped_search_for_disabled_regex_pattern
-        expected_results = (
-            self.test_unescaped_search_for_disabled_regex_pattern_expected)
-
-        self.assertUnescapedSearchForResultEqual(search_pattern,
-                                                 self.test_strings,
-                                                 expected_results,
-                                                 0,
-                                                 0,
-                                                 False)
 
     # Test the basic split() functionality.
     def test_split(self):
---
+++ coalib/tests/parsing/StringProcessingTests/UnescapedSearchForTest.py
@@ -0,0 +1,175 @@
+import sys
+sys.path.insert(0, ".")
+import unittest
+
+from coalib.tests.parsing.StringProcessingTest import StringProcessingTest
+from coalib.parsing.StringProcessing import unescaped_search_for
+
+
+class UnescapedSearchForTest(StringProcessingTest):
+    def setUp(self):
+        StringProcessingTest.setUp(self)
+
+        # Match either "out1" or "out2".
+        self.test_basic_pattern = "out1|out2"
+        # These are the expected results for the zero-group of the
+        # returned MatchObject's.
+        self.test_basic_expected_results = [
+            [r"out1", r"out2"],
+            [r"out1", r"out2"],
+            [r"out1", r"out2"],
+            [r"out1", r"out2"],
+            [r"out1", r"out2"],
+            [r"out1", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2", r"out2"],
+            [r"out1", r"out2"],
+            [],
+            [r"out1", r"out2"],
+            [],
+            []]
+
+        self.test_max_match_pattern = self.test_basic_pattern
+        self.test_max_match_expected_master_result = (
+            self.test_basic_expected_results)
+
+    @staticmethod
+    def list_zero_group(it):
+        """
+        Collects all MatchObject elements from the given iterator and extracts
+        their first matching group (group 0).
+
+        :param it: The input iterator where to collect from.
+        """
+        return [elem.group(0) for elem in it]
+
+    # Test the unescaped_search_for() function.
+    def test_basic(self):
+        expected_results = self.test_basic_expected_results
+
+        self.assertResultsEqual(
+            unescaped_search_for,
+            {(self.test_basic_pattern, test_string, 0, 0, True): result
+             for test_string, result in zip(self.test_strings,
+                                            expected_results)},
+            self.list_zero_group)
+
+    # Test unescaped_search_for() with a simple pattern.
+    def test_simple_pattern(self):
+        # Don't forget to prepend consumed escape characters.
+        expected_results = [
+            2 * [r"'"],
+            2 * [r"'"],
+            2 * [r"'"],
+            2 * [r"'"],
+            2 * [r"'"],
+            [r"'", r"\\'"],
+            4 * [r"'"],
+            4 * [r"'"],
+            4 * [r"'"],
+            4 * [r"'"],
+            4 * [r"'"],
+            [r"\\'"] + 3 * [r"'"],
+            [r"\\\\'"] + 3 * [r"'"],
+            6 * [r"'"],
+            [],
+            [],
+            [],
+            []]
+
+        self.assertResultsEqual(
+            unescaped_search_for,
+            {(r"'", test_string, 0, 0, use_regex): result
+             for test_string, result in zip(self.test_strings,
+                                            expected_results)
+             for use_regex in [True, False]},
+            self.list_zero_group)
+
+    # Test unescaped_search_for() with an empty pattern.
+    def test_empty_pattern(self):
+        # Since an empty pattern can also be escaped, the result contains
+        # special cases. Especially we check the completely matched string (and
+        # not only the matched pattern itself) we need to place also the
+        # matched escape characters inside the result list consumed from the
+        # internal regex of unescaped_search_for().
+        expected_results = [
+            29 * [r""] + [2 * self.bs] + 7 * [r""],
+            38 * [r""],
+            38 * [r""],
+            27 * [r""] + [4 * self.bs] + 7 * [r""],
+            38 * [r""],
+            30 * [r""] + [2 * self.bs] + 6 * [r""],
+            39 * [r""],
+            38 * [r""],
+            5 * [r""] + [2 * self.bs] + 30 * [r""],
+            5 * [r""] + [2 * self.bs] + 31 * [r""],
+            5 * [r""] + [4 * self.bs] + 29 * [r""],
+            13 * [r""] + [2 * self.bs] + 23 * [r""],
+            11 * [r""] + [4 * self.bs] + 23 * [r""],
+            39 * [r""],
+            [r""],
+            15 * [r""],
+            [r""],
+            [2 * self.bs]]
+
+        self.assertResultsEqual(
+            unescaped_search_for,
+            {(r"", test_string, 0, 0, use_regex): result
+             for test_string, result in zip(self.test_strings,
+                                            expected_results)
+             for use_regex in [True, False]},
+            self.list_zero_group)
+
+    # Test unescaped_search_for() for its max_match parameter.
+    def test_max_match(self):
+        search_pattern = self.test_max_match_pattern
+        expected_master_results = self.test_max_match_expected_master_result
+
+        self.assertResultsEqual(
+            unescaped_search_for,
+            {(search_pattern, test_string, 0, max_match, True): result
+             for max_match in [1, 2, 3, 4, 5, 6, 987, 1122334455]
+             for test_string, result in zip(
+                 self.test_strings,
+                 [elem[0 : max_match] for elem in expected_master_results])},
+            self.list_zero_group)
+
+    # Test unescaped_search_for() with regexes disabled.
+    def test_disabled_regex(self):
+        search_pattern = r"\'"
+        expected_results = [
+            [],
+            [search_pattern],
+            [],
+            [],
+            [search_pattern],
+            [],
+            [],
+            [search_pattern],
+            [2 * self.bs + search_pattern],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []]
+
+        self.assertResultsEqual(
+            unescaped_search_for,
+            {(search_pattern, test_string, 0, 0, False): result
+             for test_string, result in zip(self.test_strings,
+                                            expected_results)},
+            self.list_zero_group)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
