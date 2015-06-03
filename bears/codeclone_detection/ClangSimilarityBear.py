05ad473362fc5645920d7325320b005ab07fd7a2
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-03 12:33:47 +0200

    ClangSimilarityBear: Remove unneeded import

--- bears/codeclone_detection/ClangSimilarityBear.py
+++ bears/codeclone_detection/ClangSimilarityBear.py
@@ -4,7 +4,7 @@ import multiprocessing
 from coalib.misc.StringConverter import StringConverter
 from coalib.processes.SectionExecutor import get_cpu_count
 from coalib.results.HiddenResult import HiddenResult
-from coalib.settings.Setting import typed_dict, path
+from coalib.settings.Setting import typed_dict
 from coalib.bears.GlobalBear import GlobalBear
 from bears.codeclone_detection.ClangCountVectorCreator import \
     ClangCountVectorCreator
