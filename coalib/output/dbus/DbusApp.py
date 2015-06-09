e972a931e1bb69d806af883d2c9961e706e70699
AbdealiJK <abdealikothari@gmail.com>
2015-06-09 20:19:24 +0530

    DbusApp: Add document management functions
    The DbusApp handles creating and deletion of DbusDocuments. So, this
    commit adds the functions create_document() and dispose_document()
    which handle the lowlevel processing needed to create and delete docs.

--- coalib/output/dbus/DbusApp.py
+++ coalib/output/dbus/DbusApp.py
@@ -1,3 +1,8 @@
+import os
+
+from coalib.output.dbus.DbusDocument import DbusDocument
+
+
 class DbusApp:
     """
     Stores data about each client that connects to the DbusServer
@@ -7,4 +12,39 @@ class DbusApp:
         self.name = name
 
         self.docs = {}
-        self.next_doc_id = 0
+        self.__next_doc_id = 0
+
+    def _next_doc_id(self):
+        self.__next_doc_id += 1
+        return self.__next_doc_id
+
+    def create_document(self, path):
+        """
+        Create a new dbus document.
+
+        :param path:        The path to the document to be created.
+        :param object_path: The dbus object path to use as the base for the
+                            document object path.
+        :param object_path: The connection to which the new ddocument object
+                            path should be added.
+        :return:            a DbusDocument object.
+        """
+        path = os.path.abspath(os.path.expanduser(path))
+        doc = DbusDocument(id=self._next_doc_id(), path=path)
+        self.docs[path] = doc
+
+        return doc
+
+    def dispose_document(self, path):
+        """
+        Dispose of the document with the given path. It fails silently if the
+        document does not exist. If there are no more documents in the app,
+        the app is disposed.
+
+        :param path: The path to the document.
+        """
+        path = os.path.abspath(os.path.expanduser(path))
+        try:
+            return self.docs.pop(path)
+        except KeyError:
+            return None
---
+++ coalib/tests/output/dbus/DbusAppTest.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.output.dbus.DbusApp import DbusApp
+
+
+class DbusServerTest(unittest.TestCase):
+    def test_docs(self):
+        uut = DbusApp(id=1)
+        doc1 = __file__
+        doc2 = __file__ + ".txt"
+
+        uut.create_document(doc1)
+        self.assertIn(doc1, uut.docs)
+
+        uut.dispose_document(doc2)
+        self.assertNotIn(doc2, uut.docs)
+        self.assertIn(doc1, uut.docs)
+
+        uut.dispose_document(doc1)
+        self.assertNotIn(doc1, uut.docs)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
