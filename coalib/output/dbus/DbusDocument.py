53f2bfa46fc45ba18d4b44517d7669b273ed1fa6
AbdealiJK <abdealikothari@gmail.com>
2015-06-10 18:41:32 +0530

    DbusDocument: Add methods to set/find config file
    In the dbus method, coala needs to automatically find the correct coafile
    to use with the given document. And a method which allows the
    external application to set the config file is needed as well.
    So, this commit adds the necessary functions for this functionality.

--- coalib/output/dbus/DbusDocument.py
+++ coalib/output/dbus/DbusDocument.py
@@ -1,8 +1,12 @@
 import os
 import dbus.service
 
+from coalib.settings.ConfigurationGathering import find_user_config
+
 
 class DbusDocument(dbus.service.Object):
+    interface = "org.coala.v1"
+
     def __init__(self, id, path=""):
         """
         Creates a new dbus object-path for every document that a
@@ -15,8 +19,48 @@ class DbusDocument(dbus.service.Object):
         """
         dbus.service.Object.__init__(self)
 
+        self.config_file = ""
         self.path = path
         self.id = id
+
+    @dbus.service.method(interface,
+                         in_signature="",
+                         out_signature="s")
+    def FindConfigFile(self):
+        """
+        This method uses the path of the document to identify a user config
+        file for it
+
+        :return: The config file path
+        """
+        self.config_file = find_user_config(self.path)
+        return self.config_file
+
+    @dbus.service.method(interface,
+                         in_signature="s",
+                         out_signature="s")
+    def SetConfigFile(self, config_file):
+        """
+        This method sets the config file to use. It has to be an absolute path,
+        as otherwise it is difficult to find it.
+
+        :param config_file: The path fo the config file to use. This has to be
+                            an absolute path
+        :return:            The config path which has been used
+        """
+        self.config_file = config_file
+        return self.config_file
+
+    @dbus.service.method(interface,
+                         in_signature="",
+                         out_signature="s")
+    def GetConfigFile(self):
+        """
+        This method gets the config file which is being used
+
+        :return: The config path which is being used
+        """
+        return self.config_file
 
     @property
     def path(self):
--- coalib/tests/output/dbus/DbusDocumentTest.py
+++ coalib/tests/output/dbus/DbusDocumentTest.py
@@ -15,6 +15,15 @@ class DbusDocumentTest(unittest.TestCase):
         uut = DbusDocument(id=1, path=test_file)
         self.assertEqual(uut.path, os.path.abspath(test_file))
 
+    def test_config(self):
+        uut = DbusDocument("dummy_path")
+        self.assertEqual(uut.FindConfigFile(), "")
+
+        uut.SetConfigFile("config_file")
+        self.assertEqual(uut.config_file, "config_file")
+
+        self.assertEqual(uut.GetConfigFile(), "config_file")
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
