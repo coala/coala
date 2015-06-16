4e99d9d1a2eb2e3b4aecee8965218352ab17baf4
AbdealiJK <abdealikothari@gmail.com>
2015-06-16 08:52:22 +0530

    travis: Disable coverage reporting
    A part of https://github.com/coala-analyzer/coala/issues/653 where we
    stop using travis and use circleci instead. This commit removes coverage
    reporting from travis. The next commit will add it into circleci.

--- .misc/.success.sh
+++ .misc/.success.sh
@@ -1,13 +1,8 @@
 set -x
 set -e
 
-if python --version | grep 3\.4 ; then
-  coveralls
-  codecov
-
-  if [ "$TRAVIS_BRANCH" = "master" ] ; then
-    cd .misc
-    sh .install.zanata.sh
-    zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_API --username sils --url https://translate.zanata.org/zanata/
-  fi
+if [ "$TRAVIS_BRANCH" = "master" ] ; then
+  cd .misc
+  sh .install.zanata.sh
+  zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_API --username sils --url https://translate.zanata.org/zanata/
 fi
