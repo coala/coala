6bdbda53ba4b547687d699c9d058e5c9b776c134
AbdealiJK <abdealikothari@gmail.com>
2015-06-16 09:01:33 +0530

    circleci: Run zanata on success - for master
    Run zanata from circleci and remove it from travis.

--- .misc/.deploy.zanata.sh
+++ .misc/.deploy.zanata.sh
@@ -1,7 +1,7 @@
-if python --version | grep 3\.4 ; then
+if [ $CIRCLE_NODE_INDEX = 0 ] ; then
   echo Running Zanata...
   cd .misc
   sh .install.zanata.sh
-  zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_API --username sils --url https://translate.zanata.org/zanata/
+  zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
   cd ..
 fi
--- .misc/.success.sh
+++
@@ -1,6 +0,0 @@
-set -x
-set -e
-
-if [ "$TRAVIS_BRANCH" = "master" ] ; then
-  sh .misc/.deploy.zanata.sh
-fi
