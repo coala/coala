7b3956e04e1140948f2b3c15b6d791e0e47f10d4
AbdealiJK <abdealikothari@gmail.com>
2015-06-18 00:34:17 +0530

    circleci: Remove python-tmpenv
    Earlier packages like python-gi and python-dbus were installed in a
    temporary python environment and then moved to the actual virtualenv.
    This commit removes the need for the tmpenv and directly installs it in
    the virtualenv.

--- .misc/.install.python-dbus.sh
+++ .misc/.install.python-dbus.sh
@@ -10,23 +10,16 @@ echo Unpacking python-dbus...
 tar -zxvf dbus-python.tar.gz > /dev/null
 rm dbus-python.tar.gz
 
-mkdir -p python-tmpenv
-
 if [ "$CIRCLECI" = "true" ] ; then
   python_virtualenv=`pyenv prefix`
 else
   python_virtualenv=$VIRTUAL_ENV
 fi
 
-python_tmpenv=`pwd`/python-tmpenv
-
 cd dbus-python-1.2.0
 
-PYTHON=`sudo which ${system_python}` ./configure --prefix=$python_tmpenv
+PYTHON=`sudo which ${system_python}` ./configure --prefix=$python_virtualenv
 make
 make install
 
 cd ..
-
-echo Copying files from the temporary python env to the virtualenv
-cp -r $python_tmpenv/* $python_virtualenv
