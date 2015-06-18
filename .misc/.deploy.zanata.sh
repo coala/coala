a6d78a379fd0e336f26008b53fadd7c7e2c0bcf1
AbdealiJK <abdealikothari@gmail.com>
2015-06-18 23:01:14 +0530

    ruitor: Remove zanata
    Remove zanata from ruitor as it doesn't work correctly. The next commit
    will add it back to circleci.

--- .misc/.deploy.zanata.sh
+++ .misc/.deploy.zanata.sh
@@ -4,10 +4,6 @@ set -e
 set -x
 
 cd .misc
-
-echo Getting Zanata API Key...
-bash zanata_key.sh
-
 echo Running Zanata...
 sh .install.zanata.sh
 zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
