9893f37a95ba9146bb4504ee974877b6dc821841
AbdealiJK <abdealikothari@gmail.com>
2015-06-18 23:03:31 +0530

    circleci: Run zanata on master branch
    Run zanata if the build is on master.

---
+++ .misc/deploy.zanata.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ "$CIRCLE_BRANCH" = "master" ] ; then
+  cd .misc
+  echo Running Zanata...
+  sh .install.zanata.sh
+  zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
+  cd ..
+fi
