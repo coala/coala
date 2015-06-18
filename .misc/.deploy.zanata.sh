6d8e4256827a793c94456c80f237b33769f4a090
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-18 17:16:03 +0200

    deploy.zanata: Remove circle condition
    This won't apply on rultor.

--- .misc/.deploy.zanata.sh
+++ .misc/.deploy.zanata.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [ $CIRCLE_NODE_INDEX = 0 ] ; then
-  echo Running Zanata...
-  cd .misc
-  sh .install.zanata.sh
-  zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
-  cd ..
-fi
+echo Running Zanata...
+cd .misc
+sh .install.zanata.sh
+zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
+cd ..
