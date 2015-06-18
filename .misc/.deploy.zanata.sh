d9532cb50aed7503f08aa5c3d81180f8e27a6e22
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-18 17:24:43 +0200

    rultor: Add zanata_key
    The encrypted zanata_key.sh contains a bash shebang and an export of the
    ZANATA_KEY environment variable.

--- .misc/.deploy.zanata.sh
+++ .misc/.deploy.zanata.sh
@@ -3,8 +3,12 @@
 set -e
 set -x
 
-echo Running Zanata...
 cd .misc
+
+echo Getting Zanata API Key...
+bash zanata_key.sh
+
+echo Running Zanata...
 sh .install.zanata.sh
 zanata-cli-3.6.0/bin/zanata-cli -B push --key $ZANATA_KEY --username sils --url https://translate.zanata.org/zanata/
 cd ..
