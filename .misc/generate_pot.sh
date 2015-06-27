8afbfe55c86b3295742ef903a77aacd207999cf6
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-27 20:32:58 +0200

    generate_pot: Remove unneeded files
    The coala file is just a wrapper by now.

--- .misc/generate_pot.sh
+++ .misc/generate_pot.sh
@@ -6,15 +6,14 @@ set -e
 cd ..
 echo '' > locale/coala.pot
 echo Generating new pot file...
-cat `find . -name "*.py" \
- | grep -v build` coala \
-  | python3 .misc/.extract_doc_translations > .doc_strings
+cat `find . -name "*.py" | grep -v build` \
+ | python3 .misc/.extract_doc_translations > .doc_strings
 xgettext -j -c --language=Python --output=locale/coala.pot --keyword=_ \
  --keyword=N_ --from-code=UTF-8 --package-name=coala \
  --package-version=`./coala --version` \
  --msgid-bugs-address=lasse.schuirmann@gmail.com \
  --copyright-holder="The coala authors" \
- `find . -name "*.py" | grep -v build` coala .doc_strings
+ `find . -name "*.py" | grep -v build` .doc_strings
 
 echo Removing temporary files...
 rm .doc_strings
