281b8584e060a73c6164c5ee5484615982dbe384
AbdealiJK <abdealikothari@gmail.com>
2015-06-28 07:56:35 +0530

    circleci: Move python installation to deps.pre.sh

--- .misc/deps.pre.sh
+++ .misc/deps.pre.sh
@@ -5,3 +5,22 @@ set -e
 sudo service mysql stop
 sudo service mongodb stop
 sed -i '/source \/home\/ubuntu\/virtualenvs\//d' ~/.circlerc
+
+# Choose the python versions to install deps for
+case $CIRCLE_NODE_INDEX in
+ 1) dep_version="3.3.6" ;;
+ 2) dep_version="3.2.6" ;;
+ *) dep_version="3.4.2" ;;
+esac
+
+# Install python version needed
+# Note - `--with-wide-unicode` is needed only because 3.2.5 defaults to UCS2 unicode
+PYTHON_CONFIGURE_OPTS="--with-wide-unicode" pyenv install -ks $dep_version
+pyenv local $dep_version
+export python_unicode_storage=`python -c "import sys; print('UCS4' if sys.maxunicode > 65536 else 'UCS2')"`
+
+if [ ! "$python_unicode_storage" = "UCS4" ] ; then
+  pyenv uninstall -f $dep_version
+  PYTHON_CONFIGURE_OPTS="--with-wide-unicode" pyenv install -k $dep_version
+fi
+
--- .misc/deps.sh
+++ .misc/deps.sh
@@ -1,24 +1,6 @@
 set -x
 set -e
 
-# Choose the python versions to install deps for
-case $CIRCLE_NODE_INDEX in
- 1) dep_version="3.3.6" ;;
- 2) dep_version="3.2.6" ;;
- *) dep_version="3.4.2" ;;
-esac
-
-# Install python version needed
-# Note - `--with-wide-unicode` is needed only because 3.2.5 defaults to UCS2 unicode
-PYTHON_CONFIGURE_OPTS="--with-wide-unicode" pyenv install -ks $dep_version
-pyenv local $dep_version
-export python_unicode_storage=`python -c "import sys; print('UCS4' if sys.maxunicode > 65536 else 'UCS2')"`
-
-if [ ! "$python_unicode_storage" = "UCS4" ] ; then
-  pyenv uninstall -f $dep_version
-  PYTHON_CONFIGURE_OPTS="--with-wide-unicode" pyenv install -k $dep_version
-fi
-
 # Env vars for deps.sh
 export python_virtualenv=`pyenv prefix`
 
