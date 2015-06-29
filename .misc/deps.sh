3eedd457fb7843c436fdfa3a1323d8bfa244ed29
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-29 21:20:09 +0200

    coverage: Use future codecov
    This is the most up to date uploader at any time and it fiddles with the
    coverage data and should allow us to finally remove platform dependent
    no covers.

--- .misc/deps.sh
+++ .misc/deps.sh
@@ -8,7 +8,7 @@ sudo apt-get -qq install espeak libclang1-3.4
 sudo apt-get -qq install libdbus-glib-1-dev # for python-dbus
 sudo apt-get -qq install glib2.0-dev gobject-introspection libgirepository1.0-dev python3-cairo-dev # for python-gi
 
-pip install -q setuptools codecov munkres3
+pip install -q setuptools coverage munkres3
 
 cd .misc
 if [ "$python_implementation" == "CPython" ] ; then
