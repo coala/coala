3d5699b488c8dc92955aec9d3769c811e0c12638
AbdealiJK <abdealikothari@gmail.com>
2015-07-01 21:25:46 +0530

    circleci: Revert tests back to 1 job
    Tests in circleci which use subprocesses are stochastic with multiple
    jobs. Let's be safer and use -j1. The time penalty is about 5seconds
    which is alright.

--- .misc/tests.sh
+++ .misc/tests.sh
@@ -4,7 +4,7 @@ set -e
 source .misc/env_variables.sh
 
 if [ "$python_implementation" == "CPython" ] ; then
-  python run_tests.py --disallow-test-skipping --cover -j3
+  python run_tests.py --disallow-test-skipping --cover -j1
 else
-  python run_tests.py -j3
+  python run_tests.py -j1
 fi
