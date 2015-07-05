9ed36f3d42f8e9b462a530a9db15ef3310cc4a24
Udayan Tandon <udayan12167@iiitd.ac.in>
2015-07-05 20:49:30 +0530

    coalaApp: Add coala application

---
+++ coalib/output/gui/coalaApp.py
@@ -0,0 +1,31 @@
+import os
+from gi.repository import Gtk, Gio
+
+from coalib.output.gui.greeter.GreeterWindow import GreeterWindow
+
+class coalaApp(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self,
+                                 application_id="org.coala",
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE)
+        self.resource = Gio.resource_load(
+            os.getcwd()+"/output/gui/data/coala.gresource")
+        Gio.Resource._register(self.resource)
+
+        self.greeter = None
+        self.workspace = None
+
+        self.connect("activate", self.activate)
+
+    def _setup_greeter(self, app):
+        self.greeter = GreeterWindow(app)
+        self.greeter.list_box.connect("row-activated",
+                                      self._setup_workspace,
+                                      app)
+
+    def _setup_workspace(self, listbox, listboxrow, app):
+        print(listboxrow.get_child().get_name())
+
+    def activate(self, app):
+        self._setup_greeter(app)
+        self.greeter.show()
