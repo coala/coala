04c2245d3e28d0b0aed856c4dc0203d8cf4dea54
Udayan Tandon <udayan12167@iiitd.ac.in>
2015-07-08 16:50:17 +0530

    ConsoleInteractor: Move out show_enumeration
    This commit is part of a series of commits to convert ConsoleInteractor
    into a set of functions.

--- coalib/output/ConsoleInteractor.py
+++ coalib/output/ConsoleInteractor.py
@@ -126,6 +126,36 @@ def acquire_settings(log_printer, settings_names_dict):
     return result
 
 
+def show_enumeration(console_printer, title, items, indentation, no_items_text):
+    """
+    This function takes as input an iterable object (preferably a list or
+    a dict). And prints in a stylized format. If the iterable object is
+    empty, it prints a specific statement give by the user. An e.g :
+
+    <indentation>Title:
+    <indentation> * Item 1
+    <indentation> * Item 2
+
+    :param console_printer: Object to print messages on the console.
+    :param title:           Title of the text to be printed
+    :param items:           The iterable object.
+    :param indentation:     Number of spaces to indent every line by.
+    :param no_items_text:   Text printed when iterable object is empty.
+    """
+    if not items:
+        console_printer.print(indentation + no_items_text)
+    else:
+        console_printer.print(indentation + title)
+        if isinstance(items, dict):
+            for key, value in items.items():
+                console_printer.print(indentation + " * " + key + ": " +
+                                      value[0])
+        else:
+            for item in items:
+                console_printer.print(indentation + " * " + item)
+    console_printer.print()
+
+
 class ConsoleInteractor(ConsolePrinter):
     STR_LINE_DOESNT_EXIST = _("The line belonging to the following result "
                               "cannot be printed because it refers to a line "
@@ -393,46 +423,22 @@ class ConsoleInteractor(ConsolePrinter):
                                key=lambda bear: bear.__name__):
                 self._show_bear(bear, bears[bear], bear.get_metadata())
 
-    def _show_enumeration(self, title, items, indentation, no_items_text):
-        """
-        This function takes as input an iterable object (preferably a list or
-        a dict). And prints in a stylized format. If the iterable object is
-        empty, it prints a specific statement give by the user. An e.g :
-
-        <indentation>Title:
-        <indentation> * Item 1
-        <indentation> * Item 2
-
-        :param title:         Title of the text to be printed
-        :param items:         The iterable object.
-        :param indentation:   Number of spaces to indent every line by.
-        :param no_items_text: Text printed when iterable object is empty.
-        """
-        if not items:
-            self.print(indentation + no_items_text)
-        else:
-            self.print(indentation + title)
-            if isinstance(items, dict):
-                for key, value in items.items():
-                    self.print(indentation + " * " + key + ": " + value[0])
-            else:
-                for item in items:
-                    self.print(indentation + " * " + item)
-        self.print()
-
     def _show_bear(self, bear, sections, metadata):
         self.print("{bear}:".format(bear=bear.__name__))
         self.print("  " + metadata.desc + "\n")
 
-        self._show_enumeration(_("Used in:"),
-                               sections,
-                               "  ",
-                               _("No sections."))
-        self._show_enumeration(_("Needed Settings:"),
-                               metadata.non_optional_params,
-                               "  ",
-                               _("No needed settings."))
-        self._show_enumeration(_("Optional Settings:"),
-                               metadata.optional_params,
-                               "  ",
-                               _("No optional settings."))
+        show_enumeration(self,
+                         _("Used in:"),
+                         sections,
+                         "  ",
+                         _("No sections."))
+        show_enumeration(self,
+                         _("Needed Settings:"),
+                         metadata.non_optional_params,
+                         "  ",
+                         _("No needed settings."))
+        show_enumeration(self,
+                         _("Optional Settings:"),
+                         metadata.optional_params,
+                         "  ",
+                         _("No optional settings."))
