988c0399659ecc57edeb2fa5d65c8ce9b3d8fd0f
AbdealiJK <abdealikothari@gmail.com>
2015-07-11 09:16:12 +0530

    continuous-intergation: Increase timeout
    Increase timeouts for tests in circleci and appveyor

--- .misc/tests.sh
+++ .misc/tests.sh
@@ -3,12 +3,13 @@ set -e
 
 source .misc/env_variables.sh
 
+args="-j1 --timeout 120"
+
 if [ "$python_implementation" == "CPython" ] ; then
-  args=
   if [ "$python_version" == "3.2" ] ; then
-    args+="--omit PyLintBearTest"
+    args+=" --omit PyLintBearTest"
   fi
-  python run_tests.py --disallow-test-skipping --cover -j 1 $args
-else
-  python run_tests.py -j1
+  args+=" --disallow-test-skipping --cover"
 fi
+
+python run_tests.py $args
