92edf6b2327d4e5a87e5396bc5789930cd168d17
AbdealiJK <abdealikothari@gmail.com>
2015-07-13 16:52:46 +0530

    appveyor: Install espeak printer as dependency
    Using the /? command line argument, we can find the cmd line arguments
    that the espeak printer's setup accepts. The /verysilent arg can be used
    for automated installation without any user input.

--- .misc/deps.bat
+++ .misc/deps.bat
@@ -7,4 +7,9 @@ REM Install GNU gettext
 nuget install Gettext.Tools -Version 0.19.4
 SET PATH=%PATH%;%cd%\Gettext.Tools.0.19.4\tools
 
+REM Install eSpeak printer
+curl -LO http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/setup_espeak-1.48.04.exe
+setup_espeak-1.48.04.exe /NORESTART /SUPPRESSMSGBOXES /VERYSILENT /SP-
+SET PATH=%PATH%;C:\Program Files (x86)\eSpeak\command_line
+
 @echo OFF
