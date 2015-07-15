602821e546320a103451029b722b562973a000b9
AbdealiJK <abdealikothari@gmail.com>
2015-07-15 19:29:27 +0530

    mkdocs: Add superfences from pymdown-extensions
    Add the module pymdown-extensions to fix readthedocs. Currently the only
    way to install a module before readthedocs builds them is by assing it to
    out dependencies. Sadly every user of coala will have to install that to.
    But this cannot be avoided.

--- setup.py
+++ setup.py
@@ -53,7 +53,11 @@ if __name__ == "__main__":
                             "munkres3",
                             "coverage",
                             "pylint",
-                            "language-check"],
+                            "language-check",
+                            "pymdown-extensions"],
+          dependency_links=[
+              "https://github.com/facelessuser/pymdown-extensions/archive"
+              "/master.zip#egg=pymdown-extensions-1.0.0"],
           package_data={'coalib': ['default_coafile']},
           license="AGPL v3",
           data_files=data_files,
