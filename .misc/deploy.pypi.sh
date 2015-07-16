6a9d5d4664b63f4b955c9fa5b215d7641063cdf6
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-07-16 23:37:30 +0200

    deploy.pypi: Use twine to upload
    Twine is more secure and we probably should've done it in the first
    place with twine.

--- .misc/deploy.pypi.sh
+++ .misc/deploy.pypi.sh
@@ -2,14 +2,7 @@ set -e
 # Do not use `set -x` here as then it displays the PYPIPW in circleci logs
 
 if [ "$CIRCLE_BRANCH" = "master" ] ; then
-  echo "[distutils]
-  index-servers=pypi
-  [pypi]
-  repository = https://pypi.python.org/pypi
-  username = $PYPIUSER
-  password = $PYPIPW" > ~/.pypirc
-  echo Deploying coala `coala -v`...
-  python setup.py register
-  python setup.py sdist upload
-  rm ~/.pypirc
+  pip install twine wheel
+  python setup.py sdist bdist_wheel
+  twine upload dist/* -u "$PYPIUSER" -p "$PYPIPW"
 fi
