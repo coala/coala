c82941399f06d6a73af4cf3a5060d67ee42f42a4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-07-16 22:07:11 +0200

    rultor: Add release script
    Part of https://github.com/coala-analyzer/coala/issues/668

---
+++ .misc/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# Abort if uncommitted things lie around
+git diff HEAD --exit-code
+
+# Adjust version number to next release, script will check validity
+python3 .misc/adjust_version_number.py coalib/VERSION --new-version ${tag} -b 0
+
+# Commit it
+git add coalib/VERSION
+git commit -m "Release ${tag}"
+
+# Put this commit onto master too, rultor doesn't do that by default
+git checkout -b tomerge
+git checkout master
+git merge --ff-only tomerge
