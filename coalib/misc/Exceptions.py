b9edf9d4ee8f398bf9a789507a72afbc4b9ed3b1
AbdealiJK <abdealikothari@gmail.com>
2015-07-18 18:18:39 +0530

    misc: Add Exceptions

---
+++ coalib/misc/Exceptions.py
@@ -0,0 +1,21 @@
+from coalib.output.printers.NullPrinter import NullPrinter
+from coalib.misc.StringConstants import StringConstants
+from coalib.misc.i18n import _
+
+
+def get_exitcode(exception, log_printer=None):
+    log_printer = log_printer or NullPrinter()
+    exitcode = 0
+    if isinstance(exception, KeyboardInterrupt):  # Ctrl+C
+        print(_("Program terminated by user."))
+        exitcode = 130
+    elif isinstance(exception, EOFError):  # Ctrl+D
+        print(_("Found EOF. Exiting gracefully."))
+    elif isinstance(exception, SystemExit):
+        exitcode = exception.code
+    elif isinstance(exception, Exception):
+        log_printer.log_exception(StringConstants.CRASH_MESSAGE, exception)
+        exitcode = 255
+
+    return exitcode
+
---
+++ coalib/tests/misc/ExceptionsTest.py
@@ -0,0 +1,19 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+
+from coalib.misc.Exceptions import get_exitcode
+
+
+class ExceptionsTest(unittest.TestCase):
+    def test_get_exitcode(self):
+        self.assertEqual(get_exitcode(KeyboardInterrupt()), 130)
+        self.assertEqual(get_exitcode(AssertionError()), 255)
+        self.assertEqual(get_exitcode(SystemExit(999)), 999)
+        self.assertEqual(get_exitcode(EOFError()), 0)
+        self.assertEqual(get_exitcode(None), 0)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
