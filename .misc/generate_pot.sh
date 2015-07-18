bfdc7cb82f0df8e2c1be946d0b7e3fa549aaa183
AbdealiJK <abdealikothari@gmail.com>
2015-07-18 07:59:44 +0530

    zanata: Translate from coalib and bears only
    If the whole repo is taken, it has a chance of taking .py files which
    we did not write - and this may make it break.

--- .misc/generate_pot.sh
+++ .misc/generate_pot.sh
@@ -6,14 +6,14 @@ set -e
 cd ..
 echo '' > locale/coala.pot
 echo Generating new pot file...
-cat `find . -name "*.py" | grep -v build` \
+cat `find coalib/ bears/ -name "*.py"` \
  | python3 .misc/.extract_doc_translations > .doc_strings
 xgettext -j -c --language=Python --output=locale/coala.pot --keyword=_ \
  --keyword=N_ --from-code=UTF-8 --package-name=coala \
  --package-version=`./coala --version` \
  --msgid-bugs-address=lasse.schuirmann@gmail.com \
  --copyright-holder="The coala authors" \
- `find . -name "*.py" | grep -v build` .doc_strings
+ `find coalib/ bears/ -name "*.py"` .doc_strings
 
 echo Removing temporary files...
 rm .doc_strings
