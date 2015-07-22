68354762aa2fe47a37da7130e0ef59938532a331
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-07-23 00:03:02 +0200

    ClangFunctionDifferenceBear: Correct indentation

--- bears/codeclone_detection/ClangFunctionDifferenceBear.py
+++ bears/codeclone_detection/ClangFunctionDifferenceBear.py
@@ -95,22 +95,22 @@ class ClangFunctionDifferenceBear(GlobalBear):
         be clones at the same difference value than big functions which may
         provide a better refactoring opportunity for the user.
 
-        :param counting_conditions:  A comma seperated list of counting
-                                     conditions. Possible values are: used,
-                                     returned, is_condition, in_condition,
-                                     in_second_level_condition,
-                                     in_third_level_condition, is_assignee,
-                                     is_assigner, loop_content,
-                                     second_level_loop_content,
-                                     third_level_loop_content, is_param,
-                                     in_sum, in_product, in_binary_operation,
-                                     member_accessed.
-                                     Weightings can be assigned to each
-                                     condition due to providing a dict
-                                     value, i.e. having used weighted in
-                                     half as much as other conditions would
-                                     simply be: "used: 0.5, is_assignee".
-                                     Weightings default to 1 if unset.
+        :param counting_conditions: A comma seperated list of counting
+                                    conditions. Possible values are: used,
+                                    returned, is_condition, in_condition,
+                                    in_second_level_condition,
+                                    in_third_level_condition, is_assignee,
+                                    is_assigner, loop_content,
+                                    second_level_loop_content,
+                                    third_level_loop_content, is_param,
+                                    in_sum, in_product, in_binary_operation,
+                                    member_accessed.
+                                    Weightings can be assigned to each
+                                    condition due to providing a dict
+                                    value, i.e. having used weighted in
+                                    half as much as other conditions would
+                                    simply be: "used: 0.5, is_assignee".
+                                    Weightings default to 1 if unset.
         :param average_calculation: If set to true the difference calculation
                                     function will take the average of all
                                     variable differences as the difference,
