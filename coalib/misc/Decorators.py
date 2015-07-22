84941273624525f2e419f19b75b1402011b97c2c
Makman2 <mak95@arcor.de>
2015-07-22 03:34:22 +0200

    Decorators: Implement generate_repr
    This flexible decorator automatically implements __repr__ into the
    attached class. The __repr__ function prints in following format:
    
    <X object(field1=3, field2="a", field3=[3, 2, 1]) at 0xAAAAAAAA>
    
    Fixes https://github.com/coala-analyzer/coala/issues/858.

--- coalib/misc/Decorators.py
+++ coalib/misc/Decorators.py
@@ -54,3 +54,107 @@ def arguments_to_lists(function):
         return function(*l_args, **l_kwargs)
 
     return l_function
+
+
+def _get_member(obj, member):
+    # If not found, pass AttributeError to invoking function.
+    attribute = getattr(obj, member)
+
+    if callable(attribute) and hasattr(attribute, "__self__"):
+        # If the value is a bound method, invoke it like a getter and return
+        # its value.
+        try:
+            return attribute()
+        except TypeError:
+            # Don't use repr() to display the member more accurately, because
+            # invoking repr() on a bound method prints in this format:
+            # <bound method CLASS.METHOD of **repr(instance)**>
+            # This invokes repr() recursively.
+            raise TypeError("Given bound method '" + member + "' must be "
+                            "callable like a getter, taking no arguments.")
+    else:
+        # Otherwise it's a member variable or property (or any other attribute
+        # that holds a value).
+        return attribute
+
+
+def _construct_repr_string(obj, members):
+    # The passed entries have format (member-name, repr-function).
+    values = ", ".join(member + "=" + func(_get_member(obj, member))
+                       for member, func in members)
+    return ("<" + type(obj).__name__ + " object(" + values + ") at "
+            + hex(id(obj)) + ">")
+
+
+def generate_repr(*members):
+    """
+    Decorator that binds an auto-generated `__repr__()` function to a class.
+
+    The generated `__repr__()` function prints in following format:
+    <ClassName object(field1=1, field2='A string', field3=[1, 2, 3]) at 0xAAAA>
+
+    Note that this decorator modifies the given class in place!
+
+    :param members:         An iterable of member names to include into the
+                            representation-string. Providing no members yields
+                            to inclusion of all member variables in
+                            alphabetical order (even that ones are included
+                            that change after instantation, but no properties
+                            or other attributes starting with an underscore).
+
+                            To control the representation of each member, you
+                            can also pass a tuple where the first element
+                            contains the member to print and the second one the
+                            representation function (which defaults to the
+                            built-in `repr()`). Using None as representation
+                            function is the same as using `repr()`.
+
+                            Supported members are fields/variables, properties
+                            and getter-like functions (functions that accept no
+                            arguments).
+    :raises ValueError:     Raised when the passed
+                            (member, repr-function)-tuples have not a length of
+                            2.
+    :raises AttributeError: Raised when a given member/attribute was not found
+                            in class.
+    :raises TypeError:      Raised when a provided member is a bound method
+                            that is not a getter-like function (means it must
+                            accept no parameters).
+    :return:                The class armed with an auto-generated __repr__
+                            function.
+    """
+    def decorator(cls):
+        cls.__repr__ = __repr__
+        return cls
+
+    if members:
+        # Prepare members list.
+        members_to_print = list(members)
+        for i, member in enumerate(members_to_print):
+            if isinstance(member, tuple):
+                # Check tuple dimensions.
+                length = len(member)
+                if length == 2:
+                    members_to_print[i] = (member[0],
+                                           member[1] if member[1] else repr)
+                else:
+                    raise ValueError("Passed tuple " + repr(member) +
+                                     " needs to be 2-dimensional, but has " +
+                                     str(length) + " dimensions.")
+            else:
+                members_to_print[i] = (member, repr)
+
+        def __repr__(self):
+            return _construct_repr_string(self, members_to_print)
+    else:
+        def __repr__(self):
+            # Need to fetch member variables every time since they are unknown
+            # until class instantation.
+            members_to_print = (
+                (member, repr) for member in
+                sorted(filter(lambda mem: not mem.startswith("_"),
+                              self.__dict__),
+                       key=str.lower))
+            return _construct_repr_string(self, members_to_print)
+
+    return decorator
--- coalib/tests/misc/DecoratorsTest.py
+++ coalib/tests/misc/DecoratorsTest.py
@@ -2,7 +2,9 @@ import unittest
 import sys
 
 sys.path.insert(0, ".")
-from coalib.misc.Decorators import yield_once, arguments_to_lists
+from coalib.misc.Decorators import (arguments_to_lists,
+                                    yield_once,
+                                    generate_repr)
 
 
 class YieldOnceTest(unittest.TestCase):
@@ -28,6 +30,181 @@ class ArgumentsToListsTest(unittest.TestCase):
                                                        "d": [{"a": 1}]
                                                        })
         )
+
+
+class GenerateReprTest(unittest.TestCase):
+    # We can't define the class in the scope of this test because generate_repr
+    # modifies the class in place, so we need to redefine it every time.
+    @staticmethod
+    def define_class():
+        class X:
+            def __init__(self):
+                self.A = 2
+                self.B = "A string"
+                self.ComplexMember = [3, 2, 1]
+                self.Q = 0.5
+                self.one = 1
+                self._private_member = 0
+                self.__DONT_TOUCH = 1
+
+            def computeA(self):
+                return self.A ** 3 - 7
+
+            @property
+            def getter(self):
+                return self.one * "getter()"
+
+            @property
+            def defaulted_getter(self, a=3, b=2):
+                return a * b * self.one
+
+            def getter_like_function(self):
+                return ["A", "B"] * self.one
+
+            def defaulted_getter_like_function(self, num=33):
+                return (num, num + self.one, num + self.one * 2)
+
+            def polynome(self, x, a, b, c):
+                return a + b * x + self.one * c * x ** 2
+
+        return X
+
+    def test_manual_argument_list(self):
+        X = generate_repr("A")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr("A", "B")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, B='A string'\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr("A", "B", "ComplexMember")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, B='A string', ComplexMember=\\[3, "
+                             "2, 1\\]\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr("A", "B", "ComplexMember", "Q")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, B='A string', "
+                             "ComplexMember=\\[3, 2, 1\\]\\, Q=0\\.5\\) at "
+                             "0x[0-9a-fA-F]+>")
+
+        # Switch order.
+        X = generate_repr("ComplexMember", "A")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(ComplexMember=\\[3, 2, 1\\], A=2\\) at "
+                             "0x[0-9a-fA-F]+>")
+
+        X = generate_repr("Q", "ComplexMember", "A", "B")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(Q=0\\.5, ComplexMember=\\[3, 2, 1\\], "
+                             "A=2, B='A string'\\) at 0x[0-9a-fA-F]+>")
+
+    def test_manual_argument_list_with_custom_repr(self):
+        X = generate_repr(("A", lambda x: str(x ** 2)))(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=4\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr(("A", lambda x: str(x ** 2)), ("B", None))
+        X = X(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=4, B='A string'\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr(
+            ("B", None),
+            ("A", lambda x: str(x ** 2)),
+            ("ComplexMember", lambda x: ".".join(str(v) for v in x)))
+        X = X(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(B='A string', A=4, "
+                             "ComplexMember=3\\.2\\.1\\) at 0x[0-9a-fA-F]+>")
+
+        # Combine normal strings with tuples.
+        X = generate_repr("A",
+                          ("B", str),
+                          "ComplexMember",
+                          ("Q", lambda x: "OVERRIDE"))
+        X = X(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, B=A string, "
+                             "ComplexMember=\\[3, 2, 1\\], Q=OVERRIDE\\) at "
+                             "0x[0-9a-fA-F]+>")
+
+    def test_properties(self):
+        X = generate_repr("getter")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(getter='getter\\(\\)'\\) at "
+                             "0x[0-9a-fA-F]+>")
+
+        X = generate_repr("A",
+                          "getter",
+                          "defaulted_getter")
+        X = X(self.define_class())
+
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, getter='getter\\(\\)', "
+                             "defaulted_getter=6\\) at 0x[0-9a-fA-F]+>")
+
+    def test_getter_like_functions(self):
+        X = generate_repr("getter_like_function")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(getter_like_function=\\['A', 'B'\\]\\) "
+                             "at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr("defaulted_getter_like_function")
+        X = X(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(defaulted_getter_like_function=\\(33, "
+                             "34, 35\\)\\) at 0x[0-9a-fA-F]+>")
+
+    def test_invalid_attribute(self):
+        X = generate_repr("A", "B", "INVALID")(self.define_class())
+        self.assertRaises(AttributeError, repr, X())
+
+        X = generate_repr("A", "polynome")(self.define_class())
+        self.assertRaises(TypeError, repr, X())
+
+    def test_invalid_argument_tuple_size(self):
+        self.assertRaises(ValueError, generate_repr, (88,))
+        self.assertRaises(ValueError, generate_repr, ("A", repr, 3, 4))
+        self.assertRaises(ValueError, generate_repr, ("A", repr, False))
+        self.assertRaises(ValueError,
+                          generate_repr,
+                          ("A", repr),
+                          ("B", None),
+                          ("Q", 3, 2, 1, 0))
+
+    def test_auto_repr(self):
+        X = generate_repr()(self.define_class())
+        x = X()
+
+        self.assertRegex(repr(x),
+                         "<X object\\(A=2, B='A string', ComplexMember=\\[3, "
+                         "2, 1\\], one=1, Q=0\\.5\\) at 0x[0-9a-fA-F]+>")
+
+        # Insert member after instantation.
+        x.Z = 17
+        self.assertRegex(repr(x),
+                         "<X object\\(A=2, B='A string', ComplexMember=\\[3, "
+                         "2, 1\\], one=1, Q=0\\.5, Z=17\\) at 0x[0-9a-fA-F]+>")
+
+        # Test alphabetical order a bit more.
+        x.Ba = 4
+        x.g_mem = 0
+        self.assertRegex(repr(x),
+                         "<X object\\(A=2, B='A string', Ba=4, "
+                         "ComplexMember=\\[3, 2, 1\\], g_mem=0, one=1, "
+                         "Q=0\\.5, Z=17\\) at 0x[0-9a-fA-F]+>")
+
+    def test_duplicate_member(self):
+        X = generate_repr("A", "A")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, A=2\\) at 0x[0-9a-fA-F]+>")
+
+        X = generate_repr("A", "B", "A")(self.define_class())
+        self.assertRegex(repr(X()),
+                         "<X object\\(A=2, B='A string', A=2\\) at "
+                             "0x[0-9a-fA-F]+>")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
