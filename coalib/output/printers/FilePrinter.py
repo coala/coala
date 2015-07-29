5ddf7a74161fe4001cd99b3def91f21e728135c8
Makman2 <mak95@arcor.de>
2015-07-29 21:22:29 +0200

    FilePrinter: Don't inherit LogPrinter

--- coalib/output/printers/FilePrinter.py
+++ coalib/output/printers/FilePrinter.py
@@ -1,30 +1,24 @@
 from coalib.output.ClosableObject import ClosableObject
 from coalib.output.printers.Printer import Printer
-from coalib.output.printers.LogPrinter import LogPrinter
-from coalib.output.printers.LOG_LEVEL import LOG_LEVEL
 
 
-class FilePrinter(Printer, LogPrinter, ClosableObject):
+class FilePrinter(Printer, ClosableObject):
     """
     This is a simple printer/logprinter that prints everything to a file. Note
     that everything will be appended.
     """
-    def __init__(self,
-                 filename,
-                 log_level=LOG_LEVEL.WARNING,
-                 timestamp_format="%X"):
+    def __init__(self, filename):
         """
         Creates a new FilePrinter. If the directory of the given file doesn't
         exist or if there's any access problems, an exception will be thrown.
 
         :param filename: the name of the file to put the data into (string).
         """
+        Printer.__init__(self)
         ClosableObject.__init__(self)
         self.file = None
         if not isinstance(filename, str):
             raise TypeError("filename must be a string.")
-
-        LogPrinter.__init__(self, self, log_level, timestamp_format)
 
         self.file = open(filename, 'a+')
 
