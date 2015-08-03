e57fa6370894780fff02837e02818596ecc2e177
Makman2 <mak95@arcor.de>
2015-08-03 17:03:45 +0200

    Interrupt for Windows - FIXUP WITH PREVIOUS

--- coalib/processes/communication/InterruptProcess.py
+++ coalib/processes/communication/InterruptProcess.py
@@ -2,9 +2,28 @@ import os
 import signal
 import platform
 
+if platform.system() == "Windows":
+    from ctypes import windll
+    from time import sleep
+    CTRL_C_EVENT = 0x0
+
 
 def interrupt_processes(pid):
+    """
+    Interrupt a process by its pid.
+
+    Note for Windows users:
+    Be sure that the pid you pass shares the same process group as your program
+    that calls this function. If they don't share the same process group, this
+    call blocks for 10 seconds to be sure that this is the case.
+
+    :param pid: The pid of the process to interrupt.
+    """
     if platform.system() == "Windows":  # pragma: no cover
-        os.kill(pid, signal.CTRL_BREAK_EVENT)
+        try:
+            windll.kernel32.GenerateConsoleCtrlEvent(CTRL_C_EVENT, pid)
+            sleep(10)
+        except KeyboardInterrupt:
+            pass
     else:
         os.kill(pid, signal.SIGINT)
