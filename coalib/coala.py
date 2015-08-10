69f053303e285a178935ce907cd5d62c7feef6aa
Makman2 <mak95@arcor.de>
2015-08-11 00:36:22 +0200

    coala.py: Remove superfluous space

--- coalib/coala.py
+++ coalib/coala.py
@@ -9,7 +9,7 @@
 # for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from coalib.output.ConsoleInteraction import (nothing_done,
                                               acquire_settings,
