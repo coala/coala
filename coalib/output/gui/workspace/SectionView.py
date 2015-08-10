938d9623c3f5514ed63e8e8db0c6747ed7e8f10f
Udayan Tandon <udayan12167@iiitd.ac.in>
2015-08-10 10:29:09 +0200

    coala-gui: Read coafile

--- coalib/output/gui/workspace/SectionView.py
+++ coalib/output/gui/workspace/SectionView.py
@@ -1,8 +1,11 @@
 from gi.repository import Gtk, Gdk
 
+from coalib.settings.Setting import Setting
+from coalib.output.ConfWriter import ConfWriter
+
 
 class SectionView(Gtk.ListBox):
-    def __init__(self):
+    def __init__(self, sections_dict, src):
         Gtk.ListBox.__init__(self, selection_mode=Gtk.SelectionMode.NONE)
 
         css_provider = Gtk.CssProvider()
@@ -15,26 +18,65 @@ class SectionView(Gtk.ListBox):
 
         self.get_style_context().add_class("settings-group")
 
-    def add_setting(self):
+        self.sections_dict = sections_dict
+        self.src = src
+
+    def add_setting(self, setting=None):
         setting_row_template = Gtk.Builder()
         setting_row_template.add_from_resource("/org/coala/SettingRow.ui")
         box = setting_row_template.get_object("setting_row")
         key_label = setting_row_template.get_object("name")
         delete_button = setting_row_template.get_object("delete_button")
-        key_label.connect("edited", self.on_key_changed, delete_button)
-
+        value_entry = setting_row_template.get_object("value")
+        key_label.connect("edited",
+                          self.on_key_changed,
+                          delete_button,
+                          value_entry)
         setting_row = Gtk.ListBoxRow()
         setting_row.get_style_context().add_class("setting")
         setting_row.add(box)
         setting_row.set_visible(True)
         delete_button.connect("clicked", self.delete_setting, setting_row)
+
+        if setting is not None:
+            key_label.label.set_label(setting.key)
+            value_entry.set_text(setting.value)
+            key_label.connect("edited", self.update_key, setting)
+            value_entry.connect("changed", self.update_value, setting)
+            delete_button.set_sensitive(True)
+            delete_button.set_name(setting.key)
+
         self.add(setting_row)
 
     def delete_setting(self, button, setting_row):
+        self.sections_dict[self.get_name()].delete_setting(button.get_name())
         setting_row.destroy()
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
 
-    def on_key_changed(self, widget, arg, sensitive_button):
+    def on_key_changed(self, widget, arg, sensitive_button, value_entry):
+        setting = Setting(widget.get_name(), value_entry.get_text())
+        self.sections_dict[self.get_name()].append(setting)
         self.add_setting()
         sensitive_button.set_sensitive(True)
-        widget.connect("edited", lambda a, b: None)
+        sensitive_button.set_name(setting.key)
+        widget.connect("edited", self.update_key, setting, sensitive_button)
+        value_entry.connect("focus-out-event", self.update_value, setting)
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
+
+    def update_key(self, widget, arg, setting, delete_button):
+        setting.key = widget.get_name()
+        delete_button.set_name(widget.get_name())
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
+
+    def update_value(self, widget, setting):
+        setting.value = widget.get_text()
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
 
--- coalib/output/gui/workspace/WorkspaceWindow.py
+++ coalib/output/gui/workspace/WorkspaceWindow.py
@@ -1,4 +1,12 @@
 from gi.repository import Gtk
+import os
+from collections import OrderedDict
+from coalib.settings.ConfigurationGathering import load_configuration
+from coalib.output.ConfWriter import ConfWriter
+from coalib.output.printers.LogPrinter import LogPrinter
+from coalib.output.printers.NullPrinter import NullPrinter
+from coalib.settings.Section import Section
+from coalib.misc.DictUtilities import update_ordered_dict_key
 
 from coalib.output.gui.workspace.SectionView import SectionView
 
@@ -13,8 +21,11 @@ class WorkspaceWindow(Gtk.ApplicationWindow):
         self._ui.add_from_resource("/org/coala/WorkspaceWindow.ui")
 
         self.section_stack_map = {}
+        self.sections_dict = OrderedDict()
+        self.src = src
 
         self._setup_view()
+        self.read_coafile()
 
     def _setup_view(self):
         self.headerbar = self._ui.get_object("headerbar")
@@ -30,41 +41,88 @@ class WorkspaceWindow(Gtk.ApplicationWindow):
         self.add_section_button = self._ui.get_object("add_section_button")
         self.add_section_button.connect("clicked", self.add_section)
 
-    def add_section(self, button=None):
+    def add_section(self, button=None, name=None):
         section_row_template = Gtk.Builder()
         section_row_template.add_from_resource('/org/coala/SectionRow.ui')
 
         section_row = Gtk.ListBoxRow()
         box = section_row_template.get_object("section_row")
         editable_label = section_row_template.get_object("name-edit")
-        editable_label.connect("edited", self.create_section_view, section_row)
         delete_button = section_row_template.get_object("delete_button")
+        if name is not None:
+            editable_label.entry.set_text(name)
+            self.create_section_view(widget=editable_label,
+                                     row_obejct=section_row)
+            editable_label.connect("edited",
+                                   self.update_section_name,
+                                   name,
+                                   self.section_stack_map[section_row])
+        else:
+            editable_label.connect("edited",
+                                   self.create_section_view,
+                                   section_row)
         section_row.add(box)
         section_row.set_visible(True)
         delete_button.connect("clicked", self.delete_row, section_row)
 
         self.section_switcher.add(section_row)
         self.section_switcher.queue_draw()
+        return section_row
 
     def delete_row(self, button, listboxrow):
+        del self.sections_dict[self.section_stack_map[listboxrow].get_name()]
+        self.section_stack_map[listboxrow].destroy()
+        del self.section_stack_map[listboxrow]
         listboxrow.destroy()
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
 
     def on_close(self, event, widget):
         self.get_application().greeter.show()
         self.destroy()
 
     def create_section_view(self, widget=None, arg=None, row_obejct=None):
-        section_view = SectionView()
+        section_view = SectionView(self.sections_dict, self.src)
         section_view.set_visible(True)
-        section_view.add_setting()
         section_view.set_name(widget.get_name())
         self.sections.add_named(section_view, widget.get_name())
         self.sections.set_visible_child_name(widget.get_name())
-        widget.connect("edited", self.set_section_view_name, section_view)
+        if arg is not None:
+            widget.connect("edited",
+                           self.update_section_name,
+                           widget.get_name(),
+                           section_view)
+            self.sections_dict[widget.get_name()] = Section(widget.get_name())
+            section_view.add_setting()
+            conf_writer = ConfWriter(self.src+'/.coafile')
+            conf_writer.write_sections(self.sections_dict)
+            conf_writer.close()
         self.section_stack_map[row_obejct] = section_view
-
-    def set_section_view_name(self, widget, arg, view):
-        view.set_name(widget.get_name())
 
     def on_row_selection_changed(self, listbox, row):
         self.sections.set_visible_child(self.section_stack_map[row])
+
+    def read_coafile(self):
+        if os.path.isfile(self.src+'/.coafile'):
+            self.sections_dict = load_configuration(
+                ["-c", self.src+'/.coafile'], LogPrinter(NullPrinter()))[0]
+            for section in self.sections_dict:
+                section_row = self.add_section(name=section)
+                for setting in self.sections_dict[section].contents:
+                    if "comment" in setting:
+                        continue
+                    self.section_stack_map[section_row].add_setting(
+                        self.sections_dict[section].contents[setting])
+                self.section_stack_map[section_row].add_setting()
+
+    def update_section_name(self, widget, arg, old_name, section_view):
+        section_view.set_name(widget.get_name())
+        self.sections_dict[old_name].name = widget.get_name()
+        self.sections_dict = update_ordered_dict_key(self.sections_dict,
+                                                     old_name,
+                                                     widget.get_name())
+        widget.connect("edited", self.update_section_name, widget.get_name())
+        conf_writer = ConfWriter(self.src+'/.coafile')
+        conf_writer.write_sections(self.sections_dict)
+        conf_writer.close()
