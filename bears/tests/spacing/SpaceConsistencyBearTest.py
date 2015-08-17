368d0ff09e3e4dd35efa99915dccb1b8fccf22e0
Makman2 <mak95@arcor.de>
2015-08-18 01:34:10 +0200

    SpaceConsistencyBearTest: Simplify
    Don't instantiate bears and sections again and again, setUp() does this
    before every test and Section is not immutable, we can change it after
    instantiating a bear with it.

--- bears/tests/spacing/SpaceConsistencyBearTest.py
+++ bears/tests/spacing/SpaceConsistencyBearTest.py
@@ -13,25 +13,26 @@ from coalib.settings.Section import Section
 class SpaceConsistencyBearTest(LocalBearTestHelper):
     def setUp(self):
         self.section = Section("test section")
-        self.section.append(Setting("use_spaces", "true"))
         self.uut = SpaceConsistencyBear(self.section, Queue())
 
     def test_needed_settings(self):
+        self.section.append(Setting("use_spaces", "true"))
+
         needed_settings = self.uut.get_non_optional_settings()
         self.assertEqual(len(needed_settings),
                          1 + len(SpacingHelper.get_non_optional_settings()))
         self.assertIn("use_spaces", needed_settings)
 
     def test_data_sets_spaces(self):
+        self.section.append(Setting("use_spaces", "true"))
+
         self.assertLineValid(self.uut, "    t")
         self.assertLineInvalid(self.uut, "t \n")
         self.assertLineInvalid(self.uut, "\tt\n")
 
     def test_data_sets_tabs(self):
-        self.section = Section("test section")
         self.section.append(Setting("use_spaces", "false"))
         self.section.append(Setting("allow_trailing_whitespace", "true"))
-        self.uut = SpaceConsistencyBear(self.section, Queue())
 
         self.assertLineInvalid(self.uut, "    t")
         self.assertLineValid(self.uut, "t \n")
