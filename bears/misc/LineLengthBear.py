fc1f5a5760a689a2bf1cf7aa34f6a8a57e571314
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-08-31 22:41:00 +0200

    Add Prototype
    This is a prototype for a bear decorator that allows creating a local
    bear which is automatically transformed into a global one. Furthermore
    it allows the user to create a bear out of a function instead of a
    class.

--- bears/misc/LineLengthBear.py
+++ bears/misc/LineLengthBear.py
@@ -1,30 +1,31 @@
 from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
-from coalib.bears.LocalBear import LocalBear
+from coalib.bears.LocalBear import LocalBearDecorator
 from coalib.results.Result import Result
 from coalib.misc.i18n import _
 
 
-class LineLengthBear(LocalBear):
-    def run(self,
-            filename,
+@LocalBearDecorator
+def linelen(filename,
             file,
             max_line_length: int,
             tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH):
-        '''
-        Yields results for all lines longer than the given maximum line length.
+    '''
+    Yields results for all lines longer than the given maximum line length.
 
-        :param max_line_length: Maximum number of characters for a line.
-        :param tab_width: Number of spaces to show for one tab.
-        '''
-        spacing_helper = SpacingHelper(tab_width)
+    :param max_line_length: Maximum number of characters for a line.
+    :param tab_width: Number of spaces to show for one tab.
+    '''
+    spacing_helper = SpacingHelper(tab_width)
 
-        for line_number, line in enumerate(file):
-            line = spacing_helper.replace_tabs_with_spaces(line)
-            if len(line) > max_line_length + 1:
-                yield Result(origin=self,
-                             message=_("Line is longer than allowed.") +
-                                     " ({actual} > {maximum})".format(
-                                         actual=len(line),
-                                         maximum=max_line_length),
-                             file=filename,
-                             line_nr=line_number + 1)
+    for line_number, line in enumerate(file):
+        line = spacing_helper.replace_tabs_with_spaces(line)
+        if len(line) > max_line_length + 1:
+            yield Result(origin="LineLengthBear",
+                         message=_("Line is longer than allowed.") +
+                                 " ({actual} > {maximum})".format(
+                                     actual=len(line),
+                                     maximum=max_line_length),
+                         file=filename,
+                         line_nr=line_number + 1)
+
+__additional_bears__ = [linelen]
