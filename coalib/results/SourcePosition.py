26d567346d3a8a9e349114e346b8f60ddcd57171
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-10-11 18:30:01 +0200

    SourcePosition: Add column

--- coalib/results/SourcePosition.py
+++ coalib/results/SourcePosition.py
@@ -3,20 +3,23 @@ from functools import total_ordering
 from coalib.misc.Decorators import generate_repr
 
 
-@generate_repr("file", "line")
+@generate_repr("file", "line", "column")
 @total_ordering
 class SourcePosition:
-    def __init__(self, file=None, line=None):
+    def __init__(self, file=None, line=None, column=None):
         """
         Creates a new result position object that represents the position of a
         result in the source code.
 
         :param file:            The filename or None.
-        :param line:            The line in file or None.
+        :param line:            The line in file or None, the first line is 1.
+        :param column:          The column indicating the character. The first
+                                one in a line is 1.
         :raises AssertionError: If a line number without a file is provided.
         """
         self.file = file
         self.line = line
+        self.column = column
 
     @property
     def line(self):
@@ -27,14 +30,25 @@ class SourcePosition:
         assert self.file is not None or other is None
         self._line = other
 
+    @property
+    def column(self):
+        return self._column
+
+    @column.setter
+    def column(self, other):
+        assert self.line is not None or other is None
+        self._column = other
+
     def __str__(self):
-        return "file: {}, line: {}".format(
+        return "file: {}, line: {}, column: {}".format(
             str(repr(self.file)),
-            str(self.line))
+            str(self.line),
+            str(self.column))
 
     def __eq__(self, other):
         return (self.file == other.file and
-                self.line == other.line)
+                self.line == other.line and
+                self.column == other.column)
 
     def __lt__(self, other):
         # Show elements without files first
@@ -51,5 +65,12 @@ class SourcePosition:
 
         if self.line != other.line:
             return self.line < other.line
+
+        # Show results with no or lesser column first
+        if (self.column is None) != (other.column is None):
+            return self.column is None
+
+        if self.column != other.column:
+            return self.column < other.column
 
         return False
