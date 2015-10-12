8f9ab1e8c5f7d46458093109a354435af055db66
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-10-12 14:15:58 +0200

    OpenEditorAction: Write to real file
    Fixes https://github.com/coala-analyzer/coala/issues/990

--- coalib/results/result_actions/OpenEditorAction.py
+++ coalib/results/result_actions/OpenEditorAction.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 import subprocess
 
 from coalib.results.Diff import Diff
@@ -25,25 +23,14 @@ class OpenEditorAction(ApplyPatchAction):
         return False
 
     def apply(self, result, original_file_dict, file_diff_dict, editor: str):
-        """
-        Open a temporary clone of the file in an editor.
+        '''
+        Open the file in an editor.
 
         :param editor: The editor to open the file with.
-        """
+        '''
         filename = result.file
-        original_file = original_file_dict[filename]
-        diff = file_diff_dict.get(filename, Diff())
-        current_file = diff.apply(original_file)
 
-        # Prefix is nice for the user so he has an indication that its the
-        # right file he's editing
-        temphandle, tempname = tempfile.mkstemp(
-            "_" + os.path.basename(filename))
-        os.close(temphandle)
-        with open(tempname, "w") as temphandle:
-            temphandle.writelines(current_file)
-
-        editor_args = [editor, tempname]
+        editor_args = [editor, filename]
         arg = EDITOR_ARGS.get(editor.strip(), None)
         if arg:
             editor_args.append(arg)
@@ -55,10 +42,12 @@ class OpenEditorAction(ApplyPatchAction):
         else:
             subprocess.call(editor_args)
 
-        with open(tempname) as temphandle:
-            new_file = temphandle.readlines()
+        with open(filename) as filehandle:
+            new_file = filehandle.readlines()
 
-        os.remove(tempname)
+        original_file = original_file_dict[filename]
+        diff = file_diff_dict.get(filename, Diff())
+        current_file = diff.apply(original_file)
 
         intermediate_diff = Diff.from_string_arrays(current_file, new_file)
         file_diff_dict[filename] = diff + intermediate_diff
