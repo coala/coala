438b9b91eb24b21d97129d8ab838258a5003a7d4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-10-15 10:38:37 +0200

    generate_ordering: Use for SourcePosition/Range

--- coalib/tests/results/SourcePositionTest.py
+++ coalib/tests/results/SourcePositionTest.py
@@ -31,32 +31,6 @@ class SourcePositionTest(unittest.TestCase):
             "<SourcePosition object\\(file='None', line=None, column=None\\) "
                 "at 0x[0-9a-fA-F]+>")
 
-    def test_equality(self):
-        self.assert_equal(SourcePosition("some"),
-                          SourcePosition("some", None, None))
-        self.assert_equal(SourcePosition("some", 4),
-                          SourcePosition("some", 4, None))
-        self.assert_equal(SourcePosition("some", 4, 5),
-                          SourcePosition("some", 4, 5))
-
-    def test_file_ordering(self):
-        self.assert_ordering(SourcePosition("b file", None),
-                             SourcePosition("a file", None))
-        self.assert_ordering(SourcePosition("b file", 0),
-                             SourcePosition("a file", 4))
-
-    def test_line_ordering(self):
-        self.assert_ordering(SourcePosition("a file", 4),
-                             SourcePosition("a file", 0))
-        self.assert_ordering(SourcePosition("a file", 4),
-                             SourcePosition("a file", None))
-
-    def test_column_ordering(self):
-        self.assert_ordering(SourcePosition("a file", 4, 3),
-                             SourcePosition("a file", 4, 2))
-        self.assert_ordering(SourcePosition("a file", 4, 3),
-                             SourcePosition("a file", 4, None))
-
 
     def assert_equal(self, first, second):
         self.assertGreaterEqual(first, second)
