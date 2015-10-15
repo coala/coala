47dd65dfa59fe04174b2cb4aa05df6435c85ced4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-10-15 10:38:37 +0200

    generate_ordering: Use for SourcePosition/Range

--- coalib/results/SourceRange.py
+++ coalib/results/SourceRange.py
@@ -1,11 +1,9 @@
-from functools import total_ordering
-
-from coalib.misc.Decorators import generate_repr
+from coalib.misc.Decorators import generate_repr, generate_ordering
 from coalib.results.SourcePosition import SourcePosition
 
 
 @generate_repr("start", "end")
-@total_ordering
+@generate_ordering("start", "end")
 class SourceRange:
     def __init__(self, start, end=None):
         """
@@ -55,19 +53,3 @@ class SourceRange:
     @property
     def end(self):
         return self._end
-
-    def __eq__(self, other):
-        if not isinstance(other, SourceRange):
-            return False
-
-        return self.start == other.start and self.end == other.end
-
-    def __lt__(self, other):
-        if not isinstance(other, SourceRange):
-            raise TypeError("Cannot compare SourceRange with a variable of"
-                            "another type")
-
-        if self.start != other.start:
-            return self.start < other.start
-
-        return self.end < other.end
