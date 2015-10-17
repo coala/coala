439d79977e86235a4f7e6ec8aa274520674f7cad
Makman2 <mak95@arcor.de>
2015-10-18 01:19:12 +0200

    doc-extraction: Implement doctype 'continuous'
    Contains algorithms and tests. Also extends the already existing
    Python test since the doxygen style continuous markers are now
    supported.

---
+++ coalib/tests/bearlib/languages/documentation/documentation_extraction_testdata/data.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+
+/**
+ * This is the main function.
+ * @returns Exit code.
+ *          Or any other number.
+ */
+int main()
+{
+    // Your main code here.
+    return 0;
+}
+
+/** foobar
+ * @param xyz
+ */
+int foobar(int xyz)  {
+    return xyz * (1 - xyz)
+}
+
+/// Some alternate style of documentation
+void alternatestyle()
+{}
+
+/// Should work
+///
+/// even without a function standing below.
+///
+/// @param foo WHAT PARAM PLEASE!?
