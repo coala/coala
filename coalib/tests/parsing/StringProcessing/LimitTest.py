f94625cf73097c4060ec2a026c58c871d82c67f0
Makman2 <makman@alice.de>
2015-10-23 21:42:29 +0200

    StringProcessing: Add test for `limit` filter

---
+++ coalib/tests/parsing/StringProcessing/LimitTest.py
@@ -0,0 +1,23 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.parsing.StringProcessing.Filters import limit
+
+
+class LimitTest(unittest.TestCase):
+    sequence = (1, 5, 19, 22, -3, 18, 99, 500, 2015)
+
+    def test_finite(self):
+        for test_limit in (1, 2, 3, 7, 8, 10, 22, 500000):
+            self.assertEqual(tuple(limit(self.sequence, test_limit)),
+                             self.sequence[0:test_limit])
+
+    def test_infinite(self):
+        for test_limit in (0, -1, -2, -6555123):
+            self.assertEqual(tuple(limit(self.sequence, test_limit)),
+                             self.sequence)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
