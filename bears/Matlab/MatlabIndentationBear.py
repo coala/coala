1b9c4adc44c285533cfe78e388054778107fe24e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-11-17 18:57:14 +0100

    MatlabIndentationBear: Report changes in chunks
    This is better than each line on its own.

--- bears/Matlab/MatlabIndentationBear.py
+++ bears/Matlab/MatlabIndentationBear.py
@@ -15,22 +15,31 @@ class MatlabIndentationBear(LocalBear):
 
         :param indentation: Number of spaces per indentation level.
         """
+        new_file = list(self.reindent(file, indentation))
+
+        if new_file != file:
+            wholediff = Diff.from_string_arrays(file, new_file)
+            for diff in wholediff.split_diff():
+                yield Result(
+                    self,
+                    'The indentation could be changed to improve readability.',
+                    severity=RESULT_SEVERITY.INFO,
+                    affected_code=(diff.range(filename),),
+                    diffs={filename: diff})
+
+    @staticmethod
+    def reindent(file, indentation):
         indent, nextindent = 0, 0
         for line_nr, line in enumerate(file, start=1):
             indent = nextindent
-            indent, nextindent = self.get_indent(line, indent, nextindent)
+            indent, nextindent = MatlabIndentationBear.get_indent(line,
+                                                                  indent,
+                                                                  nextindent)
             stripped = line.lstrip()
-            new_line = indent*indentation*' ' + stripped
-            if stripped and new_line != line:
-                diff = Diff(file)
-                diff.change_line(line_nr, line, new_line)
-                yield Result.from_values(
-                    self,
-                    'Indentation is wrong.',
-                    severity=RESULT_SEVERITY.INFO,
-                    file=filename,
-                    line=line_nr,
-                    diffs={filename: diff})
+            if stripped:
+                yield indent*indentation*' ' + stripped
+            else:
+                yield line
 
     @staticmethod
     def get_indent(line, indent, nextindent):
