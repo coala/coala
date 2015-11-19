060d3e00a4d246b4a302aa182b57c8e6b3ae9e3e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-11-19 16:50:40 +0100

    LanguageToolBear: Return full SourceRange
    Finally the LanguageToolBear can highlight the actual affected word.

--- bears/linters/LanguageToolBear.py
+++ bears/linters/LanguageToolBear.py
@@ -3,26 +3,30 @@ from language_check import LanguageTool, correct
 from coalib.bears.LocalBear import LocalBear
 from coalib.results.Result import Result
 from coalib.results.Diff import Diff
+from coalib.results.SourceRange import SourceRange
 
 
-def get_language_tool_results(file_contents, locale):
+def get_language_tool_results(filename, file_contents, locale):
     tool = LanguageTool(locale)
     joined_text = "".join(file_contents)
     matches = tool.check(joined_text)
     for match in matches:
         if not match.replacements:
-            diff = None
+            diffs = None
         else:
             replaced = correct(joined_text, [match]).splitlines(True)
-            diff = Diff.from_string_arrays(file_contents, replaced)
+            diffs = {filename: Diff.from_string_arrays(file_contents, replaced)}
 
         rule_id = match.ruleId
         if match.subId is not None:
             rule_id += '[{}]'.format(match.subId)
 
-        message = (match.msg + ' (' + rule_id + ', ' +
-                   'Found at column {col}.').format(col=match.fromx+1) + ')'
-        yield message, diff, match.fromy+1
+        message = match.msg + ' (' + rule_id + ')'
+        yield message, diffs, SourceRange.from_values(filename,
+                                                      match.fromy+1,
+                                                      match.fromx+1,
+                                                      match.toy+1,
+                                                      match.tox+1)
 
 
 class LanguageToolBear(LocalBear):
@@ -35,15 +39,6 @@ class LanguageToolBear(LocalBear):
 
         locale: A locale representing the language you want to have checked.
         '''
-        for message, diff, line in get_language_tool_results(file, locale):
-            if diff:
-                yield Result.from_values(self,
-                                         message,
-                                         diffs={filename: diff},
-                                         file=filename,
-                                         line=line)
-            else:
-                yield Result.from_values(self,
-                                         message,
-                                         file=filename,
-                                         line=line)
+        for message, diffs, range in get_language_tool_results(
+                filename, file, locale):
+            yield Result(self, message, diffs=diffs, affected_code=(range,))
