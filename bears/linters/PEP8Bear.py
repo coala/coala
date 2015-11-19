9fad04908be1b3eb9f46f2e5577ea0ece42d10cf
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-11-19 17:20:46 +0100

    PEP8Bear: Add ignore/select options

--- bears/linters/PEP8Bear.py
+++ bears/linters/PEP8Bear.py
@@ -1,17 +1,36 @@
 import autopep8
 
 from bears.linters.CorrectionBasedBear import CorrectionBasedBear
+from coalib.settings.Setting import typed_list
 
 
 class PEP8Bear(CorrectionBasedBear):
-    GET_REPLACEMENT = staticmethod(
-        lambda file: (autopep8.fix_code(''.join(file)).splitlines(True), []))
     RESULT_MESSAGE = "The code does not comply to PEP8."
 
-    def run(self, filename, file):
+    @staticmethod
+    def run_autopep(file, pep_ignore, pep_select):
+        new_file = autopep8.fix_code(''.join(file),
+                                     options={'ignore': pep_ignore,
+                                              'select': pep_select})
+
+        return new_file.splitlines(True), []
+
+    GET_REPLACEMENT = run_autopep
+
+    def run(self,
+            filename,
+            file,
+            pep_ignore: typed_list(str)=(),
+            pep_select: typed_list(str)=()):
         """
         Detects and fixes PEP8 incompliant code. This bear will not change
         functionality of the code in any way.
+
+        :param pep_ignore: A list of errors/warnings to ignore.
+        :param pep_select: A list of errors/warnings to exclusively apply.
         """
-        for result in self.retrieve_results(filename, file):
+        for result in self.retrieve_results(filename,
+                                            file,
+                                            pep_ignore=pep_ignore,
+                                            pep_select=pep_select):
             yield result
--- bears/tests/linters/PEP8BearTest.py
+++ bears/tests/linters/PEP8BearTest.py
@@ -6,15 +6,27 @@ sys.path.insert(0, ".")
 from bears.tests.LocalBearTestHelper import LocalBearTestHelper
 from bears.linters.PEP8Bear import PEP8Bear
 from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
 
 
 class PEP8BearTest(LocalBearTestHelper):
     def setUp(self):
-        self.uut = PEP8Bear(Section('name'), Queue())
+        self.section = Section('name')
+        self.uut = PEP8Bear(self.section, Queue())
 
     def test_valid(self):
         self.assertLinesValid(self.uut, ["import sys"])
         self.assertLinesValid(self.uut, ["a = 1 + 1"])
+
+    def test_disable_warnings(self):
+        test_code = ['def func():\n',
+                     '    pass\n',
+                     'def func2():\n',
+                     '    pass\n']
+        self.assertLinesInvalid(self.uut, test_code)
+
+        self.section.append(Setting('pep_ignore', 'E302'))
+        self.assertLinesValid(self.uut, test_code)
 
     def test_invalid(self):
         self.assertLinesInvalid(self.uut, [""])
