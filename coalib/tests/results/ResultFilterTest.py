c9e7b1ebf845d5bd8ace2ebd7764764a309b9e04
Fabian Neuschmidt <fabian@neuschmidt.de>
2015-11-24 22:58:53 +0100

    ResultFilter: Add check for basic properties

--- coalib/tests/results/ResultFilterTest.py
+++ coalib/tests/results/ResultFilterTest.py
@@ -3,6 +3,8 @@ import sys
 import unittest
 
 sys.path.insert(0, ".")
+from coalib.results.ResultFilter import filter_results
+from coalib.results.Result import Result, RESULT_SEVERITY
 
 
 class ResultFilterTest(unittest.TestCase):
@@ -15,7 +17,66 @@ class ResultFilterTest(unittest.TestCase):
                                                'modified_file.txt')
 
     def test_simple_cases(self):
-        self.assertTrue(True)
+        class Origin:
+            pass
+
+        origin_instance = Origin()
+
+        original_result = Result.from_values(origin=origin_instance,
+                                             message="original",
+                                             file="original",
+                                             severity=RESULT_SEVERITY.NORMAL,
+                                             debug_msg="original")
+
+        clone_result = Result.from_values(origin="Origin",
+                                          message="original",
+                                          file="original",
+                                          severity=RESULT_SEVERITY.NORMAL,
+                                          debug_msg="original")
+
+        wrong_origin_result = Result.from_values(
+            origin="AnotherOrigin",
+            message="original",
+            file="original",
+            severity=RESULT_SEVERITY.NORMAL,
+            debug_msg="original")
+
+        wrong_message_result = Result.from_values(
+            origin="Origin",
+            message="another message",
+            file="original",
+            severity=RESULT_SEVERITY.NORMAL,
+            debug_msg="original")
+
+        wrong_severity_result = Result.from_values(
+            origin="Origin",
+            message="original",
+            file="original",
+            severity=RESULT_SEVERITY.INFO,
+            debug_msg="original")
+
+        wrong_debug_msg_result = Result.from_values(
+            origin="Origin",
+            message="original",
+            file="original",
+            severity=RESULT_SEVERITY.NORMAL,
+            debug_msg="another debug message")
+
+        file_dict = {"original": []}
+
+        self.assertEqual(filter_results(original_file_dict=file_dict,
+                                        modified_file_dict=file_dict,
+                                        original_results=[original_result],
+                                        modified_results=[
+                                            clone_result,
+                                            wrong_origin_result,
+                                            wrong_message_result,
+                                            wrong_severity_result,
+                                            wrong_debug_msg_result]),
+                         [wrong_origin_result,
+                          wrong_message_result,
+                          wrong_severity_result,
+                          wrong_debug_msg_result])
 
     def test_affected_code(self):
         self.assertTrue(True)
