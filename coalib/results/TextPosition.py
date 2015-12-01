54a5f1bd69e244dfa3bb56fe1a7d7a51bee764b4
Makman2 <makman@alice.de>
2015-12-01 01:04:32 +0100

    Add TextPosition class
    The TextPosition class is the context-less version of SourcePosition
    that does not require a file.
    
    SourcePosition will use this class later as base class.

---
+++ coalib/results/TextPosition.py
@@ -0,0 +1,33 @@
+from coalib.misc.Decorators import (enforce_signature,
+                                    generate_ordering,
+                                    generate_repr)
+
+
+@generate_repr("line", "column")
+@generate_ordering("line", "column")
+class TextPosition:
+    @enforce_signature
+    def __init__(self, line: (int, None)=None, column: (int, None)=None):
+        """
+        Creates a new TextPosition object that represents the position inside
+        a string with line/column numbers.
+
+        :param line:        The line in file or None, the first line is 1.
+        :param column:      The column indicating the character. The first one
+                            in a line is 1.
+        :raises TypeError:  Raised when line or columns are no integers.
+        :raises ValueError: Raised when a column is set but line is None.
+        """
+        if line is None and column is not None:
+            raise ValueError("A column can only be set if a line is set.")
+
+        self._line = line
+        self._column = column
+
+    @property
+    def line(self):
+        return self._line
+
+    @property
+    def column(self):
+        return self._column
---
+++ coalib/tests/results/TextPositionTest.py
@@ -0,0 +1,37 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.results.TextPosition import TextPosition
+
+
+class TextPositionTest(unittest.TestCase):
+    def test_fail_instantation(self):
+        with self.assertRaises(ValueError):
+            TextPosition(None, 2)
+
+        with self.assertRaises(TypeError):
+            TextPosition("hello", 3)
+
+        with self.assertRaises(TypeError):
+            TextPosition(4, "world")
+
+        with self.assertRaises(TypeError):
+            TextPosition("double", "string")
+
+    def test_properties(self):
+        uut = TextPosition(None, None)
+        self.assertEqual(uut.line, None)
+        self.assertEqual(uut.column, None)
+
+        uut = TextPosition(7, None)
+        self.assertEqual(uut.line, 7)
+        self.assertEqual(uut.column, None)
+
+        uut = TextPosition(8, 39)
+        self.assertEqual(uut.line, 8)
+        self.assertEqual(uut.column, 39)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
