74db9ddde0c99b33a09140ac2adc520b5cba5b28
Makman2 <mak95@arcor.de>
2015-12-01 18:48:30 +0100

    SourcePosition: Fix __init__ documentation

--- coalib/results/SourcePosition.py
+++ coalib/results/SourcePosition.py
@@ -13,12 +13,13 @@ class SourcePosition(TextPosition):
         Creates a new result position object that represents the position of a
         result in the source code.
 
-        :param file:        The filename or None.
+        :param file:        The filename.
         :param line:        The line in file or None, the first line is 1.
         :param column:      The column indicating the character. The first one
                             in a line is 1.
-        :raises TypeError:  Raised when line or columns are no integers.
-        :raises ValueError: If a line number without a file is provided.
+        :raises TypeError:  Raised when
+                            - file is not a string or None.
+                            - line or columns are no integers.
         """
         TextPosition.__init__(self, line, column)
 
