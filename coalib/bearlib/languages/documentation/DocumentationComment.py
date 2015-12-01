27b9040d2d436c58ab0c2bef01c7131d611df3de
Makman2 <makman@alice.de>
2015-12-01 01:44:34 +0100

    Add DocumentationComment class
    This class contains information about a documentation comment inside a
    code file.

---
+++ coalib/bearlib/languages/documentation/DocumentationComment.py
@@ -0,0 +1,26 @@
+from coalib.misc.Decorators import generate_eq, generate_repr
+
+
+@generate_repr()
+@generate_eq("documentation", "marker", "range")
+class DocumentationComment:
+    """
+    The DocumentationComment holds information about a documentation comment
+    inside source-code, like position etc.
+    """
+
+    def __init__(self, documentation, marker, range):
+        """
+        Instantiates a new DocumentationComment.
+
+        :param documentation: The documentation text.
+        :param marker:        The three-element tuple with marker strings that
+                              identified this documentation comment.
+        :param range:         The position range of type TextRange.
+        """
+        self.documentation = documentation
+        self.marker = marker
+        self.range = range
+
+    def __str__(self):
+        return self.documentation
---
+++ coalib/tests/bearlib/languages/documentation/DocumentationCommentTest.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+
+sys.path.insert(0, ".")
+from coalib.bearlib.languages.documentation.DocumentationComment import (
+    DocumentationComment)
+
+
+class DocumentationCommentTest(unittest.TestCase):
+    def test_fields(self):
+        uut = DocumentationComment("my doc",
+                                   ("/**", "*", "*/"),
+                                   (25, 45))
+
+        self.assertEqual(uut.documentation, "my doc")
+        self.assertEqual(str(uut), "my doc")
+        self.assertEqual(uut.marker, ("/**", "*", "*/"))
+        self.assertEqual(uut.range, (25, 45))
+
+        uut = DocumentationComment("qwertzuiop",
+                                   ("##", "#", "#"),
+                                   None)
+
+        self.assertEqual(uut.documentation, "qwertzuiop")
+        self.assertEqual(str(uut), "qwertzuiop")
+        self.assertEqual(uut.marker, ("##", "#", "#"))
+        self.assertEqual(uut.range, None)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
