43a3338440f857c9f0a0e2da6e8be872e60fd4b9
Fabian Neuschmidt <fabian@neuschmidt.de>
2015-12-02 12:59:04 +0100

    ResultFilter: Add source_range_check

--- coalib/results/ResultFilter.py
+++ coalib/results/ResultFilter.py
@@ -1,5 +1,7 @@
 import copy
 
+from coalib.results.Diff import Diff, ConflictError
+
 
 def filter_results(original_file_dict,
                    modified_file_dict,
@@ -15,10 +17,72 @@ def filter_results(original_file_dict,
     :return:                   List of results from new files that are unique
                                from all those that existed in the old changes
     """
+    # diffs_dict[file] is a diff between the original and modified file
+    diffs_dict = {}
+    for file in original_file_dict:
+        diffs_dict[file] = Diff.from_string_arrays(original_file_dict[file],
+                                                  modified_file_dict[file])
+
+    #  orig_result_diff_dict_dict[result][file] is a diff of the changes this
+    # result would apply to the file
+
+    orig_result_diff_dict_dict = {}
+    for original_result in original_results:
+        orig_file_dict_copy = copy.deepcopy(original_file_dict)
+
+        for source_range in original_result.affected_code:
+            file_name = source_range.file
+            new_file = remove_range(orig_file_dict_copy[file_name],
+                                    source_range)
+            orig_file_dict_copy[file_name] = new_file
+
+        diff_dict = {}
+        for file_name in original_file_dict:
+            diff_dict[file_name] = Diff.from_string_arrays(
+                original_file_dict[file_name],
+                orig_file_dict_copy[file_name])
+
+        orig_result_diff_dict_dict[original_result] = diff_dict
+
+    # same thing for modified results. and yes this should really be in a
+    # function... but as long as both of these are not, code clone detection
+    # won't find them >:)
+    # fixme: yeah....
+
+    mod_result_diff_dict_dict = {}
+    for modified_result in modified_results:
+        mod_file_dict_copy = copy.deepcopy(modified_file_dict)
+
+        for source_range in modified_result.affected_code:
+            file_name = source_range.file
+            new_file = remove_range(mod_file_dict_copy[file_name],
+                                    source_range)
+            mod_file_dict_copy[file_name] = new_file
+
+        diff_dict = {}
+        for file_name in modified_file_dict:
+            diff_dict[file_name] = Diff.from_string_arrays(
+                modified_file_dict[file_name],
+                mod_file_dict_copy[file_name])
+
+        mod_result_diff_dict_dict[modified_result] = diff_dict
+
     uniques = []
+
+    # THIS IS WHERE THE FUN BEGINS
 
     for m_r in modified_results:
         if True not in [basics_match(o_r, m_r) for o_r in original_results]:
+            uniques.append(m_r)
+
+        elif True not in [source_ranges_match(
+                original_file_dict,
+                modified_file_dict,
+                diffs_dict,
+                o_r,
+                orig_result_diff_dict_dict[o_r],
+                m_r,
+                mod_result_diff_dict_dict[m_r]) for o_r in original_results]:
             uniques.append(m_r)
 
     return uniques
@@ -61,19 +125,39 @@ def basics_match(original_result,
 
 
 def source_ranges_match(original_file_dict,
+                        modified_file_dict,
                         diff_dict,
                         original_result,
-                        modified_result):
+                        original_result_diff_dict,
+                        modified_result,
+                        modified_result_diff_dict):
     """
     Checks whether the SourceRanges of two results match
 
-    :param original_file_dict: Dict of lists of file contents before the changes
-    :param diff_dict:          Dict of diffs describing the change in each file
+    :param original_file_dict: Dict of lists of file contents before changes
+    :param modified_file_dict: Dict of lists of file contents after changes
+    :param diff_dict:          Dict of diffs describing the changes per file
     :param original_result:    A result of the old files
+    :param original_result_diff_dict: diff for each file for this result
     :param modified_result:    A result of the new files
-    :return:                   Boolean value whether the SourceRanges match
+    :param modified_result_diff_dict: guess
+    :return:                     Boolean value whether the SourceRanges match
     """
-    return False # pragma: no cover
+    for file_name in original_file_dict:
+
+        try:  # fails if the affected range of the result get's modified
+            original_total_diff = \
+                diff_dict[file_name] + original_result_diff_dict[file_name]
+        except ConflictError:
+            return False
+
+        # original file with file_diff and original_diff applied
+        original_total_file = original_total_diff.modified
+        # modified file with modified_diff applied
+        modified_total_file = modified_result_diff_dict[file_name].modified
+        if original_total_file != modified_total_file:
+            return False
+    return True
 
 
 def diffs_match(original_file_dict,
@@ -103,6 +187,11 @@ def remove_range(file_contents, source_range):
     :return:              list of file contents without specified chars removed
     """
 
+    # fixme: line or column could be None -.-
+    # this is not the fixing it deserves, but the one it needs right now:
+    if source_range.start.line is None:
+        return copy.deepcopy(file_contents)
+
     newfile = copy.deepcopy(file_contents)
     # attention: line numbers in the SourceRange are human-readable,
     # list indices start with 0
@@ -129,4 +218,3 @@ def remove_range(file_contents, source_range):
             del newfile[i]
 
     return newfile
-
