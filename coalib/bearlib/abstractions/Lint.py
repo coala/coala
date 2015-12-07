14846412b8f70537784f63a0a966368ec3adf6da
AbdealiJK <abdealikothari@gmail.com>
2015-12-07 14:29:02 +0530

    abstractions: Add a Linter abstraction
    There are already many linters which exist. These linters should be
    given a simple intuitive interface to integrate themselves with coala.
    This commit adds the Lint class which helps to abstract the execution
    and parsing of output from a linter.

---
+++ coalib/bearlib/abstractions/Lint.py
@@ -0,0 +1,110 @@
+import subprocess
+import tempfile
+import re
+import sys
+
+from coalib.misc.Shell import escape_path_argument
+from coalib.results.Result import Result
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class Lint():
+    """
+    :param executable:   The executable to run the linter.
+    :param arguments:    The arguments to supply to the linter, such
+                         that the file name to be analyzed can be
+                         appended to the end.
+    :param output_regex: The regex which will match the output of the linter
+                         to get results. Thie regex should give out the
+                         following variables:
+                          line - The line where the issue starts.
+                          column - The column where the issue starts.
+                          end_line - The line where the issue ends.
+                          end_column - The column where the issue ends.
+                          severity - The severity of the issue.
+                          message - The message of the result.
+    :param use_stderr:   Uses stderr as the output stream is it's True.
+    :param severity_map: A dict where the keys are the possible severity
+                         values the Linter gives out and the values are the
+                         severity of the coala Result to set it to. If it is
+                         not a dict, it is ignored.
+    """
+    executable = None
+    arguments = ""
+    output_regex = re.compile(r'(?P<line>\d+)\.(?P<column>\d+)\|'
+                              r'(?P<severity>\d+): (?P<message>.*)')
+    use_stderr = False
+    severity_map = None
+
+    def lint(self, filename):
+        """
+        Takes a file and lints it using the linter variables defined apriori.
+
+        :param filename: The name of the file to execute.
+        """
+        command = (self.executable + ' ' + self.arguments + ' '
+                   + escape_path_argument(filename))
+        stderr_file = tempfile.TemporaryFile()
+        stdout_file = tempfile.TemporaryFile()
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=stdout_file,
+            stderr=stderr_file,
+            universal_newlines=True)
+        process.wait()
+        if self.use_stderr:
+            stderr_file.seek(0)
+            output = stderr_file.read().decode(sys.stdout.encoding,
+                                               errors="replace")
+        else:
+            stdout_file.seek(0)
+            output = stdout_file.read().decode(sys.stdout.encoding,
+                                               errors="replace")
+        stdout_file.close()
+        stderr_file.close()
+        return self.process_output(output, filename)
+
+    def process_output(self, output, filename):
+        regex = self.output_regex
+        if isinstance(regex, str):
+            regex = regex % {"file_name": filename}
+
+        for match in re.finditer(regex, output):
+            yield self.match_to_result(match, filename)
+
+    def _get_groupdict(self, match):
+        groups = match.groupdict()
+        if (
+                isinstance(self.severity_map, dict) and
+                "severity" in groups and
+                groups["severity"] in self.severity_map):
+            groups["severity"] = self.severity_map[groups["severity"]]
+        return groups
+
+    def match_to_result(self, match, filename):
+        """
+        Converts a regex match's groups into a result.
+
+        :param match:    The match got from regex parsing.
+        :param filename: The name of the file from which this match is got.
+        """
+        groups = self._get_groupdict(match)
+
+        # Pre process the groups
+        end_line = groups.get("end_line", None)
+        if end_line != None:
+            end_line = int(end_line)
+        end_column = groups.get("end_column", None)
+        if end_column != None:
+            end_column = int(end_column)
+
+        return Result.from_values(
+            origin=self,
+            message=groups.get("message", ""),
+            file=filename,
+            severity=int(groups.get("severity", RESULT_SEVERITY.NORMAL)),
+            line=int(groups.get("line")),
+            column=int(groups.get("column", 0)),
+            end_line=end_line,
+            end_column=end_column)
---
+++ coalib/tests/bearlib/abstractions/LintTest.py
@@ -0,0 +1,61 @@
+import sys
+import unittest
+
+sys.path.insert(0, ".")
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.results.SourceRange import SourceRange
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class LintTest(unittest.TestCase):
+    def test_process_output(self):
+        self.uut = Lint()
+        out = list(self.uut.process_output(
+            "1.0|0: Info message\n"
+            "2.2|1: Normal message\n"
+            "3.4|2: Major message\n",
+            "a/file.py"))
+        self.assertEqual(len(out), 3)
+        self.assertEqual(out[0].origin, "Lint")
+
+        self.assertEqual(out[0].affected_code[0],
+                         SourceRange.from_values("a/file.py", 1, 0))
+        self.assertEqual(out[0].severity, RESULT_SEVERITY.INFO)
+        self.assertEqual(out[0].message, "Info message")
+
+        self.assertEqual(out[1].affected_code[0],
+                         SourceRange.from_values("a/file.py", 2, 2))
+        self.assertEqual(out[1].severity, RESULT_SEVERITY.NORMAL)
+        self.assertEqual(out[1].message, "Normal message")
+
+        self.assertEqual(out[2].affected_code[0],
+                         SourceRange.from_values("a/file.py", 3, 4))
+        self.assertEqual(out[2].severity, RESULT_SEVERITY.MAJOR)
+        self.assertEqual(out[2].message, "Major message")
+
+        self.uut = Lint()
+        self.uut.output_regex = (r'(?P<line>\d+)\.(?P<column>\d+)\|'
+                                 r'(?P<end_line>\d+)\.(?P<end_column>\d+)\|'
+                                 r'(?P<severity>\d+): (?P<message>.*)')
+        self.uut.severity_map = {"I": RESULT_SEVERITY.INFO}
+        out = list(self.uut.process_output(
+            "1.0|2.3|0: Info message\n",
+            'a/file.py'))
+        self.assertEqual(len(out), 1)
+        self.assertEqual(out[0].affected_code[0].start.line, 1)
+        self.assertEqual(out[0].affected_code[0].start.column, 0)
+        self.assertEqual(out[0].affected_code[0].end.line, 2)
+        self.assertEqual(out[0].affected_code[0].end.column, 3)
+        self.assertEqual(out[0].severity, RESULT_SEVERITY.INFO)
+
+        self.uut = Lint()
+        out = list(self.uut.process_output(
+            "Random line that shouldn't be captured\n"
+            "*************\n",
+            'a/file.py'))
+        self.assertEqual(len(out), 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
