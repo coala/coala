337f4811ebf27a7a68e1edd740bedd3a42bdeaf8
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-12-12 00:12:42 +0100

    DockerfileLintBear: Move to configfiles package

--- bears/tests/configfiles/DockerfileLintBearTest.py
+++ bears/tests/configfiles/DockerfileLintBearTest.py
@@ -6,7 +6,7 @@ from queue import Queue
 sys.path.insert(0, ".")
 import unittest
 from bears.tests.LocalBearTestHelper import LocalBearTestHelper
-from bears.linters.DockerfileLintBear import DockerfileLintBear
+from bears.configfiles.DockerfileLintBear import DockerfileLintBear
 from coalib.settings.Section import Section
 
 
