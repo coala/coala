1ec4469474e537296d6b6aebe8f49e1d464b1720
Makman2 <mak95@arcor.de>
2015-12-20 01:02:42 +0100

    Exceptions.get_exitcode: Little performance gain
    Moving the `exitcode = 0` assignment into an else clause boosts this
    function a little, since now in every case only a single assignment is
    performed.

--- coalib/misc/Exceptions.py
+++ coalib/misc/Exceptions.py
@@ -6,16 +6,19 @@ from coalib.misc.Constants import Constants
 
 def get_exitcode(exception, log_printer=None):
     log_printer = log_printer or LogPrinter(NullPrinter())
-    exitcode = 0
+
     if isinstance(exception, KeyboardInterrupt):  # Ctrl+C
         print("Program terminated by user.")
         exitcode = 130
     elif isinstance(exception, EOFError):  # Ctrl+D
         print("Found EOF. Exiting gracefully.")
+        exitcode = 0
     elif isinstance(exception, SystemExit):
         exitcode = exception.code
     elif isinstance(exception, BaseException):
         log_printer.log_exception(Constants.CRASH_MESSAGE, exception)
         exitcode = 255
+    else:
+        exitcode = 0
 
     return exitcode
