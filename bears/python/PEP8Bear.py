ecd49d2b1807ed93e56a4fbfdc29d681688dfae9
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-12-20 18:26:00 +0100

    PEP8Bear: Obey max_line_length and tab_width

--- bears/python/PEP8Bear.py
+++ bears/python/PEP8Bear.py
@@ -1,6 +1,7 @@
 import autopep8
 
 from coalib.bearlib.abstractions.CorrectionBasedBear import CorrectionBasedBear
+from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
 from coalib.settings.Setting import typed_list
 
 
@@ -8,10 +9,17 @@ class PEP8Bear(CorrectionBasedBear):
     RESULT_MESSAGE = "The code does not comply to PEP8."
 
     @staticmethod
-    def run_autopep(file, pep_ignore, pep_select):
-        new_file = autopep8.fix_code(''.join(file),
-                                     options={'ignore': pep_ignore,
-                                              'select': pep_select})
+    def run_autopep(file,
+                    pep_ignore,
+                    pep_select,
+                    max_line_length,
+                    indent_size):
+        new_file = autopep8.fix_code(
+            ''.join(file),
+            options={'ignore': pep_ignore,
+                     'select': pep_select,
+                     'max_line_length': max_line_length,
+                     'indent_size': indent_size})
 
         return new_file.splitlines(True), []
 
@@ -20,17 +28,23 @@ class PEP8Bear(CorrectionBasedBear):
     def run(self,
             filename,
             file,
+            max_line_length: int=80,
+            tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
             pep_ignore: typed_list(str)=(),
             pep_select: typed_list(str)=()):
         """
         Detects and fixes PEP8 incompliant code. This bear will not change
         functionality of the code in any way.
 
-        :param pep_ignore: A list of errors/warnings to ignore.
-        :param pep_select: A list of errors/warnings to exclusively apply.
+        :param max_line_length: Maximum number of characters for a line.
+        :param tab_width:       Number of spaces per indent level.
+        :param pep_ignore:      A list of errors/warnings to ignore.
+        :param pep_select:      A list of errors/warnings to exclusively apply.
         """
         for result in self.retrieve_results(filename,
                                             file,
                                             pep_ignore=pep_ignore,
-                                            pep_select=pep_select):
+                                            pep_select=pep_select,
+                                            max_line_length=max_line_length,
+                                            indent_size=tab_width):
             yield result
--- bears/tests/python/PEP8BearTest.py
+++ bears/tests/python/PEP8BearTest.py
@@ -12,11 +12,26 @@ from coalib.settings.Setting import Setting
 class PEP8BearTest(LocalBearTestHelper):
     def setUp(self):
         self.section = Section('name')
+        self.section.append(Setting('max_line_length', '80'))
         self.uut = PEP8Bear(self.section, Queue())
 
     def test_valid(self):
         self.assertLinesValid(self.uut, ["import sys"])
         self.assertLinesValid(self.uut, ["a = 1 + 1"])
+
+    def test_line_length(self):
+        self.assertLinesValid(self.uut, ["a = 1 + 1 + 1 + 1 + 1 + 1 + 1"])
+        self.section.append(Setting('max_line_length', '10'))
+        self.assertLinesInvalid(self.uut, ["a = 1 + 1 + 1 + 1 + 1 + 1 + 1"])
+
+    def test_indent_level(self):
+        test_code = ['def func():\n',
+                     '    pass\n']
+        self.assertLinesValid(self.uut, test_code)
+
+        self.section.append(Setting('tab_width', '2'))
+        self.assertLinesInvalid(self.uut, test_code)
+        self.assertLinesValid(self.uut, ['def func():\n', '  pass\n'])
 
     def test_disable_warnings(self):
         test_code = ['def func():\n',
