3f0c971d403fdffeca1b558469488d0d2710a3ab
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-12-20 19:29:44 +0100

    IndentBear: Obey use_spaces, max_line_length, tab_width

--- bears/c_languages/IndentBear.py
+++ bears/c_languages/IndentBear.py
@@ -1,6 +1,7 @@
 import platform
 
 from coalib.bearlib.abstractions.CorrectionBasedBear import CorrectionBasedBear
+from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
 
 
 class IndentBear(CorrectionBasedBear):
@@ -10,17 +11,28 @@ class IndentBear(CorrectionBasedBear):
     def run(self,
             filename,
             file,
-            indent_cli_options: str='--k-and-r-style'):
+            max_line_length: int,
+            use_spaces: bool,
+            tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
+            indent_cli_options: str=''):
         """
         This bear checks and corrects spacing and indentation via the well
         known Indent utility. It is designed to work with the C programming
         language but may work reasonably with syntactically similar languages.
 
+        :param max_line_length:    Maximum number of characters for a line.
+        :param use_spaces:         True if spaces are to be used, else tabs.
+        :param tab_width:          Number of spaces per indent level.
         :param indent_cli_options: Any command line options the indent binary
                                    understands. They will be simply passed
                                    through.
         """
+        options = "--no-tabs" if use_spaces else "--use-tabs"
+        options += (" --line-length {0} --indent-level {1} --tab-size {1} "
+                    "{2}".format(max_line_length,
+                                 tab_width,
+                                 indent_cli_options))
         for result in self.retrieve_results(filename,
                                             file,
-                                            cli_options=indent_cli_options):
+                                            cli_options=options):
             yield result
--- bears/tests/c_languages/IndentBearTest.py
+++ bears/tests/c_languages/IndentBearTest.py
@@ -13,21 +13,31 @@ from coalib.settings.Setting import Setting
 class IndentBearTest(LocalBearTestHelper):
     def setUp(self):
         self.section = Section('name')
+        self.section.append(Setting("use_spaces", "true"))
+        self.section.append(Setting("max_line_length", "80"))
         self.uut = IndentBear(self.section, Queue())
 
     def test_valid(self):
-        self.assertLinesValid(self.uut, ["int main()\n",
+        self.assertLinesValid(self.uut, ["int\n",
+                                         "main ()\n",
                                          "{\n",
                                          "    return 0;\n",
                                          "}\n"])
 
-    def test_valid_gnu(self):
-        self.section.append(Setting("indent_cli_options", "-gnu"))
+    def test_tab_width(self):
+        self.section.append(Setting("tab_width", "2"))
         self.assertLinesValid(self.uut, ["int\n",
                                          "main ()\n",
                                          "{\n",
                                          "  return 0;\n",
                                          "}\n"])
+
+    def test_tabs(self):
+        test_code = ["int\n", "main ()\n", "{\n", "\treturn 0;\n", "}\n"]
+        self.assertLinesInvalid(self.uut, test_code)
+
+        self.section.append(Setting("use_spaces", "nope"))
+        self.assertLinesValid(self.uut, test_code)
 
     def test_invalid(self):
         self.assertLinesInvalid(self.uut, ["int main() {\n",
