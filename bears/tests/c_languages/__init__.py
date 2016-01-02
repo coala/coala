664a7863856b9386062bd8594d72a899af5fd211
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-01-02 16:10:20 +0100

    c_languages: Add skip_if_no_clang
    This will simplify test skipping for clang related tests.

---
+++ bears/tests/c_languages/__init__.py
@@ -0,0 +1,14 @@
+from unittest.case import skipIf, skip
+
+from clang.cindex import Index, LibclangError
+
+
+def skip_if_no_clang():
+    """
+    Decorate your test with this to skip it if clang isn't present.
+    """
+    try:
+        Index.create()
+        return skipIf(False, '')
+    except LibclangError as error:
+        return skip(str(error))
