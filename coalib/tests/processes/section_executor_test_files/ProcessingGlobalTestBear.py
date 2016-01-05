fae56063c75d07aa30f6d4a7a5cc138ffde0a75c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-01-05 19:09:31 +0100

    Testfiles: Make names unique
    We must not have two test bears that have the same name, otherwise they
    cannot both be imported. When using pytest, imports are cached and
    Python won't reimport them, unique test names are thus needed.

--- coalib/tests/processes/section_executor_test_files/ProcessingGlobalTestBear.py
+++ coalib/tests/processes/section_executor_test_files/ProcessingGlobalTestBear.py
@@ -2,7 +2,7 @@ from coalib.bears.GlobalBear import GlobalBear
 from coalib.results.Result import Result
 
 
-class GlobalTestBear(GlobalBear):  # pragma: no cover
+class ProcessingGlobalTestBear(GlobalBear):  # pragma: no cover
     def run(self):
         for filename in self.file_dict:
             return [Result.from_values("GlobalTestBear",
--- coalib/tests/processes/section_executor_test_files/ProcessingLocalTestBear.py
+++ coalib/tests/processes/section_executor_test_files/ProcessingLocalTestBear.py
@@ -4,7 +4,7 @@ from coalib.bears.LocalBear import LocalBear
 from coalib.results.Result import Result
 
 
-class LocalTestBear(LocalBear):  # pragma: no cover
+class ProcessingLocalTestBear(LocalBear):  # pragma: no cover
     def run(self, filename, file):
         # we need to test that the SectionExecutor holds back the global
         # results until processing of all local ones is finished
