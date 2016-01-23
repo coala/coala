b707d30a3f357e88a5845a85db57504b9a41d2d1
Josemaria William <jmw.it50@gmail.com>
2016-01-23 15:55:39 +0530

    ApplyPatchActionTest: Implement make_temp()
    Fixes https://github.com/coala-analyzer/coala/issues/1159

--- coalib/tests/results/result_actions/ApplyPatchActionTest.py
+++ coalib/tests/results/result_actions/ApplyPatchActionTest.py
@@ -1,6 +1,4 @@
-import os
 import sys
-from tempfile import mkstemp
 import unittest
 
 sys.path.insert(0, ".")
@@ -8,67 +6,59 @@ from coalib.results.result_actions.ApplyPatchAction import ApplyPatchAction
 from coalib.results.Diff import Diff
 from coalib.results.Result import Result
 from coalib.settings.Section import Section
+from coalib.misc.ContextManagers import make_temp
 
 
 class ApplyPatchActionTest(unittest.TestCase):
 
     def test_apply(self):
         uut = ApplyPatchAction()
-        fh_a, f_a = mkstemp()
-        fh_b, f_b = mkstemp()
-        fh_c, f_c = mkstemp()
-        os.close(fh_a)
-        os.close(fh_b)
-        os.close(fh_c)
+        with make_temp() as f_a, make_temp() as f_b, make_temp() as f_c:
 
-        file_dict = {
-            f_a: ["1\n", "2\n", "3\n"],
-            f_b: ["1\n", "2\n", "3\n"],
-            f_c: ["1\n", "2\n", "3\n"]
-        }
-        expected_file_dict = {
-            f_a: ["1\n", "3_changed\n"],
-            f_b: ["1\n", "2\n", "3_changed\n"],
-            f_c: ["1\n", "2\n", "3\n"]
-        }
+            file_dict = {
+                f_a: ["1\n", "2\n", "3\n"],
+                f_b: ["1\n", "2\n", "3\n"],
+                f_c: ["1\n", "2\n", "3\n"]
+            }
+            expected_file_dict = {
+                f_a: ["1\n", "3_changed\n"],
+                f_b: ["1\n", "2\n", "3_changed\n"],
+                f_c: ["1\n", "2\n", "3\n"]
+            }
 
-        file_diff_dict = {}
+            file_diff_dict = {}
 
-        diff = Diff(file_dict[f_a])
-        diff.delete_line(2)
-        uut.apply_from_section(Result("origin", "msg", diffs={f_a: diff}),
-                               file_dict,
-                               file_diff_dict,
-                               Section("t"))
+            diff = Diff(file_dict[f_a])
+            diff.delete_line(2)
+            uut.apply_from_section(Result("origin", "msg", diffs={f_a: diff}),
+                                   file_dict,
+                                   file_diff_dict,
+                                   Section("t"))
 
-        diff = Diff(file_dict[f_a])
-        diff.change_line(3, "3\n", "3_changed\n")
-        uut.apply_from_section(Result("origin", "msg", diffs={f_a: diff}),
-                               file_dict,
-                               file_diff_dict,
-                               Section("t"))
+            diff = Diff(file_dict[f_a])
+            diff.change_line(3, "3\n", "3_changed\n")
+            uut.apply_from_section(Result("origin", "msg", diffs={f_a: diff}),
+                                   file_dict,
+                                   file_diff_dict,
+                                   Section("t"))
 
-        diff = Diff(file_dict[f_b])
-        diff.change_line(3, "3\n", "3_changed\n")
-        uut.apply(Result("origin", "msg", diffs={f_b: diff}),
-                  file_dict,
-                  file_diff_dict)
+            diff = Diff(file_dict[f_b])
+            diff.change_line(3, "3\n", "3_changed\n")
+            uut.apply(Result("origin", "msg", diffs={f_b: diff}),
+                      file_dict,
+                      file_diff_dict)
 
-        for filename in file_diff_dict:
-            file_dict[filename] = file_diff_dict[filename].modified
+            for filename in file_diff_dict:
+                file_dict[filename] = file_diff_dict[filename].modified
 
-        self.assertEqual(file_dict, expected_file_dict)
-        with open(f_a) as fa:
-            self.assertEqual(file_dict[f_a], fa.readlines())
-        with open(f_b) as fb:
-            self.assertEqual(file_dict[f_b], fb.readlines())
-        with open(f_c) as fc:
-            # File c is unchanged and should be untouched
-            self.assertEqual([], fc.readlines())
-
-        os.remove(f_a)
-        os.remove(f_b)
-        os.remove(f_c)
+            self.assertEqual(file_dict, expected_file_dict)
+            with open(f_a) as fa:
+                self.assertEqual(file_dict[f_a], fa.readlines())
+            with open(f_b) as fb:
+                self.assertEqual(file_dict[f_b], fb.readlines())
+            with open(f_c) as fc:
+                # File c is unchanged and should be untouched
+                self.assertEqual([], fc.readlines())
 
     def test_is_applicable(self):
         diff = Diff(["1\n", "2\n", "3\n"])
