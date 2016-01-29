8f1be37d6edd322aa4e46a2b4f5ace41d9f5c441
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-01-29 23:36:51 +0100

    DefaultArgParser: Add bear explanation
    Usability testing with Ana has revealed that this is a weak point in our
    documentation.

--- coalib/parsing/DefaultArgParser.py
+++ coalib/parsing/DefaultArgParser.py
@@ -10,7 +10,8 @@ def default_arg_parser(formatter_class=None):
         prog="coala",
         description="coala is a simple COde AnaLysis Application. Its goal "
                     "is to make static code analysis easy and convenient "
-                    "for all languages.")
+                    "for all languages. coala uses bears, which are analaysis "
+                    "routines that can be combined arbitrarily.")
 
     arg_parser.add_argument('TARGETS',
                             nargs='*',
