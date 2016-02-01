90e7bf30864176010a5d531b8d3fab34cb870d18
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-02-01 17:27:05 +0100

    Checkstyle improvements (still wip)

--- bears/java/CheckstyleBear.py
+++ bears/java/CheckstyleBear.py
@@ -1,4 +1,5 @@
 import re
+from os.path import dirname, abspath, join
 
 from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bears.LocalBear import LocalBear
@@ -7,17 +8,18 @@ from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
 
 class CheckstyleBear(LocalBear, Lint):
     executable = 'java'
-    arguments = '-jar checkstyle-6.15-all.jar -c google_checks.xml'
+    google_checks = join(dirname(abspath(__file__)), 'google_checks.xml')
     output_regex = re.compile(
         r'\[(?P<severity>WARN|INFO)\]\s*'
         r'(?P<file_name>.+?):(?P<line>\d+)(:(?P<col>\d+))?:\s*'
-        r'(?P<message>.*)')
+        r'(?P<message>.*?)\s*\[(?P<origin>[a-zA-Z]+?)\]')
     severity_map = {
         "INFO": RESULT_SEVERITY.INFO,
         "WARN": RESULT_SEVERITY.NORMAL}
 
     def run(self, filename, file):
         '''
-        Checks the code with `checkstyle`.
+        Checks the code using `checkstyle`.
         '''
+        self.arguments = '-jar checkstyle-6.15-all.jar -c ' + self.google_checks
         return self.lint(filename)
