ec359120ab1a5170643e30509c8c70615b0d6dc8
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-02-01 17:33:26 +0100

    hardcode filename, less error prone

--- bears/java/CheckstyleBear.py
+++ bears/java/CheckstyleBear.py
@@ -9,10 +9,7 @@ from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
 class CheckstyleBear(LocalBear, Lint):
     executable = 'java'
     google_checks = join(dirname(abspath(__file__)), 'google_checks.xml')
-    output_regex = re.compile(
-        r'\[(?P<severity>WARN|INFO)\]\s*'
-        r'(?P<file_name>.+?):(?P<line>\d+)(:(?P<col>\d+))?:\s*'
-        r'(?P<message>.*?)\s*\[(?P<origin>[a-zA-Z]+?)\]')
+
     severity_map = {
         "INFO": RESULT_SEVERITY.INFO,
         "WARN": RESULT_SEVERITY.NORMAL}
@@ -21,5 +18,9 @@ class CheckstyleBear(LocalBear, Lint):
         '''
         Checks the code using `checkstyle`.
         '''
+        self.output_regex = re.compile(
+            r'\[(?P<severity>WARN|INFO)\]\s*' + abspath(filename) +
+            r':(?P<line>\d+)(:(?P<col>\d+))?:\s*'
+            r'(?P<message>.*?)\s*\[(?P<origin>[a-zA-Z]+?)\]')
         self.arguments = '-jar checkstyle-6.15-all.jar -c ' + self.google_checks
         return self.lint(filename)
