58cb929575fffe6396bc8dae220940d24b54b8db
Tushar Gautam <tushar.rishav@gmail.com>
2016-02-12 20:19:57 +0530

    TestUtilities.py: Create utility to run coala
    `execute_coala` moved out to
    `coalib/tests/TestUtilities.py`. So it can be
    shared by other tests
    
    Fixes https://github.com/coala-analyzer/coala/issues/1384

---
+++ coalib/tests/TestUtilities.py
@@ -0,0 +1,17 @@
+import sys
+from coalib.misc.ContextManagers import retrieve_stdout
+
+
+def execute_coala(func, binary, *args):
+    """
+    Executes the main function with the given argument string from given module.
+
+    :param function: A main function from coala_json, coala_ci module etc.
+    :param binary:   A binary to execute coala test
+    :return:         A tuple holding a return value first and
+                     a stdout output as second element.
+    """
+    sys.argv = [binary] + list(args)
+    with retrieve_stdout() as stdout:
+        retval = func()
+        return retval, stdout.getvalue()
--- coalib/tests/coalaTest.py
+++ coalib/tests/coalaTest.py
@@ -3,34 +3,10 @@ import os
 import unittest
 import re
 from tempfile import TemporaryDirectory, NamedTemporaryFile
-
-if sys.version_info < (3, 4):
-    import imp as importlib
-else:
-    import importlib
-
-from coalib.misc.ContextManagers import retrieve_stdout, make_temp
+from coalib.misc.ContextManagers import make_temp
 from coalib import coala_ci
-from coalib.settings import ConfigurationGathering
 from coalib.output.Tagging import get_tag_path
-
-
-def execute_coala_ci(args):
-    """
-    Executes the coala-ci main function with the given argument string.
-
-    :param args: A list of arguments to pass to coala.
-    :return:     A tuple holding the return value first and the stdout output
-                 as second element.
-    """
-    sys.argv = ["coala-ci"] + list(args)
-    # Need to reload both as ConfigurationGathering has the old sys.argv loaded
-    # and coala_ci has the old ConfigurationGathering values.
-    importlib.reload(ConfigurationGathering)
-    importlib.reload(coala_ci)
-    with retrieve_stdout() as stdout:
-        retval = coala_ci.main()
-        return retval, stdout.getvalue()
+from coalib.tests.TestUtilities import execute_coala
 
 
 class coalaTest(unittest.TestCase):
@@ -44,13 +20,15 @@ class coalaTest(unittest.TestCase):
         sys.argv = self.old_argv
 
     def test_nonexistent(self):
-        retval, output = execute_coala_ci(("-c", "nonex", "test"))
+        retval, output = execute_coala(
+            coala_ci.main, "coala-ci", "-c", 'nonex', "test")
         self.assertRegex(
             output,
             ".*\\[ERROR\\].*The requested coafile '.*' does not exist.\n")
 
     def test_find_no_issues(self):
-        retval, output = execute_coala_ci(('docs', '-c', self.coafile))
+        retval, output = execute_coala(
+            coala_ci.main, "coala-ci", 'docs', '-c', self.coafile)
         self.assertRegex(output,
                          "(.*Unable to collect bears from.*PyLintBear.*)?",
                          "coala-ci output should be empty when running "
@@ -61,7 +39,8 @@ class coalaTest(unittest.TestCase):
                          "own code.")
 
     def test_find_issues(self):
-        retval, output = execute_coala_ci(("todos", "-c", self.coafile))
+        retval, output = execute_coala(
+            coala_ci.main, "coala-ci", "todos", "-c", self.coafile)
         self.assertRegex(output,
                          "(.*Unable to collect bears from.*PyLintBear.*)?",
                          "coala-ci output should be empty when running "
@@ -72,15 +51,21 @@ class coalaTest(unittest.TestCase):
                             "its own code. (Target section: todos)")
 
     def test_tagging(self):
-        execute_coala_ci(('docs', "-S", "tag=test_tag", "-c", self.coafile))
+        execute_coala(coala_ci.main, "coala-ci", 'docs',
+                      "-S", "tag=test_tag", "-c", self.coafile)
         tag_path = get_tag_path("test_tag", self.unescaped_coafile)
         self.assertTrue(os.path.exists(tag_path))
-        execute_coala_ci(('docs', "-S", "dtag=test_tag", "-c", self.coafile))
+        execute_coala(coala_ci.main, "coala-ci", 'docs',
+                      "-S", "dtag=test_tag", "-c", self.coafile)
         self.assertFalse(os.path.exists(tag_path))
 
     def test_fail_acquire_settings(self):
-        retval, output = execute_coala_ci((
-            "-b", 'SpaceConsistencyBear', '-c', os.devnull))
+        retval, output = execute_coala(coala_ci.main,
+                                       "coala-ci",
+                                       "-b",
+                                       'SpaceConsistencyBear',
+                                       '-c',
+                                       os.devnull)
         self.assertIn("During execution, we found that some", output)
 
     def test_coala_delete_orig(self):
@@ -91,6 +76,7 @@ class coalaTest(unittest.TestCase):
              make_temp(suffix='.coafile', prefix='', dir=tempdir) as coafile,\
              make_temp(dir=tempdir) as unrelated_file:
             orig_file.close()
-            execute_coala_ci(("-c", re.escape(coafile)))
+            execute_coala(coala_ci.main, "coala-ci",
+                          "-c", re.escape(coafile))
             self.assertFalse(os.path.isfile(orig_file.name))
             self.assertTrue(os.path.isfile(unrelated_file))
