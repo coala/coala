caa5dcc39d99852b75c0f0d41dd6f7752bb7ecf7
AbdealiJK <abdealikothari@gmail.com>
2016-02-16 13:08:27 +0530

    MarkdownBear: Add options for remark
    Add all the options that remark provides. Let Lint create
    a config file and give that to remark.
    The remark configs for cli are complicated to setup, and it
    is much easier to use the JSON configuration file which remark
    accepts. Hence let Lint take the configs that are required and
    give those to remark.
    
    Fixes https://github.com/coala-analyzer/coala/issues/1415

--- bears/tests/natural_language/MarkdownBearTest.py
+++ bears/tests/natural_language/MarkdownBearTest.py
@@ -1,6 +1,22 @@
 from bears.natural_language.MarkdownBear import MarkdownBear
 from bears.tests.LocalBearTestHelper import verify_local_bear
 
-MarkdownBearTest = verify_local_bear(MarkdownBear,
-                                     (["```\n", "some code\n", "```\n"],),
-                                     (['    some code'],))
+test_file1 = """1. abc
+1. def
+""".splitlines()
+
+
+test_file2 = """1. abc
+2. def
+""".splitlines()
+
+
+MarkdownBear1Test = verify_local_bear(MarkdownBear,
+                                      valid_files=(test_file2,),
+                                      invalid_files=(test_file1,))
+
+MarkdownBear2Test = verify_local_bear(MarkdownBear,
+                                      valid_files=(test_file1,),
+                                      invalid_files=(test_file2,),
+                                      settings={
+                                          "markdown_list_increment": False})
