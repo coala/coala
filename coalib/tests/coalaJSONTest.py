41814a1848705ab1e1a718ad9f114e529bb4e4e8
AbdealiJK <abdealikothari@gmail.com>
2016-02-17 23:34:04 +0530

    coalaJSONTest: Remove unused values self.coafile

--- coalib/tests/coalaJSONTest.py
+++ coalib/tests/coalaJSONTest.py
@@ -13,8 +13,6 @@ class coalaJSONTest(unittest.TestCase):
 
     def setUp(self):
         self.old_argv = sys.argv
-        self.unescaped_coafile = os.path.abspath("./.coafile")
-        self.coafile = re.escape(self.unescaped_coafile)
 
     def tearDown(self):
         sys.argv = self.old_argv
