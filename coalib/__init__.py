aa9eb0b1d68e34f015eb7fd31b061a0315bf583f
vidhan1995 <vidhan1995.jain@gmail.com>
2016-02-19 19:18:26 +0530

    coalib/__init__.py:Remove pypy support
    Fixes https://github.com/coala-analyzer/coala/issues/1590

--- coalib/__init__.py
+++ coalib/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 
 def assert_supported_version():  # pragma: no cover
-    if (not sys.version_info > (3, 2) or
-            (sys.version_info[:2] == (3, 2))):
+    if (not sys.version_info > (3, 3) or
+            (sys.version_info[:2] == (3, 3))):
         print("coala supports only python 3.3 or later.")
         exit(4)
