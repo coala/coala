707fbc93a81476dee51069c3b75565b8ee3c50dc
AbdealiJK <abdealikothari@gmail.com>
2016-02-20 08:13:41 +0530

    coalaTest: Install test bears module and test
    As coalaTest is an integration test, we use the bear test module
    to do the test.
    
    Fixes https://github.com/coala-analyzer/coala/issues/1610

--- coalib/tests/coalaTest.py
+++ coalib/tests/coalaTest.py
@@ -3,10 +3,11 @@ import re
 import sys
 import unittest
 
-from bears.general.LineCountBear import LineCountBear
 from coalib import coala
 from coalib.misc.ContextManagers import prepare_file
-from coalib.tests.TestUtilities import execute_coala
+from coalib.tests.coala_bears_test_module.LineCountTestBear import (
+    LineCountTestBear)
+from coalib.tests.TestUtilities import execute_coala, bear_test_module
 
 
 class coalaTest(unittest.TestCase):
@@ -18,16 +19,16 @@ class coalaTest(unittest.TestCase):
         sys.argv = self.old_argv
 
     def test_coala(self):
-        with prepare_file(["#fixme"], None) as (lines, filename):
-            bear = "LineCountBear"
-            retval, output = execute_coala(
-                             coala.main,
-                            "coala", "-c", os.devnull,
-                            "--settings", "files=" + re.escape(filename),
-                            "bears=" + bear)
-        self.assertIn("This file has 1 lines.",
-                      output,
-                      "The output should report count as 1 lines")
+        with bear_test_module():
+            with prepare_file(["#fixme"], None) as (lines, filename):
+                retval, output = execute_coala(
+                                 coala.main,
+                                "coala", "-c", os.devnull,
+                                "-f", re.escape(filename),
+                                "-b", "LineCountTestBear")
+            self.assertIn("This file has 1 lines.",
+                          output,
+                          "The output should report count as 1 lines")
 
     def test_did_nothing(self):
         retval, output = execute_coala(coala.main, "coala", "-c", os.devnull,
@@ -36,13 +37,14 @@ class coalaTest(unittest.TestCase):
         self.assertIn("No existent section was targeted or enabled", output)
 
     def test_show_bears(self):
-        retval, output = execute_coala(coala.main, "coala", "-A")
-        self.assertEqual(retval, 0)
-        bear_lines = [i.startswith(" * ") for i in output.split()]
-        self.assertGreater(len(bear_lines), 0)
+        with bear_test_module():
+            retval, output = execute_coala(coala.main, "coala", "-A")
+            self.assertEqual(retval, 0)
+            bear_lines = [i.startswith(" * ") for i in output.split()]
+            self.assertGreater(len(bear_lines), 0)
 
-        retval, output = execute_coala(coala.main, "coala", "-B",
-                                       "-b", "LineCountBear",
-                                       "-c", os.devnull)
-        self.assertEqual(retval, 0)
-        self.assertIn(LineCountBear.run.__doc__.strip(), output)
+            retval, output = execute_coala(coala.main, "coala", "-B",
+                                           "-b", "LineCountTestBear",
+                                           "-c", os.devnull)
+            self.assertEqual(retval, 0)
+            self.assertIn(LineCountTestBear.run.__doc__.strip(), output)
