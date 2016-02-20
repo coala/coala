e0b13d774fabd5580e377562305945eb904d9c24
Arush Goyal <arushgyl@gmail.com>
2016-02-20 06:54:27 +0530

    setup.py: Remove '[]' from maintainer name
    Fixes https://github.com/coala-analyzer/coala/issues/1574

--- setup.py
+++ setup.py
@@ -93,8 +93,8 @@ if __name__ == "__main__":
           version=Constants.VERSION,
           description='Code Analysis Application (coala)',
           author="The coala developers",
-          maintainer=["Lasse Schuirmann, Fabian Neuschmidt, Mischa Kr\xfcger"
-                      if not on_rtd else "L.S., F.N., M.K."],
+          maintainer="Lasse Schuirmann, Fabian Neuschmidt, Mischa Kr\xfcger"
+                      if not on_rtd else "L.S., F.N., M.K.",
           maintainer_email=('lasse.schuirmann@gmail.com, '
                             'fabian@neuschmidt.de, '
                             'makman@alice.de'),
