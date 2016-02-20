792cf98a34b0d95fc4222b725ccc740bb8d2c52e
AbdealiJK <abdealikothari@gmail.com>
2016-02-20 08:59:41 +0530

    coalib/tests: Add module coala_bears_test_module
    The coala_bears_test_module contains some bears. It is useful
    when running integration tests on coala.
    Helps in https://github.com/coala-analyzer/coala/issues/1610

---
+++ coalib/tests/coala_bears_test_module/LineCountTestBear.py
@@ -0,0 +1,16 @@
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.Result import Result
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class LineCountTestBear(LocalBear):
+
+    def run(self, filename, file):
+        '''
+        Counts the lines of each file.
+        '''
+        yield Result.from_values(
+            origin=self,
+            message="This file has {count} lines.".format(count=len(file)),
+            severity=RESULT_SEVERITY.INFO,
+            file=filename)
