bc4e6d7b4f73605124bd88571c8432b772cef782
AbdealiJK <abdealikothari@gmail.com>
2016-02-20 19:33:30 +0530

    coalaJSONTest: Use bear_test_module()

--- coalib/tests/coalaJSONTest.py
+++ coalib/tests/coalaJSONTest.py
@@ -6,7 +6,7 @@ import unittest
 
 from coalib import coala_json
 from coalib.misc.ContextManagers import prepare_file
-from coalib.tests.TestUtilities import execute_coala
+from coalib.tests.TestUtilities import bear_test_module, execute_coala
 
 
 class coalaJSONTest(unittest.TestCase):
@@ -26,32 +26,30 @@ class coalaJSONTest(unittest.TestCase):
             "The requested coafile '.*' does not exist.")
 
     def test_find_issues(self):
-        with prepare_file(["#todo this is todo"], None) as (lines, filename):
-            bear = "KeywordBear"
-            retval, output = execute_coala(coala_json.main, "coala-json", "-c",
-                                           os.devnull, "-S",
-                                           "ci_keywords=#TODO",
-                                           "cs_keywords=#todo",
-                                           "bears=" + bear,
+        with bear_test_module(), \
+                prepare_file(["#fixme"], None) as (lines, filename):
+            retval, output = execute_coala(coala_json.main, "coala-json",
+                                           "-c", os.devnull,
+                                           "-b", "LineCountTestBear",
                                            "-f", re.escape(filename))
             output = json.loads(output)
             self.assertEqual(output["results"]["default"][0]["message"],
-                             "The line contains the keyword `#todo`.",
-                             "coala-json output should match the keyword #todo")
-            self.assertNotEqual(retval,
-                                0, "coala-json must return nonzero when "
-                                "matching `#todo` keyword")
+                             "This file has 1 lines.")
+            self.assertNotEqual(retval, 0,
+                                "coala-json must return nonzero when "
+                                "results found")
 
     def test_fail_acquire_settings(self):
-        retval, output = execute_coala(
-            coala_json.main, 'coala-json', '-b',
-            'SpaceConsistencyBear', '-c', os.devnull)
-        output = json.loads(output)
-        found = False
-        for msg in output["logs"]:
-            if "During execution, we found that some" in msg["message"]:
-                found = True
-        self.assertTrue(found)
+        with bear_test_module():
+            retval, output = execute_coala(coala_json.main, 'coala-json',
+                                           '-c', os.devnull,
+                                           '-b', 'SpaceConsistencyTestBear')
+            output = json.loads(output)
+            found = False
+            for msg in output["logs"]:
+                if "During execution, we found that some" in msg["message"]:
+                    found = True
+            self.assertTrue(found, "Missing settings not logged")
 
     def test_version(self):
         with self.assertRaises(SystemExit):
