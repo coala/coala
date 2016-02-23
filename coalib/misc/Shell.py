801092d1f01896f6c627cea35d1beeb82a533f5a
Makman2 <mak95@arcor.de>
2016-02-23 23:54:17 +0100

    Shell.run_shell_command: Remove `shell=True`
    This protects us against shell injection and also surprises due to
    shell interpretation.
    
    For compatability use `shlex.split()` when providing a string as a
    command and not a sequence.
    
    Also the `ShellTest` needs adaption since it used a shell-command
    before which is not possible any more. Now argument-lists are used
    instead.
    
    `Lint` does still use `shell=True` for compatability purposes. This
    will be changed in following commits.
    
    This reintroduces commit adc94c745e4d9f792fd9c9791c7b4cd8790d0d2f.

--- coalib/misc/Shell.py
+++ coalib/misc/Shell.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+import shlex
 from subprocess import PIPE, Popen
 
 from coalib.parsing.StringProcessing import escape
@@ -7,23 +8,36 @@ from coalib.parsing.StringProcessing import escape
 @contextmanager
 def run_interactive_shell_command(command, **kwargs):
     """
-    Runs a command in shell and provides stdout, stderr and stdin streams.
+    Runs a single command in shell and provides stdout, stderr and stdin
+    streams.
 
-    This function creates a context manager that sets up the process, returns
-    to caller, closes streams and waits for process to exit on leaving.
+    This function creates a context manager that sets up the process (using
+    ``subprocess.Popen()``), returns to caller, closes streams and waits for
+    process to exit on leaving.
 
-    The process is opened in ``universal_newlines`` mode.
+    Shell execution is disabled by default (so no shell expansion takes place).
+    If you want to turn shell execution on, you can pass ``shell=True`` like
+    you would do for ``subprocess.Popen()``.
 
-    :param command: The command to run on shell.
-    :param kwargs:  Additional keyword arguments to pass to ``subprocess.Popen``
-                    that is used to spawn the process (except ``shell``,
-                    ``stdout``, ``stderr``, ``stdin`` and
+    The process is opened in ``universal_newlines`` mode by default.
+
+    :param command: The command to run on shell. This parameter can either
+                    be a sequence of arguments that are directly passed to
+                    the process or a string. A string gets splitted beforehand
+                    using ``shlex.split()``. If providing ``shell=True`` as a
+                    keyword-argument, no ``shlex.split()`` is performed and the
+                    command string goes directly to ``subprocess.Popen()``.
+    :param kwargs:  Additional keyword arguments to pass to
+                    ``subprocess.Popen`` that is used to spawn the process
+                    (except ``stdout``, ``stderr``, ``stdin`` and
                     ``universal_newlines``, a ``TypeError`` is raised then).
     :return:        A context manager yielding the process started from the
                     command.
     """
+    if not kwargs.get("shell", False) and isinstance(command, str):
+        command = shlex.split(command)
+
     process = Popen(command,
-                    shell=True,
                     stdout=PIPE,
                     stderr=PIPE,
                     stdin=PIPE,
@@ -40,15 +54,25 @@ def run_interactive_shell_command(command, **kwargs):
 
 def run_shell_command(command, stdin=None, **kwargs):
     """
-    Runs a command in shell and returns the read stdout and stderr data.
+    Runs a single command in shell and returns the read stdout and stderr data.
 
-    This function waits for the process to exit.
+    This function waits for the process (created using ``subprocess.Popen()``)
+    to exit.
 
-    :param command: The command to run on shell.
+    Shell execution is disabled by default (so no shell expansion takes place).
+    If you want to turn shell execution on, you can pass ``shell=True`` like
+    you would do for ``subprocess.Popen()``.
+
+    See also ``run_interactive_shell_command()``.
+
+    :param command: The command to run on shell. This parameter can either
+                    be a sequence of arguments that are directly passed to
+                    the process or a string. A string gets splitted beforehand
+                    using ``shlex.split()``.
     :param stdin:   Initial input to send to the process.
-    :param kwargs:  Additional keyword arguments to pass to ``subprocess.Popen``
-                    that is used to spawn the process (except ``shell``,
-                    ``stdout``, ``stderr``, ``stdin`` and
+    :param kwargs:  Additional keyword arguments to pass to
+                    ``subprocess.Popen`` that are used to spawn the process
+                    (except ``stdout``, ``stderr``, ``stdin`` and
                     ``universal_newlines``, a ``TypeError`` is raised then).
     :return:        A tuple with ``(stdoutstring, stderrstring)``.
     """
@@ -67,10 +91,10 @@ def get_shell_type():  # pragma: no cover
              Powershell is detected, "cmd" if command prompt is been
              detected or "sh" if it's neither of these.
     """
-    out = run_shell_command("echo $host.name")[0]
+    out = run_shell_command("echo $host.name", shell=True)[0]
     if out.strip() == "ConsoleHost":
         return "powershell"
-    out = run_shell_command("echo $0")[0]
+    out = run_shell_command("echo $0", shell=True)[0]
     if out.strip() == "$0":
         return "cmd"
     return "sh"
