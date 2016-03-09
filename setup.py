3600f03063e0c4fb5ee01538c3a2f69b58a1997a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-03-09 12:23:49 +0100

    setup: Pull in long description from readme
    Fixes https://github.com/coala-analyzer/coala/issues/1843

--- setup.py
+++ setup.py
@@ -62,6 +62,9 @@ with open('requirements.txt') as requirements:
 with open('test-requirements.txt') as requirements:
     test_required = requirements.read().splitlines()
 
+with open("README.rst") as readme:
+    long_description = readme.read()
+
 
 if __name__ == "__main__":
     data_files = [('.', ['coala.1']), ('.', [Constants.BUS_NAME + '.service'])]
@@ -84,16 +87,7 @@ if __name__ == "__main__":
                                    'bearlib/languages/definitions/*.coalang']},
           license="AGPL-3.0",
           data_files=data_files,
-          long_description="coala is a simple COde AnaLysis Application. Its "
-                           "goal is to make static code analysis easy while "
-                           "remaining completely modular and therefore "
-                           "extendable and language independent. Code analysis"
-                           " happens in python scripts while coala manages "
-                           "these, tries to provide helpful libraries and "
-                           "provides a user interface. Please visit "
-                           "http://coala.rtfd.org/ for more information or "
-                           "our development repository on "
-                           "https://github.com/coala-analyzer/coala/.",
+          long_description=long_description,
           entry_points={
               "console_scripts": [
                   "coala = coalib.coala:main",
