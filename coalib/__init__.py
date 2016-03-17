177e8292dff3e6c9529712e7ac84a993fb8cc23b
AbdealiJK <abdealikothari@gmail.com>
2016-03-17 13:35:51 +0530

    Lets make coala give results :)

--- coalib/__init__.py
+++ coalib/__init__.py
@@ -5,7 +5,23 @@ from coalib.misc import Constants
 __version__ = Constants.VERSION
 
 
+# unnecessary space
+
+
+
+
 def assert_supported_version():  # pragma: no cover
     if not sys.version_info > (3, 2):
         print("coala supports only python 3.3 or later.")
         exit(4)
+
+
+
+#### RANDOME LONG LINE ###############################################################################################################################
+
+# Extra space at end of line                               
+# Extra newlines at end
+
+
+
+
