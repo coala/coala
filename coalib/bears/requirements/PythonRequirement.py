b01d61fb4b7a32ea636648824d615f4cc790c7d9
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-05-26 01:36:13 +0300

    PythonRequirement: Add multiple method
    Method can be used to provide multiple requirements,
    without specifying the manager each time.

--- coalib/bears/requirements/PythonRequirement.py
+++ coalib/bears/requirements/PythonRequirement.py
@@ -24,3 +24,65 @@ class PythonRequirement(PackageRequirement):
         :param version: A version string. Leave empty to specify latest version.
         """
         PackageRequirement.__init__(self, 'pip', package, version)
+
+    @classmethod
+    def multiple(cls, *args):
+        """
+        Creates a tuple of multiple ``PythonRequirements``.
+
+        You should use the ``multiple`` method if you have more
+        requirements from the same manager. This can receive both tuples of
+        strings, in case you want a specific version, or a simple string, in
+        case you want the latest version to be specified.
+
+        This is the case where you would provide strings only, to specify the
+        latest version automatically:
+
+        >>> REQUIREMENTS = PythonRequirement.multiple(
+        ...     'coala_decorators', 'setuptools')
+
+        And if you choose to mix them, specifying version for some and for some
+        not:
+
+        >>> REQUIREMENTS = PythonRequirement.multiple(
+        ...     'coala_decorators', ('setuptools', '19.2'))
+
+        In case you provide too many arguments into the tuple, an error will be
+        raised:
+
+        >>> REQUIREMENTS = PythonRequirement.multiple(
+        ...     'coala_decorators', ('setuptools', '19.2', 'colorama'))
+        Traceback (most recent call last):
+        ...
+        TypeError: The tuple must have 2 elements.
+
+        The same would happen in case you provide something different than a
+        string or a tuple:
+
+        >>> x = [1, 2, 3, 4]
+        >>> REQUIREMENTS = PythonRequirement.multiple(x)
+        Traceback (most recent call last):
+        ...
+        TypeError: The arguments need to be tuples or strings.
+
+        :param args:       Should be tuples of strings: ``('packageName',
+                           'version')`` or strings: ``'packageName'`` if latest
+                           version is wanted.
+        :return:           A tuple containing ``PythonRequirements``.
+        :raises TypeError: In case the tuples contain more or less than two
+                           elements. Also raised when arguments are neither
+                           tuples nor strings.
+        """
+        reqs = []
+        for requirement in args:
+            if isinstance(requirement, str):
+                reqs.append(cls(requirement),)
+            elif isinstance(requirement, tuple):
+                try:
+                    name, version = requirement
+                    reqs.append(cls(name, version),)
+                except ValueError:
+                    raise TypeError('The tuple must have 2 elements.')
+            else:
+                raise TypeError('The arguments need to be tuples or strings.')
+        return tuple(reqs)
