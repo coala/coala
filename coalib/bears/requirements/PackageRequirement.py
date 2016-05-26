3ab66cbb2cb4ab018b1de75fdf57aed82ba26722
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-05-26 23:43:49 +0300

    PackageRequirement: Add multiple method
    Adds multiple method to PackageRequirement. It
    receives as arguments multiple tuples with requirements.
    Also removes the method from the PythonRequirement
    subclass.
    
    Fixes https://github.com/coala-analyzer/coala/issues/2176

--- coalib/bears/requirements/PackageRequirement.py
+++ coalib/bears/requirements/PackageRequirement.py
@@ -49,3 +49,73 @@ class PackageRequirement:
         :return: Returns True if satisfied, False if not.
         """
         raise NotImplementedError
+
+    @classmethod
+    def multiple(cls, *args):
+        """
+        Creates a tuple of multiple instances of a class.
+
+        Should not be instances of ``PackageRequirement``, as this is an
+        abstract class:
+
+        >>> PackageRequirement.multiple(('pip', 'coala_decorators', '0.1.0'),)
+        Traceback (most recent call last):
+        ...
+        NotImplementedError
+
+        It can only be used for requirements of the same manager. For example,
+        consider a manager ``XYZRequirement`` that inherits from
+        PackageRequirement. This subclass will have the manager set to XYZ:
+
+        >>> class XYZRequirement(PackageRequirement):
+        ...     manager = 'xyz'
+        ...     def __init__(self, package, version=""):
+        ...         PackageRequirement.__init__(self, package, version)
+
+        This is the case where you would provide strings only, to specify the
+        latest version automatically:
+
+        >>> REQUIREMENTS = XYZRequirement.multiple(
+        ...     "package1", "package2")
+
+        And if you choose to mix them, specifying version for some and for some
+        not:
+
+        >>> REQUIREMENTS = XYZRequirement.multiple(
+        ...     'package1', ('package2', '2.0'))
+
+        Lists are also valid arguments:
+
+        >>> REQUIREMENTS = XYZRequirement.multiple(
+        ...     ['package1', '1.0'],)
+
+        In case you provide too many arguments into the tuple, an error will be
+        raised:
+
+        >>> REQUIREMENTS = XYZRequirement.multiple(
+        ...     'package1', ('package2', '2.0', 'package3'))
+        Traceback (most recent call last):
+        ...
+        TypeError: Too many elements provided.
+
+        :param args:       In the subclasses, the ``manager`` is already
+                           specified, so they hould be iterables with two
+                           elements: ``('packageName', 'version')`` or strings:
+                           ``'packageName'`` if latest version is wanted.
+        :return:           A tuple containing instances of the subclass.
+        :raises TypeError: In case the iterables contain more than two
+                           elements.
+        """
+        if cls == PackageRequirement:
+            raise NotImplementedError
+        else:
+            reqs = []
+            for requirement in args:
+                if isinstance(requirement, str):
+                    reqs.append(cls(requirement))
+                elif len(requirement) == 2:
+                    name, version = requirement
+                    reqs.append(cls(name, version))
+                else:
+                    raise TypeError('Too many elements provided.')
+            return tuple(reqs)
