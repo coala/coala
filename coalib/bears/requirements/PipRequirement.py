b0b3d6b36a4741d01b98e8cb4d98f9e761f3b41d
Adrian Zatreanu <zgadi_2007@yahoo.com>
2016-06-29 14:55:51 +0300

    PipRequirement: Add installation methods

--- coalib/bears/requirements/PipRequirement.py
+++ coalib/bears/requirements/PipRequirement.py
@@ -1,4 +1,6 @@
 from coalib.bears.requirements.PackageRequirement import PackageRequirement
+from coalib.misc.Shell import call_without_output
+import sys
 
 
 class PipRequirement(PackageRequirement):
@@ -6,6 +8,8 @@ class PipRequirement(PackageRequirement):
     This class is a subclass of ``PackageRequirement``, and helps specifying
     requirements from ``pip``, without using the manager name.
     """
+
+    MANAGER = sys.executable + ' -m pip'
 
     def __init__(self, package, version=""):
         """
@@ -24,3 +28,22 @@ class PipRequirement(PackageRequirement):
         :param version: A version string. Leave empty to specify latest version.
         """
         PackageRequirement.__init__(self, 'pip', package, version)
+
+    def install_command(self):
+        """
+        Creates the installation command for the instance of the class.
+
+        :param return: A string with the installation command.
+        """
+        result = "{} install {}".format(self.MANAGER, self.package)
+        if self.version:
+            result += "=={}".format(self.version)
+        return result
+
+    def is_installed(self):
+        """
+        Checks if the dependency is installed.
+
+        :param return: True if dependency is installed, false otherwise.
+        """
+        return not call_without_output(('pip', 'show', self.package))
---
+++ tests/bears/requirements/PipRequirementTest.py
@@ -0,0 +1,21 @@
+import unittest
+import shutil
+from coalib.bears.requirements.PipRequirement import PipRequirement
+
+
+@unittest.skipIf(shutil.which('pip') is None, "Pip is not installed.")
+class PipRequirementTestCase(unittest.TestCase):
+
+    def test_install_command_with_version(self):
+        self.assertIn('-m pip install setuptools==19.2',
+                      PipRequirement('setuptools', '19.2').install_command())
+
+    def test_install_command_without_version(self):
+        self.assertIn('-m pip install setuptools',
+                      PipRequirement('setuptools').install_command())
+
+    def test_installed_requirement(self):
+        self.assertTrue(PipRequirement('pip').is_installed())
+
+    def test_not_installed_requirement(self):
+        self.assertFalse(PipRequirement('some_bad_package').is_installed())
