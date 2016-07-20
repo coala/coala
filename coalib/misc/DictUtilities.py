82a4da5d42c9126349bece2514b7773d73dd6226
Steven Van den Berghe <svdbergh@vub.ac.be>
2016-07-20 23:02:53 +0200

    DictUtilities: Fix incorrect example in docstring
    Change the result of inverse_dict to the correct value
    
    Fixes https://github.com/coala-analyzer/coala/issues/2438

--- coalib/misc/DictUtilities.py
+++ coalib/misc/DictUtilities.py
@@ -4,8 +4,8 @@ from collections import Iterable, OrderedDict
 def inverse_dicts(*dicts):
     """
     Inverts the dicts, e.g. {1: 2, 3: 4} and {2: 3, 4: 4} will be inverted
-    {2: [1, 2], 4: [3, 4]}. This also handles dictionaries with Iterable items
-    as values e.g. {1: [1, 2, 3], 2: [3, 4, 5]} and
+    {2: [1], 3: [2], 4: [3, 4]}. This also handles dictionaries
+    with Iterable items as values e.g. {1: [1, 2, 3], 2: [3, 4, 5]} and
     {2: [1], 3: [2], 4: [3, 4]} will be inverted to
     {1: [1, 2], 2: [1, 3], 3: [1, 2, 4], 4: [2, 4], 5: [2]}.
     No order is preserved.
