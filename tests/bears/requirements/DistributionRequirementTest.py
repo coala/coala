69b7d3faccbb1ed7c92fdb5ca9bd60acd6a21ce3
SanketDG <sanketdasgupta@gmail.com>
2016-08-28 23:11:46 +0530

    DistributionRequirement: Use distro module
    Some platform module functions are deprecated as of python version
    3.5.
    Source: https://bugs.python.org/issue1322
    
    distro module is recommended as the alternative
    
    Fixes https://github.com/coala-analyzer/coala/issues/2675

--- tests/bears/requirements/DistributionRequirementTest.py
+++ tests/bears/requirements/DistributionRequirementTest.py
@@ -1,23 +1,31 @@
 import platform
+import subprocess
 import unittest
 from unittest.mock import patch
+
+try:
+    import distro
+except subprocess.CalledProcessError:
+    pass
 
 from coalib.bears.requirements.DistributionRequirement import (
     DistributionRequirement)
 
 
+@unittest.skipIf(platform.system() == 'Windows',
+                 "This test cannot be run on Windows.")
 class DistributionRequirementTestCase(unittest.TestCase):
 
-    @patch('platform.linux_distribution', return_value=('Fedora',))
+    @patch('distro.linux_distribution', return_value=('Fedora',))
     def test_install_command_mock_fedora(self, call_mock):
-        self.assertEqual(platform.linux_distribution()[0], 'Fedora')
+        self.assertEqual(distro.linux_distribution()[0], 'Fedora')
         self.assertEqual(DistributionRequirement(
             dnf='libclang', apt_get='libclangs').install_command(),
             ['dnf', 'install', 'libclang'])
 
-    @patch('platform.linux_distribution', return_value=('bad_os',))
+    @patch('distro.linux_distribution', return_value=('bad_os',))
     def test_install_command_mock_incompatible_os(self, call_mock):
-        self.assertEqual(platform.linux_distribution()[0], 'bad_os')
+        self.assertEqual(distro.linux_distribution()[0], 'bad_os')
         with self.assertRaises(OSError):
             DistributionRequirement(
                 dnf='libclang', apt_get='libclangs').install_command()
