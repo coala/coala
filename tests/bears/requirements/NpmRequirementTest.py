1a6eb98e942c863e7093cab2992b7f1ac6a7b583
Fabian Neuschmidt <fabian@neuschmidt.de>
2016-09-13 19:10:41 +0200

    NpmRequirement: Fix is_installed()
    `npm show` uses the network to look up information about a package, and
    fails only on non-existent packages, independent of whether or not they
    are installed. To only check for locally installed packages, 'npm list'
    is now used.
    
    Since 'npm' is not a locally installed package according to 'npm list',
    the test had to be changed to make use of a mock, since we cannot rely
    upon any one package always bein there.
    
    fixes https://github.com/coala-analyzer/coala/issues/2751

--- tests/bears/requirements/NpmRequirementTest.py
+++ tests/bears/requirements/NpmRequirementTest.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock
 import shutil
 from coalib.bears.requirements.NpmRequirement import NpmRequirement
 
@@ -7,7 +8,10 @@ from coalib.bears.requirements.NpmRequirement import NpmRequirement
 class NpmRequirementTestCase(unittest.TestCase):
 
     def test_installed_requirement(self):
-        self.assertTrue(NpmRequirement('npm').is_installed())
+        with unittest.mock.patch(
+                'coalib.bears.requirements.NpmRequirement.call_without_output',
+                return_value=0):
+            self.assertTrue(NpmRequirement('some_good_package').is_installed())
 
     def test_not_installed_requirement(self):
         self.assertFalse(NpmRequirement('some_bad_package').is_installed())
