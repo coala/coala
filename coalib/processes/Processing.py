83f9111583d6b48e9197ffbf64a89ff845d9e5e8
Makman2 <makman@alice.de>
2016-11-19 21:29:07 +0100

    Processing: Remove unused import

--- coalib/processes/Processing.py
+++ coalib/processes/Processing.py
@@ -5,7 +5,6 @@ import queue
 import subprocess
 from itertools import chain
 
-from coalib.collecting import Dependencies
 from coalib.collecting.Collectors import collect_files
 from coala_utils.string_processing.StringConverter import StringConverter
 from coalib.output.printers.LOG_LEVEL import LOG_LEVEL
