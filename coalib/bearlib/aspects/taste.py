b2e7dd4e578295436877be0d58a22826b8d42e23
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-11-20 17:31:58 +0100

    aspects.Taste: Allow restricting to languages

--- coalib/bearlib/aspects/taste.py
+++ coalib/bearlib/aspects/taste.py
@@ -1,5 +1,7 @@
 from coala_utils.decorators import enforce_signature
 
+from coalib.bearlib.languages import Language
+
 
 class TasteMeta(type):
     """
@@ -9,6 +11,29 @@ class TasteMeta(type):
 
        Taste[int](...)
     """
+    class Languages(tuple):
+        """
+        A ``tuple``-based container for
+        :class:`coalib.bearlib.languages.Language` instances.
+
+        It supports language identifiers in any format accepted by
+        ``Language[...]`` and provides :meth:`.__contains__` for checking
+        if a given language identifier is included:
+
+        >>> 'Python 2.7, 3.5' in Taste.Languages([Language.Python()])
+        True
+        >>> 'Py 3.3' in Taste.Languages(['Python 2'])
+        False
+        >>> 'csharp' in Taste.Languages(['C#', Language.Python == 3])
+        True
+        """
+
+        def __new__(cls, items):
+            return tuple.__new__(cls, (Language[i] for i in items))
+
+        def __contains__(self, item):
+            return any(item in lang for lang in self)
+
     def __getitem__(cls, _cast_type):
         class Taste(cls):
             cast_type = _cast_type
@@ -28,9 +53,9 @@ class Taste(metaclass=TasteMeta):
 
     @enforce_signature
     def __init__(self, description: str='', suggested_values: tuple=(),
-                 default=None):
+                 default=None, languages: tuple=()):
         """
-        No need to specify name an cast type:
+        No need to specify name and cast type:
 
         The taste name is defined by the taste's attribute name in an
         aspectclass definition.
@@ -40,3 +65,4 @@ class Taste(metaclass=TasteMeta):
         self.description = description
         self.suggested_values = suggested_values
         self.default = default
+        self.languages = type(self).Languages(languages)
