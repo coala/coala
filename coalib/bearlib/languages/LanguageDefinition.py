168608f8c57967e28c7c06782cc963c0b2f7327f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-11-21 11:29:04 +0100

    LanguageDefinition: Move documentation to class
    Otherwise it's not shown in API docs.

--- coalib/bearlib/languages/LanguageDefinition.py
+++ coalib/bearlib/languages/LanguageDefinition.py
@@ -8,57 +8,62 @@ from coalib.settings.Setting import Setting
 
 
 class LanguageDefinition(SectionCreatable):
+    """
+    **This class is deprecated!** Use the `Language` class instead.
+
+    A Language Definition holds constants which may help parsing the language.
+    If you want to write a bear you'll probably want to use those definitions
+    to keep your bear independent of the semantics of each language.
+
+    You can easily get your language definition by just creating it with
+    the name of the language desired:
+
+    >>> list(LanguageDefinition("cpp")['extensions'])
+    ['.c', '.cpp', '.h', '.hpp']
+
+    For some languages aliases exist, the name is case insensitive; they will
+    behave just like before and return settings:
+
+    >>> dict(LanguageDefinition('C++')['comment_delimiter'])
+    {'//': ''}
+    >>> dict(LanguageDefinition('C++')['string_delimiters'])
+    {'"': '"'}
+
+    If no language exists, you will get a ``FileNotFoundError``:
+
+    >>> LanguageDefinition("BULLSHIT!")
+    Traceback (most recent call last):
+     ...
+    FileNotFoundError
+
+    Custom coalangs are no longer supported. You can simply register your
+    languages to the Languages decorator. When giving a custom coalang
+    directory a warning will be emitted and it will attempt to load the given
+    Language anyway through conventional means:
+
+    >>> LanguageDefinition("custom", coalang_dir='somewhere')
+    Traceback (most recent call last):
+     ...
+    FileNotFoundError
+
+    If you need a custom language, just go like this:
+
+    >>> @Language
+    ... class MyLittlePony:
+    ...     color = 'green'
+    ...     legs = 5
+    >>> int(LanguageDefinition('mylittlepony')['legs'])
+    5
+
+    But seriously, just use `Language` - and mind that it's already typed:
+
+    >>> Language['mylittlepony'].get_default_version().legs
+    5
+    """
 
     def __init__(self, language: str, coalang_dir=None):
         """
         Creates a new LanguageDefinition object from file.
-
-        THIS FUNCTION IS DEPRECATED. Use the Language class instead.
-
-        A Language Definition holds constants which may help parsing the
-        language. If you want to write a bear you'll probably want to use those
-        definitions to keep your bear independent of the semantics of each
-        language.
-
-        You can easily get your language definition by just creating it with
-        the name of the language desired:
-
-        >>> list(LanguageDefinition("cpp")['extensions'])
-        ['.c', '.cpp', '.h', '.hpp']
-
-        For some languages aliases exist, the name is case insensitive; they
-        will behave just like before and return settings:
-
-        >>> dict(LanguageDefinition('C++')['comment_delimiter'])
-        {'//': ''}
-        >>> dict(LanguageDefinition('C++')['string_delimiters'])
-        {'"': '"'}
-
-        If no language exists, you will get a ``FileNotFoundError``:
-
-        >>> LanguageDefinition("BULLSHIT!")
-        Traceback (most recent call last):
-         ...
-        FileNotFoundError
-
-        Custom coalangs are no longer supported. You can simply register your
-        languages to the Languages decorator. When giving a custom coalang
-        directory a warning will be emitted and it will attempt to load the
-        given Language anyway through conventional means:
-
-        >>> LanguageDefinition("custom", coalang_dir='somewhere')
-        Traceback (most recent call last):
-         ...
-        FileNotFoundError
-
-        If you need a custom language, just go like this:
-
-        >>> @Language
-        ... class MyLittlePony:
-        ...     color = 'green'
-        ...     legs = 5
-        >>> int(LanguageDefinition('mylittlepony')['legs'])
-        5
 
         :param language:           The actual language (e.g. C++).
         :param coalang_dir:        Path to directory with coalang language
