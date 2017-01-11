cb2ea278995c8640852ae48182cc4404a26fffe2
rishikksh20 <rishikksh20@gmail.com>
2017-01-11 09:01:45 +0530

    CSharp: Add fields for CSharp language support
    Need for CSharp language compatability to support AnnotationBear.
    
    Closes https://github.com/coala/coala/issues/3514

--- coalib/bearlib/languages/definitions/CSharp.py
+++ coalib/bearlib/languages/definitions/CSharp.py
@@ -6,3 +6,6 @@ class CSharp:
     __qualname__ = 'C#'
     aliases = 'CS',
     extensions = '.cs',
+    comment_delimiter = '//'
+    multiline_comment_delimiters = {'/*': '*/'}
+    string_delimiters = {'"': '"'}
