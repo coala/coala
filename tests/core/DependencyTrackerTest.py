e5de0595ee3554f1e779beb8df61121f06db7bf0
Makman2 <makmanx64@gmail.com>
2017-01-11 01:15:54 +0100

    DependencyTracker: Add circular dependency check
    Adds a function to check for closed dependency chains.

--- tests/core/DependencyTrackerTest.py
+++ tests/core/DependencyTrackerTest.py
@@ -1,9 +1,22 @@
 import unittest
 
+from coalib.core.CircularDependencyError import CircularDependencyError
 from coalib.core.DependencyTracker import DependencyTracker
 
 
 class DependencyTrackerTest(unittest.TestCase):
+
+    def test_check_circular_dependencies(self):
+        uut = DependencyTracker()
+        uut.add(0, 1)
+        uut.add(1, 2)
+
+        uut.check_circular_dependencies()
+
+        uut.add(2, 0)
+
+        with self.assertRaises(CircularDependencyError):
+            uut.check_circular_dependencies()
 
     def test_resolve(self):
         uut = DependencyTracker()
