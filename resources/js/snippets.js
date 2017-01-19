b08888b98254eaa8226882867605cccc18a15565
Hemang Kumar <hemangsk@gmail.com>
2017-01-19 15:47:19 +0530

    Add files

---
+++ resources/js/snippets.js
@@ -0,0 +1,14 @@
+var snippets = {
+
+"Python" :
+"$ cat file.py \n" + 
+"$ print ('hello, here is some   inconsistent space') \n" +
+"$ coala --bears=PEP8Bear --files=file.py",
+
+"C++" :
+"$ cat py \n" + 
+"$ #include <iostream> \n" +
+"$ void main(){  		} \n" +
+"$ coala --bears=SpaceConsistencyBear --files=*.cpp \n"
+
+}
