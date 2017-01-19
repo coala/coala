f38e2bed4fd80c6e15391117aff440763d5b57c1
Hemang Kumar <hemangsk@gmail.com>
2017-01-19 16:28:52 +0530

    frontend: Fetch changes from GitLab upstream

--- resources/js/snippets.js
+++ resources/js/snippets.js
@@ -1,14 +1,12 @@
-var snippets = {
-
-"Python" :
-"$ cat file.py \n" + 
-"$ print ('hello, here is some   inconsistent space') \n" +
-"$ coala --bears=PEP8Bear --files=file.py",
-
-"C++" :
-"$ cat py \n" + 
-"$ #include <iostream> \n" +
-"$ void main(){  		} \n" +
-"$ coala --bears=SpaceConsistencyBear --files=*.cpp \n"
-
+var snippets = {	
+	"Python" : [
+	"cat file.py",
+	"print('Hey')",
+	"coala"
+],
+	"CPP" : [
+	"cat file.cpp",
+	"cout<<\"Hello World\"",
+	"coala"
+]
 }
