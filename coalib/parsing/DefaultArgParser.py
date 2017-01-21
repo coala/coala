dc4f890019f28a73bd30dcd8088909a27cdd2c2b
reetheshv <reetheshv.rv@gmail.com>
2017-01-21 22:14:04 +0530

    DefaultArgumentParser.py: Make find_config default
    Makes the parser default through default statement.
    
    Closes https://github.com/coala/coala/issues/2535

--- coalib/parsing/DefaultArgParser.py
+++ coalib/parsing/DefaultArgParser.py
@@ -110,7 +110,7 @@ To run coala without user interaction, run the `coala --non-interactive`,
             Constants.default_coafile))
 
     config_group.add_argument(
-        '-F', '--find-config', action='store_const', const=True,
+        '-F', '--find-config', action='store', default=True, type=bool,
         help='find {} in ancestors of the working directory'.format(
             Constants.default_coafile))
 
