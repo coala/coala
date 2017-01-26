b2a044bc5dd9a0ccff88e77b905462ca4b816ca0
Nitin Choudhary <nitin.iitkgp23@gmail.com>
2017-01-26 07:31:37 +0530

    DefaultArgParser.py: Add Default log level to help
    This adds default log level - INFO to `-L help`.
    
    Closes https://github.com/coala/coala/issues/3618

--- coalib/parsing/DefaultArgParser.py
+++ coalib/parsing/DefaultArgParser.py
@@ -166,7 +166,8 @@ To run coala without user interaction, run the `coala --non-interactive`,
     outputs_group.add_argument(
         '-L', '--log-level', nargs=1,
         choices=['ERROR', 'INFO', 'WARNING', 'DEBUG'], metavar='ENUM',
-        help='set log output level to ERROR/INFO/WARNING/DEBUG')
+        help='set log output level to ERROR/INFO/WARNING/DEBUG, '
+             'defaults to INFO')
 
     outputs_group.add_argument(
         '-m', '--min-severity', nargs=1,
