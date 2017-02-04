2040dd4693f9e480217fd9d70d14492a415f6bdc
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-02-05 00:08:22 +0100

    snippets: Add English sample

--- resources/js/snippets.js
+++ resources/js/snippets.js
@@ -1,6 +1,7 @@
 var snippets = {
        "HTML" : "html.md",
        "CSS" : "css.md",
+       "English" : "english.md"
        "JavaScript" : "javascript.md",
        "PHP" : "php.md",
        "Python" : "python.md",
