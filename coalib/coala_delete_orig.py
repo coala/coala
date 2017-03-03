710d71ecd54f0911e11862c289bfff2aa3a55752
Arjun Nemani <nemaniarjun@gmail.com>
2017-03-03 18:14:03 +0530

    coala-delete-orig: Sort Imports
    The imports should always be in alphabetical order.

--- coalib/coala_delete_orig.py
+++ coalib/coala_delete_orig.py
@@ -5,9 +5,9 @@ from pyprint.ConsolePrinter import ConsolePrinter
 from coalib.output.Logging import configure_logging
 from coalib.output.printers.LogPrinter import LogPrinter
 from coalib.parsing import Globbing
+from coalib.parsing.Globbing import glob_escape
 from coalib.settings.ConfigurationGathering import get_config_directory
 from coalib.settings.Section import Section
-from coalib.parsing.Globbing import glob_escape
 
 
 def main(log_printer=None, section: Section=None):
