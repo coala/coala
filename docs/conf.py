aaf42d86845ac35d3f1c6ff797086577e48c3875
Hemang Kumar <hemangsk@gmail.com>
2017-03-10 22:14:07 +0530

    conf.py: Change pygments theme
    The 'sphinx' pygments theme is not fitting on dark backgrounds, so
    replaced 'sphinx' with 'native', which is more effective on dark
    backgrounds.

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
