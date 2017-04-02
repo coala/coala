8da816471f5d2bd6423650b2dfd54460a772a2c6
MariosPanag <mariospanag96@gmail.com>
2017-04-03 01:06:53 +0300

    LanguageTest.py: Add tests for Languages class
    Add tests in coalib.bearlib.languages.Language.py
    for Languages class.
    
    Closes https://github.com/coala/coala/issues/4037

--- tests/bearlib/languages/LanguageTest.py
+++ tests/bearlib/languages/LanguageTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from coalib.bearlib.languages.Language import Language, LanguageMeta
+from coalib.bearlib.languages.Language import Language, LanguageMeta, Languages
 
 
 class LanguageTest(unittest.TestCase):
@@ -27,3 +27,28 @@ class LanguageAttributeErrorTest(unittest.TestCase):
     def test_attribute_list_empy(self):
         with self.assertRaisesRegexp(AttributeError, 'no available attribute'):
             self.lang_unknown.not_an_attribute
+
+
+class LanguagesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tuple_derived = Languages([Language.Python == 3])
+        for x in self.tuple_derived:
+            self.assertIsInstance(x, Language)
+        self.tuple = (Language['Python 3.3, 3.4, 3.5, 3.6'],)
+
+    def tearDown(self):
+        pass
+
+    def test_invalid_args(self):
+        with self.assertRaises(TypeError):
+            Languages()
+        with self.assertRaises(AttributeError):
+            Languages(['nonexistentLanguage'])
+
+    def test_tuple(self):
+        self.assertTupleEqual(self.tuple_derived, self.tuple)
+
+    def test_contains(self):
+        self.assertTrue('Python 3.3, 3.5' in self.tuple_derived)
+        self.assertFalse('Py 2.7' in self.tuple_derived)
