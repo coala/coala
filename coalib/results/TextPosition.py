c62459b56679150a0402a06a2f95d31200eec8fc
aptrishu <code.rishus23@gmail.com>
2017-04-20 10:53:16 +0530

    TextPosition.py: Use double backticks

--- coalib/results/TextPosition.py
+++ coalib/results/TextPosition.py
@@ -9,14 +9,14 @@ class TextPosition:
     @enforce_signature
     def __init__(self, line: (int, None)=None, column: (int, None)=None):
         """
-        Creates a new TextPosition object that represents the position inside
-        a string with line/column numbers.
+        Creates a new ``TextPosition`` object that represents the position
+        inside a string with line/column numbers.
 
-        :param line:        The line in file or None, the first line is 1.
+        :param line:        The line in file or ``None``, the first line is 1.
         :param column:      The column indicating the character. The first one
                             in a line is 1.
         :raises TypeError:  Raised when line or columns are no integers.
-        :raises ValueError: Raised when a column is set but line is None.
+        :raises ValueError: Raised when a column is set but line is ``None``.
         """
         if line is None and column is not None:
             raise ValueError('A column can only be set if a line is set.')
