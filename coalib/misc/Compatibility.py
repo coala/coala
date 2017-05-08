8da8726eee3996b3b9531a973def2f7c50612b2b
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-05-08 22:46:10 +0530

    compatibility.py: Adds comment about missing JSONDecodeError
    This comment explains that JSONDecodeError is missing in Python 3.4
    
    Fixes https://github.com/coala/coala/issues/4156

--- coalib/misc/Compatibility.py
+++ coalib/misc/Compatibility.py
@@ -1,4 +1,9 @@
 import json
+"""
+json.decoder.JSONDecodeError gives an AttributeError
+because JSONDecodeError missing in Python 3.4
+
+"""
 try:
     JSONDecodeError = json.decoder.JSONDecodeError
 except AttributeError:  # pragma: no cover
