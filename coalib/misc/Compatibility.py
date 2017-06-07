6d7512ce9afd562905d1d935f63c9d72c1f89fc9
John Vandenberg <jayvdb@gmail.com>
2017-06-07 18:26:53 +0700

    Compatibility.py: Require coverage on Python 3.4
    The Python 3.4 CI jobs should require coverage of the
    code adding backwards compatibility for JSONDecodeError
    which is missing only in Python 3.4.
    
    Closes https://github.com/coala/coala/issues/4157

--- coalib/misc/Compatibility.py
+++ coalib/misc/Compatibility.py
@@ -1,5 +1,5 @@
 import json
 try:
     JSONDecodeError = json.decoder.JSONDecodeError
-except AttributeError:  # pragma: no cover
+except AttributeError:  # pragma Python 3.5,3.6: no cover
     JSONDecodeError = ValueError
