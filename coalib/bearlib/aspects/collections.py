852ba5e277f951a5a5f576860aa6e2a11d6f2d24
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-06-22 22:42:53 +0700

    AspectList: Overload __init__ to accept strings
    Overload AspectList so it could be instanced from a list of string
    containing aspects name. The reason of this overloading is to make
    it possible to instanced list of desired aspects name found in
    coafiles.
    
    ```python
    >>> AspectList(['coalaCorrect', 'aspectsYEAH'])
    [<aspectclass '...coalaCorrect'>, <aspectclass '...aspectsYEAH'>]
    ```
    
    Replacement of https://github.com/coala/coala/pull/4379
    Closes https://github.com/coala/coala/issues/4382

--- coalib/bearlib/aspects/collections.py
+++ coalib/bearlib/aspects/collections.py
@@ -1,4 +1,5 @@
-from coalib.bearlib.aspects.meta import issubaspect, assert_aspect
+import coalib.bearlib.aspects
+from coalib.bearlib.aspects.meta import isaspect, issubaspect
 
 
 class AspectList(list):
@@ -7,7 +8,22 @@ class AspectList(list):
     """
 
     def __init__(self, seq=()):
-        super().__init__(map(assert_aspect, seq))
+        """
+        Initialize a new AspectList.
+
+        >>> from .Metadata import CommitMessage
+        >>> AspectList([CommitMessage.Shortlog, CommitMessage.Body])
+        [<aspectclass '...Shortlog'>, <aspectclass '...Body'>]
+        >>> AspectList(['Shortlog', 'CommitMessage.Body'])
+        [<aspectclass '...Shortlog'>, <aspectclass '...Body'>]
+        >>> AspectList([CommitMessage.Shortlog, 'CommitMessage.Body'])
+        [<aspectclass '...Shortlog'>, <aspectclass '...Body'>]
+
+        :param seq: A sequence containing either aspectclass, aspectclass
+                    instance, or string of partial/full qualified aspect name.
+        """
+        super().__init__((item if isaspect(item) else
+                          coalib.bearlib.aspects[item] for item in seq))
 
     def __contains__(self, aspect):
         for item in self:
