faf4184fc8dc9923f1cd7a3b77b255ca214d0841
Hemang Kumar <hemangsk@gmail.com>
2017-06-23 20:44:28 +0530

    exts.js: Add extensions array

---
+++ resources/js/exts.js
@@ -0,0 +1,5 @@
+/*
+Extensions from coala-utils python library
+https://gitlab.com/coala/coala-utils/blob/master/coala_utils/Extensions.py
+*/
+exts = ['XML', 'Markdown', 'C++', 'Ruby', 'TeX', 'Perl', 'TypeScript', 'Python', 'Cuda', 'PHP', 'CoffeeScript', 'Lua', 'JavaScript', 'C#', 'YAML', 'Objective-C', 'Fortran', 'OpenCL', 'HTML', 'VHDL', 'C', 'Go', 'R', 'SCSS', 'reStructuredText', 'Scala', 'SQL', 'CMake', 'Haskell', 'CSS', 'Matlab', 'Octave', 'Verilog', 'Java', 'Julia', 'JSON', 'Dart'];
