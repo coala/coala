633399f6ba9dbead90546f088701fbd521f3db72
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-06-29 23:17:07 +0700

    getinvolvedDirective.js: Exclude bots
    Exclude rultor and gitmate-bot
    
    Fixes https://github.com/coala/landing-frontend/issues/129

--- resources/js/Directives/getinvolvedDirective.js
+++ resources/js/Directives/getinvolvedDirective.js
@@ -6,13 +6,22 @@ app.directive('getinvolved', ['$http', function ($http) {
             var self = this
             $scope.$get_involved_storage = $sessionStorage
             self.contributors
+
+            function excludeBot(contributors) {
+                return contributors.filter(function(member){
+                    if (member.login == 'rultor') return false
+                    if (member.login == 'gitmate-bot') return false
+                    return true
+                })
+            }
+
             if($scope.$get_involved_storage.contributors_data){
-                self.contributors = $scope.$get_involved_storage.contributors_data
+                self.contributors = excludeBot($scope.$get_involved_storage.contributors_data)
             }else{
                 $http.get(api_link + '/contrib/')
                     .then(function (data) {
                         $scope.$get_involved_storage.contributors_data = data["data"]
-                        self.contributors = data["data"]
+                        self.contributors = excludeBot(data["data"])
                     }).catch(function (c) {
                     console.log(c);
                 })
