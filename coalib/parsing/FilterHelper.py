a690322c65588712648e5eae70cd2d606070d1cf
Ayush Shridhar <ayush.shridhar1999@gmail.com>
2017-07-27 18:19:04 +0530

    Error fixed

--- coalib/parsing/FilterHelper.py
+++ coalib/parsing/FilterHelper.py
@@ -1,27 +1,26 @@
 from coalib.output.printers.LogPrinter import LogPrinter
-
 from coalib.parsing.InvalidFilterException import InvalidFilterException
-
 from coalib.parsing.filters.LanguageFilter import language_filter
 from coalib.parsing.filters.CanDetectFilter import can_detect_filter
 from coalib.parsing.filters.CanFixFilter import can_fix_filter
 
-def get_all_filters_str(cls, sep=', '):
-    return sep.join(sorted(cls.available_filters))
+def get_all_filters_str(arg, sep=', '):
+    return sep.join(sorted(arg.available_filters))
 
 
-def is_valid_filter(cls, filter):
-    return filter in cls.available_filters
+def is_valid_filter(arg, filter):
+    return filter in arg.available_filters
 
-def get_filtered_bears(cls, filter, args, all_bears=None):
+
+def get_filtered_bears(arg, filter, args, all_bears=None):
     if all_bears is None:
         from coalib.settings.ConfigurationGathering import (
             get_all_bears)
         all_bears = get_all_bears(LogPrinter())
-    if not cls.is_valid_filter(filter):
+    if not arg.is_valid_filter(filter):
         raise InvalidFilterException('{!r} is an invalid filter. '
                                            'Available filters: {}'.format(
-                                             filter, cls.get_all_filters_str()))
+                                             filter, arg.get_all_filters_str()))
     if not args or len(args) == 0:
         return all_bears
     return cls.available_filters[filter](all_bears, args)
