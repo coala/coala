1eb9d0f2ae675f5b4276b2733801a4fb37b21fcb
Ayush Shridhar <ayush.shridhar1999@gmail.com>
2017-07-30 19:22:24 +0530

    Patch fixed

--- coalib/parsing/InvalidFilterException.py
+++ coalib/parsing/InvalidFilterException.py
@@ -1,10 +1,8 @@
 class InvalidFilterException(Exception):
-    def __init__(self, filter_name=None, available_filters=None ):
-    	self.filter_name=filter_name
-    	self.available_filters=available_filters
+    def __init__(self, filter_name=None, available_filters=None):
+        self.filter_name = filter_name
+        self.available_filters = available_filters
 
     def __str__(self):
-    	res= self.filter_name+' is an invalid filter. Available filters: '+self.available_filters
-    	return res
-
- 
+        res= self.filter_name + ' is an invalid filter. Available filters: ' + self.available_filters
+        return res
