52c126b6de1ebb5756781baab5e0a85244f599e3
Ayush Shridhar <ayush.shridhar1999@gmail.com>
2017-08-02 18:59:53 +0530

    Changes made

--- coalib/parsing/InvalidFilterException.py
+++ coalib/parsing/InvalidFilterException.py
@@ -1,2 +1,17 @@
 class InvalidFilterException(Exception):
-    pass
+
+    def __init__(self, filter_name, available_filters):
+        self.filter_name = filter_name
+        self.available_filters = available_filters
+        if self.available_filters is None:
+            message = 'Invalid Filter'
+
+        if self.available_filters is not None:
+            message = 'Invalid filter. Available filters are : ' + \
+                     ' ,'.join(self.available_filters)
+
+        else:
+            message = self.filter_name + \
+                      ' is an invalid filter. Available filters: ' + \
+                      ' ,'.join(self.available_filters)
+        super().__init__(message)
