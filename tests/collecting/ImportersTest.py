c312bd3e01bf75cec3aef3a1971706037bd1bf12
John Vandenberg <jayvdb@gmail.com>
2017-08-04 10:07:53 +0700

    ImportersTest: Add detailed assertions
    The current assertions do little to assure the results
    are stable.
    
    Related to https://github.com/coala/coala/issues/4190

--- tests/collecting/ImportersTest.py
+++ tests/collecting/ImportersTest.py
@@ -2,6 +2,12 @@ import os
 import unittest
 from collections import OrderedDict
 
+from importlib.machinery import (
+    ModuleSpec,
+    SourceFileLoader,
+)
+from inspect import isclass
+
 from coalib.collecting.Importers import import_objects
 
 
@@ -9,18 +15,63 @@ class ImportObjectsTest(unittest.TestCase):
 
     def setUp(self):
         current_dir = os.path.split(__file__)[0]
-        self.testfile1_path = os.path.join(current_dir,
-                                           'importers_test_dir',
+        self.test_dir = os.path.join(current_dir, 'importers_test_dir')
+        self.testfile1_path = os.path.join(self.test_dir,
                                            'file_one.py')
-        self.testfile2_path = os.path.join(current_dir,
-                                           'importers_test_dir',
+        self.testfile2_path = os.path.join(self.test_dir,
                                            'file_two.py')
+
+    def check_imported_file_one_test(self, obj):
+        self.assertTrue(isclass(obj))
+
+        self.assertEqual(obj.__name__, 'test')
+        self.assertEqual(obj.__module__, 'file_one')
+
+        instance = obj()
+        self.assertIsInstance(instance, list)
 
     def test_no_file(self):
         self.assertEqual(import_objects([]), [])
 
-    def test_no_data(self):
-        self.assertEqual(len(import_objects(self.testfile1_path)), 12)
+    def test_file_one_internal_structure(self):
+        objs = import_objects(self.testfile1_path)
+        self.assertIsInstance(objs, list)
+        self.assertEqual(len(objs), 12)
+
+        # [0] is the module __dict__
+        self.assertIsInstance(objs[0], dict)
+        self.assertIn('__name__', objs[0])
+        self.assertEqual(objs[0]['__name__'], 'builtins')
+        self.assertIn('copyright', objs[0])
+        # [1] is full filename
+        self.assertIsInstance(objs[1], str)
+        self.assertTrue(objs[1].endswith('.pyc'))
+        self.assertTrue(objs[1].startswith(self.test_dir))
+        # [2] is __doc__
+        self.assertIsNone(objs[2])
+        # [3] is the filename
+        self.assertIsInstance(objs[3], str)
+        self.assertEqual(objs[3], self.testfile1_path)
+        # [4] is the loader
+        self.assertIsInstance(objs[4], SourceFileLoader)
+        # [5] is the module name
+        self.assertIsInstance(objs[5], str)
+        self.assertEqual(objs[5], 'file_one')
+        # [6] is the package name
+        self.assertIsInstance(objs[6], str)
+        self.assertEqual(objs[6], '')
+        # [7] is the module spec object
+        self.assertIsInstance(objs[7], ModuleSpec)
+        # [8] and [9] are module members 'a' and 'b'
+        self.assertIsInstance(objs[8], list)
+        self.assertEqual(objs[8], [1, 2, 3])
+        self.assertIsInstance(objs[9], list)
+        self.assertEqual(objs[9], [1, 2, 4])
+        # [10] is the module member 'name'
+        self.assertIsInstance(objs[10], bool)
+        self.assertIs(objs[10], True)
+        # [11] is the module member 'test'
+        self.check_imported_file_one_test(objs[11])
 
     def test_name_import(self):
         self.assertEqual(
@@ -33,11 +84,13 @@ class ImportObjectsTest(unittest.TestCase):
             0)
 
     def test_type_import(self):
-        self.assertEqual(
-            len(import_objects(self.testfile1_path,
-                               types=list,
-                               verbose=True)),
-            2)
+        objs = import_objects(self.testfile1_path, types=list, verbose=True)
+        self.assertEqual(len(objs), 2)
+        self.assertIsInstance(objs[0], list)
+        self.assertEqual(objs[0], [1, 2, 3])
+        self.assertIsInstance(objs[1], list)
+        self.assertEqual(objs[1], [1, 2, 4])
+
         self.assertEqual(
             len(import_objects((self.testfile1_path, self.testfile2_path),
                                names='name',
@@ -46,11 +99,11 @@ class ImportObjectsTest(unittest.TestCase):
             0)
 
     def test_class_import(self):
-        self.assertEqual(
-            len(import_objects((self.testfile1_path, self.testfile2_path),
-                               supers=list,
-                               verbose=True)),
-            1)
+        objs = import_objects((self.testfile1_path, self.testfile2_path),
+                              supers=list, verbose=True)
+        self.assertEqual(len(objs), 1)
+        self.check_imported_file_one_test(objs[0])
+
         self.assertEqual(
             len(import_objects((self.testfile1_path, self.testfile2_path),
                                supers=str,
@@ -58,12 +111,13 @@ class ImportObjectsTest(unittest.TestCase):
             0)
 
     def test_attribute_import(self):
-        self.assertEqual(
-            len(import_objects((self.testfile1_path, self.testfile2_path),
-                               attributes='method',
-                               local=True,
-                               verbose=True)),
-            1)
+        objs = import_objects((self.testfile1_path, self.testfile2_path),
+                              attributes='method',
+                              local=True,
+                              verbose=True)
+        self.assertEqual(len(objs), 1)
+        self.check_imported_file_one_test(objs[0])
+
         self.assertEqual(
             len(import_objects((self.testfile1_path, self.testfile2_path),
                                attributes='something',
