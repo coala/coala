4118ef01d97e185f6745c2047e71ce2c51b71fd7
Ravi Teja Gannavarapu <grt4004@gmail.com>
2017-09-17 15:27:39 +0530

    Remove use of line continuation
    Remove use of line continuation completely.
    
    Closes https://github.com/coala/coala/issues/4552

--- coalib/bearlib/languages/Language.py
+++ coalib/bearlib/languages/Language.py
@@ -271,7 +271,7 @@ class Language(metaclass=LanguageMeta):
     the class:
 
     >>> Language.TrumpScript.comment_delimiter
-    OrderedDict([(<Version('2.7')>, '#'), (<Version('3.3')>, '#'), 
+    OrderedDict([(<Version('2.7')>, '#'), (<Version('3.3')>, '#'),
 (<Version('3.4')>, '#'), (<Version('3.5')>, '#'), (<Version('3.6')>, '#')])
 
     Any nonexistent item will of course not be served:
