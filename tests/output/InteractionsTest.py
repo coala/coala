66a3d813e750321939dbfce221f193171e315b77
Alphadose <anish.mukherjee1996@gmail.com>
2017-11-13 04:20:34 +0530

    Interactions.py: Deprecate section parameter
    Closes https://github.com/coala/coala/issues/4740

--- tests/output/InteractionsTest.py
+++ tests/output/InteractionsTest.py
@@ -1,6 +1,7 @@
 import unittest
 
 from pyprint.NullPrinter import NullPrinter
+from testfixtures import LogCapture
 
 from coalib.output.Interactions import fail_acquire_settings
 from coalib.output.printers.LogPrinter import LogPrinter
@@ -11,11 +12,21 @@ class InteractionsTest(unittest.TestCase):
 
     def test_(self):
         log_printer = LogPrinter(NullPrinter())
-        section = Section('')
-        self.assertRaises(TypeError, fail_acquire_settings, log_printer, None,
-                          section)
+        self.assertRaises(TypeError, fail_acquire_settings, log_printer, None)
         self.assertRaises(AssertionError,
                           fail_acquire_settings,
                           log_printer,
-                          {'setting': ['description', 'bear']}, section)
-        self.assertEqual(fail_acquire_settings(log_printer, {}, section), None)
+                          {'setting': ['description', 'bear']})
+        self.assertEqual(fail_acquire_settings(log_printer, {}), None)
+
+    def test_section_deprecation(self):
+        section = Section('')
+        log_printer = LogPrinter(NullPrinter())
+        with LogCapture() as capture:
+            fail_acquire_settings(log_printer, {}, section)
+            capture.check(
+                ('root',
+                 'WARNING',
+                 'fail_acquire_settings: section parameter '
+                 'is deprecated.')
+            )
