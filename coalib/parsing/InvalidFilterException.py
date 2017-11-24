7d4d0787da7da2287585a26e35274a73aa8d0e86
rhapsody <dean.lin@umbocv.com>
2017-11-25 01:12:40 +0800

    InvalidFilterException: Change base class
    Change base class from Exception to LookupError.
    
    Closes https://github.com/coala/coala/issues/4873

--- coalib/parsing/InvalidFilterException.py
+++ coalib/parsing/InvalidFilterException.py
@@ -1,7 +1,7 @@
 from coalib.parsing.filters import available_filters
 
 
-class InvalidFilterException(Exception):
+class InvalidFilterException(LookupError):
     def __init__(self, filter_name):
         super().__init__('{!r} is an invalid filter. Available filters: {}'
                          .format(filter_name,
