74fe49a75298e359154e2bd5d4a8545a5a077df9
Alphadose <anish.mukherjee1996@gmail.com>
2017-11-29 03:57:28 +0530

    Interactions.py: Remove section parameter
    Closes https://github.com/coala/coala/issues/4866

--- tests/output/InteractionsTest.py
+++ tests/output/InteractionsTest.py
@@ -1,11 +1,9 @@
 import unittest
 
 from pyprint.NullPrinter import NullPrinter
-from testfixtures import LogCapture
 
 from coalib.output.Interactions import fail_acquire_settings
 from coalib.output.printers.LogPrinter import LogPrinter
-from coalib.settings.Section import Section
 
 
 class InteractionsTest(unittest.TestCase):
@@ -18,15 +16,3 @@ class InteractionsTest(unittest.TestCase):
                           log_printer,
                           {'setting': ['description', 'bear']})
         self.assertEqual(fail_acquire_settings(log_printer, {}), None)
-
-    def test_section_deprecation(self):
-        section = Section('')
-        log_printer = LogPrinter(NullPrinter())
-        with LogCapture() as capture:
-            fail_acquire_settings(log_printer, {}, section)
-            capture.check(
-                ('root',
-                 'WARNING',
-                 'fail_acquire_settings: section parameter '
-                 'is deprecated.')
-            )
