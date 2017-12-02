bef43781cd5a317669999e60709dc21d4a40ff72
Timothy Kanarsky <tkanarsky@outlook.com>
2017-12-01 19:01:25 -0800

    Appease the gitmate bot

--- tests/coalaTest.py
+++ tests/coalaTest.py
@@ -337,6 +337,7 @@ class coalaTest(unittest.TestCase):
             self.assertIn('-d flag works!', stdout)
             if "This result has no patch attached" not in stderr:
                 self.assertEqual(
-                    retval, 0, 'coala must return zero when there are no errors')
+                    retval, 0, 
+                    'coala must return zero when there are no errors')
             self.assertEqual(
                 retval, 1, "no patch attached error kludge")
