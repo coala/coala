c267a3681f1a15aeb4e7fe1abb1c25ec0b19fbe1
Shreya <shreyateeza@gmail.com>
2017-12-10 23:30:37 +0530

    Compatibility.py: Add error message
    Add comment explaining JSONDecodeError is available from Python3.5.x.
    
    Closes #4156

--- coalib/misc/Compatibility.py
+++ coalib/misc/Compatibility.py
@@ -1,4 +1,5 @@
 import json
+# JSONDecodeError class is available starting from Python 3.5.x.
 try:
     JSONDecodeError = json.decoder.JSONDecodeError
 except AttributeError:  # pragma Python 3.5,3.6: no cover
