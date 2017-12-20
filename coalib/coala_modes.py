842ed5cfec7090b631e6bb0e37ee7b0416aac371
abdulghaffar <abdulzainulabedin@gmail.com>
2017-12-19 23:58:05 -0500

    added a deprecation warning when log_printer param is used

--- coalib/coala_modes.py
+++ coalib/coala_modes.py
@@ -9,6 +9,15 @@ def mode_normal(console_printer, log_printer, args, debug=False):
                             and not catching any exceptions.
     """
     import functools
+    import logging
+
+    try: log_printer
+    except empty: log_printer = None
+
+    if log_printer is None:
+        pass
+    else:
+        logging.warning("Warning: The use of log_printer is deprecated")
 
     from coalib.coala_main import run_coala
     from coalib.output.ConsoleInteraction import (
