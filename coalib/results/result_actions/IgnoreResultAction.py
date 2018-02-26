25899ec7d007d12dcb2fe9a92ebc0cbd590e0892
Alphadose <anish.mukherjee1996@gmail.com>
2018-02-27 01:22:56 +0530

    IgnoreResultAction.py: Fix line length violation
    IgnoreResultAction violates line length restrictions.
    This commit fixes the bug by using block style comments.
    
    Fixes https://github.com/coala/coala/issues/4851

--- coalib/results/result_actions/IgnoreResultAction.py
+++ coalib/results/result_actions/IgnoreResultAction.py
@@ -46,22 +46,23 @@ class IgnoreResultAction(ResultAction):
         """
         Add (I)gnore comment
         """
+        source_range = next(filter(lambda sr: exists(sr.file),
+                                   result.affected_code))
+        filename = source_range.file
+        ignore_diff = Diff(original_file_dict[filename])
+        line = original_file_dict[filename][source_range.start.line-1]
+        spaces = ' '*(len(line)-len(line.lstrip()))
 
-        ignore_comment = self.get_ignore_comment(result.origin, language)
+        ignore_comment, multiline = self.encase_ignore_comment(language, spaces)
 
         if not ignore_comment:
             return file_diff_dict
 
-        source_range = next(filter(lambda sr: exists(sr.file),
-                                   result.affected_code))
-        filename = source_range.file
+        original_line, new_line, position = self.get_diff(
+                        result.origin, original_file_dict[filename],
+                        source_range.start.line-1, ignore_comment, multiline)
 
-        ignore_diff = Diff(original_file_dict[filename])
-        ignore_diff.change_line(
-            source_range.start.line,
-            original_file_dict[filename][source_range.start.line-1],
-            original_file_dict[filename][source_range.start.line-1].rstrip() +
-            '  ' + ignore_comment)
+        ignore_diff.change_line(position, original_line, new_line)
 
         if filename in file_diff_dict:
             ignore_diff = file_diff_dict[filename] + ignore_diff
@@ -78,24 +79,31 @@ class IgnoreResultAction(ResultAction):
 
         return file_diff_dict
 
-    def get_ignore_comment(self, origin, language):
+    def encase_ignore_comment(self, language, spaces: str=''):
         r"""
-        Returns a string of Ignore Comment, depending on the language
+        Returns a tuple consisting of a formattable string of Ignore
+        Comment depending on the language and a boolean variable stating
+        if the Comment is in Multiline format or not.
         Supports Single Line Comments
 
-        >>> IgnoreResultAction().get_ignore_comment("Bear", "css")
-        '/* Ignore Bear */\n'
+        :param language:
+            The language in which the source file is written.
+        :param spaces:
+            The indentation of each line.
+
+        >>> IgnoreResultAction().encase_ignore_comment("css")
+        ('/* {} */\n', True)
 
         And Multiline Comments
 
-        >>> IgnoreResultAction().get_ignore_comment("Bear", "c")
-        '// Ignore Bear\n'
+        >>> IgnoreResultAction().encase_ignore_comment("c")
+        ('// {}\n', False)
         """
         try:
             comment_delimiter = Language[
                 language].get_default_version().comment_delimiter
-            ignore_comment = (str(comment_delimiter) + ' Ignore ' +
-                              origin + '\n')
+            ignore_comment = (spaces + str(comment_delimiter) + ' {}\n')
+            multiline = False
         except AttributeError:
             # singleline comments not supported by language
             try:
@@ -103,9 +111,9 @@ class IgnoreResultAction(ResultAction):
                     language].get_default_version().multiline_comment_delimiters
                 start_comment, end_comment = next(iter(
                                         multiline_comment_delimiter.items()))
-                ignore_comment = (str(start_comment) + ' Ignore ' +
-                                  origin + ' ' +
+                ignore_comment = (spaces + str(start_comment) + ' {} ' +
                                   str(end_comment) + '\n')
+                multiline = True
             except UnknownLanguageError:
                 # multiline comments also not supported by language
                 logging.warning(
@@ -113,5 +121,58 @@ class IgnoreResultAction(ResultAction):
                     ' opening an issue at https://github.com/coala/coala/issues'
                     ' so we can add support for this language.'.format(
                         language=language))
-                ignore_comment = None
-        return ignore_comment
+                ignore_comment = multiline = None
+
+        return ignore_comment, multiline
+
+    def get_diff(self, origin, file, index, ignore_comment, multiline):
+        r"""
+        Returns a tuple consisting of Original Line, Changed Line and
+        its position in the file
+
+        >>> ignore_comment = '// {}\n'
+        >>> file = ['1\n', '2\n', '3\n']
+        >>> IgnoreResultAction().get_diff('origin', file, 1,
+        ...                               ignore_comment, False)
+        ('2\n', '// Start Ignoring origin\n2\n// Stop Ignoring\n', 2)
+
+        >>> file = ['1\n', '// Start Ignoring origin\n', '2\n',
+        ...         '// Stop Ignoring\n', '3\n']
+        >>> IgnoreResultAction().get_diff('origin2', file, 2,
+        ...                               ignore_comment, False)
+        ('// Start Ignoring origin\n', '// Start Ignoring origin, origin2\n', 2)
+
+        >>> file = ['1\n', '// Start Ignoring origin\n', '2\n',
+        ...         '// Stop Ignoring\n', '3\n']
+        >>> for i in range(2,25):
+        ...     _, file[1], _ = IgnoreResultAction().get_diff(
+        ...                     'origin'+str(i), file, 2, ignore_comment, False)
+        """
+        if file[index+1] != ignore_comment.format('Stop Ignoring'):
+            new_line = (ignore_comment.format('Start Ignoring ' + origin) +
+                        file[index] + ignore_comment.format('Stop Ignoring'))
+            original_line = file[index]
+            position = index+1
+
+        elif len(file[index-1] + ', ' + origin) > 80:
+            new_line = file[index-1].split()
+            new_line = (' '.join(new_line[1:len(new_line)-1])
+                        if multiline is True
+                        else
+                        ' '.join(new_line[1:len(new_line)]))
+            new_line = (ignore_comment.format(new_line + ',') +
+                        ignore_comment.format(origin))
+            original_line = file[index-1]
+            position = index
+
+        else:
+            new_line = file[index-1].split()
+            new_line = (' '.join(new_line[1:len(new_line)-1])
+                        if multiline is True
+                        else
+                        ' '.join(new_line[1:len(new_line)]))
+            new_line = ignore_comment.format(new_line + ', ' + origin)
+            original_line = file[index-1]
+            position = index
+
+        return original_line, new_line, position
