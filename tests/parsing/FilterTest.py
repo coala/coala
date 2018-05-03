f61fd0245f7af3cb3f16736e5398792c79533d8f
Mihai Tiganus <mtiganus@gmail.com>
2018-05-03 21:36:40 +0300

    FilterTest.py: Import `available_filters` directly
    Import `available_filters` directly from `coalib.parsing.filters`.
    
    Closes https://github.com/coala/coala/issues/5331

--- tests/parsing/FilterTest.py
+++ tests/parsing/FilterTest.py
@@ -1,7 +1,7 @@
 import unittest
 
 from coalib import coala
-from coalib.parsing.FilterHelper import available_filters
+from coalib.parsing.filters import available_filters
 from tests.TestUtilities import (
     bear_test_module,
     execute_coala,
