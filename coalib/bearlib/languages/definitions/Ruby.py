8dff86f5d127b470277b5fa7a5fce5b95c6b87ee
Yana Agun Siswanto <yana.developer@gmail.com>
2018-06-27 21:42:39 +0700

    Ruby.py: Add ruby definition
    add aliases, versions, extensions, comment_delimiter
    multiline_comment_delimiters, string_delimiters, and encapsulators
    
    Closes https://github.com/coala/coala/issues/5572

--- coalib/bearlib/languages/definitions/Ruby.py
+++ coalib/bearlib/languages/definitions/Ruby.py
@@ -3,4 +3,9 @@ from coalib.bearlib.languages.Language import Language
 
 @Language
 class Ruby:
-    pass
+    aliases = 'rb',
+    extensions = '.rb',
+    comment_delimiter = '#'
+    multiline_comment_delimiters = {'=begin': '=end'}
+    string_delimiters = {'"': '"', "'": "'", '%q(': ')', '%Q(': ')', '%(': ')'}
+    encapsulators = {'[': ']', '{': '}', '%w(': ')', '%W(': ')'}
