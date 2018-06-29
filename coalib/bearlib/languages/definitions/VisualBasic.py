c0a78c5c718f93d1bb79926a6e71746b2184a25f
pareksha <pareksha.manchanda@gmail.com>
2018-06-29 10:53:17 +0530

    VisualBasic: Create language file for Visual Basic
    This creates a basic language file for Visual Basic in
    coala language definitions.
    
    Closes https://github.com/coala/coala/issues/5578

---
+++ coalib/bearlib/languages/definitions/VisualBasic.py
@@ -0,0 +1,12 @@
+from coalib.bearlib.languages.Language import Language
+
+
+@Language
+class VisualBasic:
+    aliases = 'vb',
+    extensions = '.vb', '.bas'
+    comment_delimiter = "'", 'REM'
+    string_delimiters = {'"': '"'}
+    # Multiline strings are only supported by Visual Basic 14.0 and above
+    multiline_string_delimiters = string_delimiters
+    encapsulators = {'(': ')', '{': '}'}
