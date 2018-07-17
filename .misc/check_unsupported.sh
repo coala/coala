2843072eeb0d63d31958e6680c074c6fcfe0ff8f
John Vandenberg <jayvdb@gmail.com>
2018-07-17 14:45:17 +0700

    check_unsupported.sh: Use template
    Small improvements by sharing the logic with bears repository.
    The use of `coverage run` has been discontinued as coverage of
    `setup.py` is not needed for assert_supported_version
    as bears repo now has mock tests for that function.
    
    Related to https://gitlab.com/coala/mobans/issues/65

--- .misc/check_unsupported.sh
+++ .misc/check_unsupported.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+# Non-zero exit code is what we want to check
 set +e
 
-# Enable capturing the non-zero exit status of coverage instead of tee
+# Enable capturing the non-zero exit status of setup.py instead of tee
 set -o pipefail
 
-coverage run setup.py install | tee setup.log
+set -x
+
+python setup.py install 2>&1 | tee setup.log
 
 retval=$?
 
+set +x
+
 # coalib.__init__.py should exit with 4 on unsupported versions of Python
+# But bears setup.py sees retval 1.
 if [[ $retval != 4 ]]; then
-  echo "Unexpected error code $?"
+  echo "Unexpected error code $retval"
 
   # When the exit code is 0, use a non-zero exit code instead
   if [[ $retval == 0 ]]; then
@@ -23,6 +29,7 @@ fi
 # error when no lines selected by grep
 set -e
 
+# The following is emitted on stdout
 grep -q 'coala supports only python 3.4.4 or later' setup.log
 
 echo "Unsupported check completed successfully"
