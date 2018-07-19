4422063fa78e6b0e0c801ea845974367baabdb69
Saksham Bansal <7020962+anctartica@users.noreply.github.com>
2018-07-19 12:20:31 +0700

    appveyor.yml: Sync moban
    Closes https://github.com/coala/coala/issues/5640

--- setup.py
+++ setup.py
@@ -150,7 +150,7 @@ def read_requirements(filename):
                     raise ValueError('Unknown requirement: {0}'
                                      .format(line))
 
-                DEPENDENCY_LINKS.append(repo_link)
+                DEPENDENCY_LINKS.append(line)
 
                 line = egg_name.replace('-', '==')
 
