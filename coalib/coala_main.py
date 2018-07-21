7d1ef1a2cd93cdc00008ed23f5673cfdda19b044
Darshan Baid <darshanbaid22@gmail.com>
2018-07-21 15:00:56 +0530

    changed ArgParser to ArgumentParser

--- coalib/coala_main.py
+++ coalib/coala_main.py
@@ -89,7 +89,7 @@ def run_coala(console_printer=None,
                                     `force_show_patch` will be ignored.
     :param force_show_patch:        If set to True, a patch will be always
                                     shown. (Using ApplyPatchAction.)
-    :param arg_parser:              Instance of ArgParser that is used to parse
+    :param arg_parser:              Instance of ArgumentParser that is used to parse
                                     non-setting arguments.
     :param arg_list:                The CLI argument list.
     :param args:                    Alternative pre-parsed CLI arguments.
