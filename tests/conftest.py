7733cf8322058fec2afbd7a492387181d35ec034
John Vandenberg <jayvdb@gmail.com>
2018-07-27 01:27:34 +0700

    setup.cfg: Use template
    Also use template for tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
-from pytest_reqs import check_requirements
+# Allow import to fail to avoid annoying developers
+try:
+    from pytest_reqs import check_requirements
+except ImportError:
+    check_requirements = None
 
 
-def pytest_collection_modifyitems(config, session, items):
-    check_requirements(config, session, items)
+if check_requirements:
+    def pytest_collection_modifyitems(config, session, items):
+        check_requirements(config, session, items)
