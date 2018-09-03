7eb687ecbb4b2276ed9914b481a993638123bee0
Mahendra Thanniru <mahendra.thanniru777@gmail.com>
2018-09-03 08:46:27 +0530

    __init__.py: Arrange  imports lexicographically
    Classes imported in
    coala/coalib/bearlib/languages/__init__.py
    file are arranged lexicographically.
    
    Closes https://github.com/coala/coala/issues/5745

--- coalib/bearlib/languages/__init__.py
+++ coalib/bearlib/languages/__init__.py
@@ -3,40 +3,56 @@ This directory holds means to get generic information for specific languages.
 """
 
 # Start ignoring PyUnusedCodeBear
-from .Language import Language
-from .Language import Languages
-
-from .definitions.Unknown import Unknown
 from .definitions.Bash import Bash
+
 from .definitions.C import C
 from .definitions.CPP import CPP
 from .definitions.CSharp import CSharp
 from .definitions.CSS import CSS
+
 from .definitions.Fortran import Fortran
+
 from .definitions.Golang import Golang
+
 from .definitions.html import HTML
+
 from .definitions.Java import Java
 from .definitions.JavaScript import JavaScript
+from .definitions.Jinja2 import Jinja2
 from .definitions.JSON import JSON
 from .definitions.JSP import JSP
+
 from .definitions.KornShell import KornShell
-from .definitions.m4 import m4
-from .definitions.Matlab import Matlab
+
+from .Language import Language
+from .Language import Languages
+
 from .definitions.Markdown import Markdown
+from .definitions.Matlab import Matlab
+from .definitions.m4 import m4
+
 from .definitions.ObjectiveC import ObjectiveC
+
 from .definitions.PHP import PHP
 from .definitions.PLSQL import PLSQL
 from .definitions.PowerShell import PowerShell
 from .definitions.Python import Python
+
 from .definitions.Ruby import Ruby
+
 from .definitions.Scala import Scala
-from .definitions.Swift import Swift
-from .definitions.Tcl import Tcl
-from .definitions.Vala import Vala
-from .definitions.TypeScript import TypeScript
 from .definitions.Shell import Shell
-from .definitions.Jinja2 import Jinja2
+from .definitions.Swift import Swift
+
+from .definitions.Tcl import Tcl
+from .definitions.TypeScript import TypeScript
+
+from .definitions.Unknown import Unknown
+
+from .definitions.Vala import Vala
 from .definitions.VisualBasic import VisualBasic
+
 from .definitions.XML import XML
+
 from.definitions.ZShell import ZShell
 # Stop ignoring PyUnusedCodeBear
